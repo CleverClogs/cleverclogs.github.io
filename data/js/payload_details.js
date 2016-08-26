40f9c2170a6eb187969931166de09af86fadbb83
CleverClogs <github@cleverclogs.org>
2016-08-26 09:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11970,
-	"created_at": "Thu Aug 25 21:19:29 +0000 2016",
+	"tweets": 11971,
+	"created_at": "Fri Aug 26 07:19:29 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_08.js",
 		"year": 2016,
 		"var_name": "tweets_2016_08",
-		"tweet_count": 418,
+		"tweet_count": 419,
 		"month": 8
 	},
 	{
