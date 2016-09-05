af8781e294190b934684ec9276f556f52f7a97fc
CleverClogs <github@cleverclogs.org>
2016-09-05 14:19:27 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12139,
-	"created_at": "Mon Sep 05 10:19:39 +0000 2016",
+	"tweets": 12140,
+	"created_at": "Mon Sep 05 12:19:27 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Mon Sep 05 11:32:27 +0000 2016",
+		"id": 772759294961942500,
+		"id_str": "772759294961942528",
+		"text": "#OneNote Onboarding is the exact topic of tomorrow's #OneNoteQ TweetMeets!\nhttps://t.co/wq3SEgl8PP\n#msfteduchat\n\n https://t.co/lOlntLWxMX",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "OneNoteQ",
+					"indices": [
+						53,
+						62
+					]
+				},
+				{
+					"text": "msfteduchat",
+					"indices": [
+						99,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wq3SEgl8PP",
+					"expanded_url": "http://onen.link/onenoteq-tweetmeet-sept6-session2",
+					"display_url": "onen.link/onenoteq-tweet…",
+					"indices": [
+						75,
+						98
+					]
+				},
+				{
+					"url": "https://t.co/lOlntLWxMX",
+					"expanded_url": "https://twitter.com/megsamanda/status/772753755141836801",
+					"display_url": "twitter.com/megsamanda/sta…",
+					"indices": [
+						114,
+						137
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Home to OneNote Community Notebook.\nCuration: Marjolein Hoekstra – OneNote MVP and Microsoft Partner Product Expert",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7090,
+			"friends_count": 3414,
+			"listed_count": 381,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 11437,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12139,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 772753755141836800,
+		"quoted_status_id_str": "772753755141836801",
+		"quoted_status": {
+			"created_at": "Mon Sep 05 11:10:26 +0000 2016",
+			"id": 772753755141836800,
+			"id_str": "772753755141836801",
+			"text": "If you had to introduce #OneNote to #teachers, what would you include in your list? @MSAUedu #AussieEd #vicpln",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OneNote",
+						"indices": [
+							24,
+							32
+						]
+					},
+					{
+						"text": "teachers",
+						"indices": [
+							36,
+							45
+						]
+					},
+					{
+						"text": "AussieEd",
+						"indices": [
+							93,
+							102
+						]
+					},
+					{
+						"text": "vicpln",
+						"indices": [
+							103,
+							110
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "MSAUedu",
+						"name": "Microsoft Education",
+						"id": 3666731533,
+						"id_str": "3666731533",
+						"indices": [
+							84,
+							92
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 39202382,
+				"id_str": "39202382",
+				"name": "Megan Iemma",
+				"screen_name": "megsamanda",
+				"location": "Melbourne, Australia",
+				"description": "Speaker, Trainer and Digital Coach specialising in Small Business and Entrepreneurs - #MicrosoftSMB Ambassador.",
+				"url": "https://t.co/iThWmt268w",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/iThWmt268w",
+								"expanded_url": "http://www.TechCoachHQ.com.au",
+								"display_url": "TechCoachHQ.com.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4781,
+				"friends_count": 5112,
+				"listed_count": 372,
+				"created_at": "Mon May 11 05:55:24 +0000 2009",
+				"favourites_count": 588,
+				"utc_offset": 36000,
+				"time_zone": "Melbourne",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 21216,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/629458537236729860/0_WtHPU4.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/629458537236729860/0_WtHPU4.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/747362688372662273/BvaFk9y-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/747362688372662273/BvaFk9y-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/39202382/1467007360",
+				"profile_link_color": "990000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Sep 05 10:10:27 +0000 2016",
 		"id": 772738656310296600,
 		"id_str": "772738656310296576",
