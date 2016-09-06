91dc577d07ccbc863af0d603ea96bebe39e38b2e
CleverClogs <github@cleverclogs.org>
2016-09-06 14:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12147,
-	"created_at": "Mon Sep 05 21:19:28 +0000 2016",
+	"tweets": 12148,
+	"created_at": "Tue Sep 06 12:19:29 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_09.js
+++ data/js/tweets/2016_09.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2016_09 = 
 [
 	{
+		"created_at": "Tue Sep 06 11:42:27 +0000 2016",
+		"id": 773124197052743700,
+		"id_str": "773124197052743680",
+		"text": "Fantastic video on evolution of feedback, thanks to @Office and #OneNote\ncc @mtholfsen @arbar13 @LBayne @OneNoteEdu https://t.co/Ui4vkP28Da",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						64,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Office",
+					"name": "Microsoft Office",
+					"id": 22209176,
+					"id_str": "22209176",
+					"indices": [
+						52,
+						59
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						76,
+						86
+					]
+				},
+				{
+					"screen_name": "arbar13",
+					"name": "Ari Schorr",
+					"id": 36726617,
+					"id_str": "36726617",
+					"indices": [
+						87,
+						95
+					]
+				},
+				{
+					"screen_name": "LBayne",
+					"name": "LBayne",
+					"id": 18954505,
+					"id_str": "18954505",
+					"indices": [
+						96,
+						103
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "@OneNoteEDU",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						104,
+						115
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ui4vkP28Da",
+					"expanded_url": "https://twitter.com/anfield1974/status/773011154180288512",
+					"display_url": "twitter.com/anfield1974/st…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Home to OneNote Community Notebook.\nCuration: Marjolein Hoekstra – OneNote MVP and Microsoft Partner Product Expert",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7105,
+			"friends_count": 3426,
+			"listed_count": 382,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 11452,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12147,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 773011154180288500,
+		"quoted_status_id_str": "773011154180288512",
+		"quoted_status": {
+			"created_at": "Tue Sep 06 04:13:15 +0000 2016",
+			"id": 773011154180288500,
+			"id_str": "773011154180288512",
+			"text": "A 2 min vid by Christine Beckmann on how feedback has evolved in the AHS English Dept.  https://t.co/czL4EAukHd #MIEE #OneNote #MSFTEDU",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MIEE",
+						"indices": [
+							112,
+							117
+						]
+					},
+					{
+						"text": "OneNote",
+						"indices": [
+							118,
+							126
+						]
+					},
+					{
+						"text": "MSFTEDU",
+						"indices": [
+							127,
+							135
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/czL4EAukHd",
+						"expanded_url": "https://youtu.be/iCldQNXPlcw",
+						"display_url": "youtu.be/iCldQNXPlcw",
+						"indices": [
+							88,
+							111
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 158195502,
+				"id_str": "158195502",
+				"name": "Steve Crapnell",
+				"screen_name": "anfield1974",
+				"location": "Brisbane",
+				"description": "Liverpool fan and interested in eLearning & Maths. Exploring ways of leveraging technologies in learning environments. Microsoft Innovative Expert Educator.",
+				"url": "https://t.co/ZhQDoENqKD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ZhQDoENqKD",
+								"expanded_url": "http://teachtechandlearning.wordpress.com",
+								"display_url": "teachtechandlearning.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 140,
+				"friends_count": 174,
+				"listed_count": 16,
+				"created_at": "Tue Jun 22 01:29:56 +0000 2010",
+				"favourites_count": 340,
+				"utc_offset": 36000,
+				"time_zone": "Brisbane",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 596,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/726752075263045633/QH1c6AJS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/726752075263045633/QH1c6AJS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/158195502/1443958047",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Sep 05 21:16:21 +0000 2016",
 		"id": 772906237969170400,
 		"id_str": "772906237969170432",
