f4e0ec994a6a67dbe266f9fd270e7b19e0e3fd61
CleverClogs <github@cleverclogs.org>
2016-10-26 00:19:34 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13036,
-	"created_at": "Tue Oct 25 21:19:34 +0000 2016",
+	"tweets": 13038,
+	"created_at": "Tue Oct 25 22:19:34 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,488 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Tue Oct 25 22:18:54 +0000 2016",
+		"id": 791041371368980500,
+		"id_str": "791041371368980481",
+		"text": "Fantastic illustrations of how students are familiarizing themselves with #DigitalInk in #OneNote.\ncc @IanMikutel… https://t.co/L16vfjKf71",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigitalInk",
+					"indices": [
+						74,
+						85
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						89,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IanMikutel",
+					"name": "Ian Mikutel",
+					"id": 2157421,
+					"id_str": "2157421",
+					"indices": [
+						102,
+						113
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/L16vfjKf71",
+					"expanded_url": "https://twitter.com/i/web/status/791041371368980481",
+					"display_url": "twitter.com/i/web/status/7…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Home to OneNote Community Notebook.\nCuration: Marjolein Hoekstra – OneNote MVP and Microsoft Partner Product Expert",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7973,
+			"friends_count": 3599,
+			"listed_count": 431,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 12671,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13018,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 790988232108417000,
+		"quoted_status_id_str": "790988232108417024",
+		"quoted_status": {
+			"created_at": "Tue Oct 25 18:47:45 +0000 2016",
+			"id": 790988232108417000,
+			"id_str": "790988232108417024",
+			"text": "7th grade Math students try completing problems with digital ink #Lenovo #Ideapadmiix &amp; @msonenote #mschat #onenote https://t.co/iBzmn9D2PM",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Lenovo",
+						"indices": [
+							65,
+							72
+						]
+					},
+					{
+						"text": "Ideapadmiix",
+						"indices": [
+							73,
+							85
+						]
+					},
+					{
+						"text": "mschat",
+						"indices": [
+							103,
+							110
+						]
+					},
+					{
+						"text": "onenote",
+						"indices": [
+							111,
+							119
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "msonenote",
+						"name": "Microsoft OneNote",
+						"id": 23735316,
+						"id_str": "23735316",
+						"indices": [
+							92,
+							102
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 790988224562884600,
+						"id_str": "790988224562884608",
+						"indices": [
+							120,
+							143
+						],
+						"media_url": "http://pbs.twimg.com/media/CvonmXKWcAAn5V7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/CvonmXKWcAAn5V7.jpg",
+						"url": "https://t.co/iBzmn9D2PM",
+						"display_url": "pic.twitter.com/iBzmn9D2PM",
+						"expanded_url": "https://twitter.com/AhernMiddle/status/790988232108417024/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1936,
+								"h": 1936,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 790988224562884600,
+						"id_str": "790988224562884608",
+						"indices": [
+							120,
+							143
+						],
+						"media_url": "http://pbs.twimg.com/media/CvonmXKWcAAn5V7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/CvonmXKWcAAn5V7.jpg",
+						"url": "https://t.co/iBzmn9D2PM",
+						"display_url": "pic.twitter.com/iBzmn9D2PM",
+						"expanded_url": "https://twitter.com/AhernMiddle/status/790988232108417024/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1936,
+								"h": 1936,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2743125368,
+				"id_str": "2743125368",
+				"name": "Ahern Middle",
+				"screen_name": "AhernMiddle",
+				"location": "Foxborough, MA",
+				"description": "John J. Ahern Middle School",
+				"url": "https://t.co/vYd9aPol3Q",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/vYd9aPol3Q",
+								"expanded_url": "http://foxborough.k12.ma.us/ahern/",
+								"display_url": "foxborough.k12.ma.us/ahern/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 212,
+				"friends_count": 175,
+				"listed_count": 11,
+				"created_at": "Mon Aug 18 21:28:17 +0000 2014",
+				"favourites_count": 16,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 105,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/501481235768557568/3Uz58Dt5_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/501481235768557568/3Uz58Dt5_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2743125368/1408398166",
+				"profile_link_color": "006BB3",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Tue Oct 25 21:28:59 +0000 2016",
+		"id": 791028811475324900,
+		"id_str": "791028811475324929",
+		"text": "#DigitalInk in #OneNote for #Windows10 really leaves a lot to be desired. In-depth article:\nhttps://t.co/N6GAhghW1g\nby @MarkHachman @PCWorld",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigitalInk",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						15,
+						23
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						28,
+						38
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "markhachman",
+					"name": "Mark Hachman",
+					"id": 34684077,
+					"id_str": "34684077",
+					"indices": [
+						119,
+						131
+					]
+				},
+				{
+					"screen_name": "pcworld",
+					"name": "PCWorld",
+					"id": 6070762,
+					"id_str": "6070762",
+					"indices": [
+						132,
+						140
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/N6GAhghW1g",
+					"expanded_url": "http://onen.link/2e8c1zk",
+					"display_url": "onen.link/2e8c1zk",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Home to OneNote Community Notebook.\nCuration: Marjolein Hoekstra – OneNote MVP and Microsoft Partner Product Expert",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7973,
+			"friends_count": 3599,
+			"listed_count": 431,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 12671,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13018,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Oct 25 20:50:10 +0000 2016",
 		"id": 791019043012698100,
 		"id_str": "791019043012698113",
