3f083a9917877ba66fabacd1d63f6b807afdb280
CleverClogs <github@cleverclogs.org>
2016-11-12 18:19:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13305,
-	"created_at": "Sat Nov 12 16:19:28 +0000 2016",
+	"tweets": 13306,
+	"created_at": "Sat Nov 12 17:19:30 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_11.js
+++ data/js/tweets/2016_11.js
@@ -1,6 +1,145 @@
 Grailbird.data.tweets_2016_11 = 
 [
 	{
+		"created_at": "Sat Nov 12 16:20:18 +0000 2016",
+		"id": 797474106950225900,
+		"id_str": "797474106950225920",
+		"text": "@EdTech_Stories @SkypeClassroom @a_minshall It's probably fine as it is, but good to know you own it. \ncc  #MSFTEduChat host @mrdearybury1",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						107,
+						119
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EdTech_Stories",
+					"name": "William Jenkins",
+					"id": 1280294108,
+					"id_str": "1280294108",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "SkypeClassroom",
+					"name": "Skype Classroom",
+					"id": 214027732,
+					"id_str": "214027732",
+					"indices": [
+						16,
+						31
+					]
+				},
+				{
+					"screen_name": "a_minshall",
+					"name": "Andrew",
+					"id": 223642779,
+					"id_str": "223642779",
+					"indices": [
+						32,
+						43
+					]
+				},
+				{
+					"screen_name": "mrdearybury1",
+					"name": "Jed Dearybury",
+					"id": 17188651,
+					"id_str": "17188651",
+					"indices": [
+						125,
+						138
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 797473008071966700,
+		"in_reply_to_status_id_str": "797473008071966720",
+		"in_reply_to_user_id": 1280294108,
+		"in_reply_to_user_id_str": "1280294108",
+		"in_reply_to_screen_name": "EdTech_Stories",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Home to OneNote Community Notebook.\nCuration: Marjolein Hoekstra – OneNote MVP and Microsoft Partner Product Expert",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8423,
+			"friends_count": 3664,
+			"listed_count": 445,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 13040,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13282,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Nov 12 16:05:25 +0000 2016",
 		"id": 797470364490231800,
 		"id_str": "797470364490231808",
