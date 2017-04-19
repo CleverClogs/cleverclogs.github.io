3c189dc4222d9a9a08b9b978db487df0d759b2f8
CleverClogs <github@cleverclogs.org>
2017-04-20 01:19:33 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15823,
-	"created_at": "Wed Apr 19 22:19:31 +0000 2017",
+	"tweets": 15824,
+	"created_at": "Wed Apr 19 23:19:32 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,147 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Wed Apr 19 22:54:45 +0000 2017",
+		"id": 854830656211308500,
+		"id_str": "854830656211308545",
+		"text": "@ArnikaMac @MSAUedu @teachertimmuir @MSNZEducation @nzannet ouch, forgive me!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ArnikaMac",
+					"name": "Arnika Macphail",
+					"id": 1629216283,
+					"id_str": "1629216283",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "MSAUedu",
+					"name": "Microsoft Education",
+					"id": 3666731533,
+					"id_str": "3666731533",
+					"indices": [
+						11,
+						19
+					]
+				},
+				{
+					"screen_name": "teachertimmuir",
+					"name": "tim muir",
+					"id": 3052067299,
+					"id_str": "3052067299",
+					"indices": [
+						20,
+						35
+					]
+				},
+				{
+					"screen_name": "MSNZEducation",
+					"name": "Microsoft NZ Edu",
+					"id": 1594828806,
+					"id_str": "1594828806",
+					"indices": [
+						36,
+						50
+					]
+				},
+				{
+					"screen_name": "nzannet",
+					"name": "Anne Taylor",
+					"id": 219530095,
+					"id_str": "219530095",
+					"indices": [
+						51,
+						59
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 854830502167040000,
+		"in_reply_to_status_id_str": "854830502167040000",
+		"in_reply_to_user_id": 1629216283,
+		"in_reply_to_user_id_str": "1629216283",
+		"in_reply_to_screen_name": "ArnikaMac",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Curation: Marjolein Hoekstra – OneNote MVP, Microsoft Partner Product Expert, OneNote trainer & consultant.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11153,
+			"friends_count": 4134,
+			"listed_count": 487,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 15946,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 15749,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Apr 19 21:55:36 +0000 2017",
 		"id": 854815770827739100,
 		"id_str": "854815770827739137",
