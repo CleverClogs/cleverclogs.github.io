1cd05da21aafe540879214c0fca664e712eb9e72
CleverClogs <github@cleverclogs.org>
2017-05-06 13:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16000,
-	"created_at": "Sat May 06 07:19:28 +0000 2017",
+	"tweets": 16001,
+	"created_at": "Sat May 06 11:19:28 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_05.js
+++ data/js/tweets/2017_05.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2017_05 = 
 [
 	{
+		"created_at": "Sat May 06 10:46:23 +0000 2017",
+		"id": 860807951761764400,
+		"id_str": "860807951761764352",
+		"text": "RT @RayFleming: Keyboard shortcut top tip in #Office365 #OneNote and #Word: Move rows in a table up\\down with SHIFT-ALT-UP/DOWN arrow",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Office365",
+					"indices": [
+						45,
+						55
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						56,
+						64
+					]
+				},
+				{
+					"text": "Word",
+					"indices": [
+						69,
+						74
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RayFleming",
+					"name": "Ray Fleming",
+					"id": 18246375,
+					"id_str": "18246375",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Curation: Marjolein Hoekstra – OneNote MVP, Microsoft Partner Product Expert, OneNote trainer & consultant.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11368,
+			"friends_count": 4204,
+			"listed_count": 490,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 16174,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 15923,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/758744402277732352/m_WyaiYK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1470234076",
+			"profile_link_color": "80397B",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu May 04 23:39:09 +0000 2017",
+			"id": 860277648127254500,
+			"id_str": "860277648127254528",
+			"text": "Keyboard shortcut top tip in #Office365 #OneNote and #Word: Move rows in a table up\\down with SHIFT-ALT-UP/DOWN arrow",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Office365",
+						"indices": [
+							29,
+							39
+						]
+					},
+					{
+						"text": "OneNote",
+						"indices": [
+							40,
+							48
+						]
+					},
+					{
+						"text": "Word",
+						"indices": [
+							53,
+							58
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18246375,
+				"id_str": "18246375",
+				"name": "Ray Fleming",
+				"screen_name": "RayFleming",
+				"location": "Sydney, Australia",
+				"description": "I work at Microsoft Australia as the Education Industry Market Manager. So my three professional passions are education, IT, and Microsoft",
+				"url": "http://t.co/odQD38fUQ1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/odQD38fUQ1",
+								"expanded_url": "http://blogs.msdn.com/education",
+								"display_url": "blogs.msdn.com/education",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2901,
+				"friends_count": 1497,
+				"listed_count": 219,
+				"created_at": "Fri Dec 19 17:36:23 +0000 2008",
+				"favourites_count": 1043,
+				"utc_offset": 36000,
+				"time_zone": "Sydney",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5630,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FCFCFC",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/166714260/RayFullSizeAvatar.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/166714260/RayFullSizeAvatar.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/705966529804836871/rKtT-Wql_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/705966529804836871/rKtT-Wql_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18246375/1448600526",
+				"profile_link_color": "6A88D4",
+				"profile_sidebar_border_color": "86A4A6",
+				"profile_sidebar_fill_color": "AAB7B8",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat May 06 07:18:19 +0000 2017",
 		"id": 860755592402665500,
 		"id_str": "860755592402665476",
