da8be8ad25a652c518135e256545736d1204929a
CleverClogs <github@cleverclogs.org>
2017-06-13 00:19:33 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16672,
-	"created_at": "Mon Jun 12 19:19:34 +0000 2017",
+	"tweets": 16673,
+	"created_at": "Mon Jun 12 22:19:32 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_06.js
+++ data/js/tweets/2017_06.js
@@ -1,6 +1,456 @@
 Grailbird.data.tweets_2017_06 = 
 [
 	{
+		"created_at": "Mon Jun 12 22:13:57 +0000 2017",
+		"id": 874389335667245000,
+		"id_str": "874389335667245056",
+		"text": "RT @iamicebrakr: Another quick #Minutemen sketch in @msonenote. #CreatedWithSurface Pro 4.\n@surface @OneNoteC https://t.co/0UOeLd4lei",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Minutemen",
+					"indices": [
+						31,
+						41
+					]
+				},
+				{
+					"text": "CreatedWithSurface",
+					"indices": [
+						64,
+						83
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iamicebrakr",
+					"name": "Icebrakr",
+					"id": 741690071116582900,
+					"id_str": "741690071116582912",
+					"indices": [
+						3,
+						15
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						52,
+						62
+					]
+				},
+				{
+					"screen_name": "surface",
+					"name": "Microsoft Surface",
+					"id": 612076511,
+					"id_str": "612076511",
+					"indices": [
+						91,
+						99
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						100,
+						109
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 874352254530334700,
+					"id_str": "874352254530334720",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+					"url": "https://t.co/0UOeLd4lei",
+					"display_url": "pic.twitter.com/0UOeLd4lei",
+					"expanded_url": "https://twitter.com/iamicebrakr/status/874352408708755457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1327,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 441,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 778,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 874352408708755500,
+					"source_status_id_str": "874352408708755457",
+					"source_user_id": 741690071116582900,
+					"source_user_id_str": "741690071116582912"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 874352254530334700,
+					"id_str": "874352254530334720",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+					"url": "https://t.co/0UOeLd4lei",
+					"display_url": "pic.twitter.com/0UOeLd4lei",
+					"expanded_url": "https://twitter.com/iamicebrakr/status/874352408708755457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1327,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 441,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 778,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 874352408708755500,
+					"source_status_id_str": "874352408708755457",
+					"source_user_id": 741690071116582900,
+					"source_user_id_str": "741690071116582912"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Curation: Marjolein Hoekstra – OneNote MVP, Microsoft Partner Product Expert, OneNote trainer & consultant.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11917,
+			"friends_count": 4304,
+			"listed_count": 498,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 17074,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16585,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/866031266528997376/EIvkBQMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/866031266528997376/EIvkBQMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1495313088",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			18578140
+		],
+		"retweeted_status": {
+			"created_at": "Mon Jun 12 19:47:13 +0000 2017",
+			"id": 874352408708755500,
+			"id_str": "874352408708755457",
+			"text": "Another quick #Minutemen sketch in @msonenote. #CreatedWithSurface Pro 4.\n@surface @OneNoteC https://t.co/0UOeLd4lei",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Minutemen",
+						"indices": [
+							14,
+							24
+						]
+					},
+					{
+						"text": "CreatedWithSurface",
+						"indices": [
+							47,
+							66
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "msonenote",
+						"name": "Microsoft OneNote",
+						"id": 23735316,
+						"id_str": "23735316",
+						"indices": [
+							35,
+							45
+						]
+					},
+					{
+						"screen_name": "surface",
+						"name": "Microsoft Surface",
+						"id": 612076511,
+						"id_str": "612076511",
+						"indices": [
+							74,
+							82
+						]
+					},
+					{
+						"screen_name": "OneNoteC",
+						"name": "OneNote Central",
+						"id": 2582768738,
+						"id_str": "2582768738",
+						"indices": [
+							83,
+							92
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 874352254530334700,
+						"id_str": "874352254530334720",
+						"indices": [
+							93,
+							116
+						],
+						"media_url": "http://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+						"url": "https://t.co/0UOeLd4lei",
+						"display_url": "pic.twitter.com/0UOeLd4lei",
+						"expanded_url": "https://twitter.com/iamicebrakr/status/874352408708755457/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1327,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 441,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 778,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 874352254530334700,
+						"id_str": "874352254530334720",
+						"indices": [
+							93,
+							116
+						],
+						"media_url": "http://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DCJSvjPXsAAj0RP.jpg",
+						"url": "https://t.co/0UOeLd4lei",
+						"display_url": "pic.twitter.com/0UOeLd4lei",
+						"expanded_url": "https://twitter.com/iamicebrakr/status/874352408708755457/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1327,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 441,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 778,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 741690071116582900,
+				"id_str": "741690071116582912",
+				"name": "Icebrakr",
+				"screen_name": "iamicebrakr",
+				"location": "Massachusetts, USA",
+				"description": "Bartlett '17 Class Artist and Graduate. I draw, build robots make music and try to make people smile. Founder of @ActiMatesHQ.",
+				"url": "https://t.co/bQF8DY6MdZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bQF8DY6MdZ",
+								"expanded_url": "https://nicktobolski.newgrounds.com",
+								"display_url": "nicktobolski.newgrounds.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 862,
+				"friends_count": 843,
+				"listed_count": 19,
+				"created_at": "Sat Jun 11 17:54:27 +0000 2016",
+				"favourites_count": 5659,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 9125,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/874281433912803328/KSiRVZfH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/874281433912803328/KSiRVZfH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/741690071116582912/1497112328",
+				"profile_link_color": "FF691F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Jun 12 18:35:25 +0000 2017",
 		"id": 874334339995140100,
 		"id_str": "874334339995140096",
