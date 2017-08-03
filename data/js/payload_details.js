c83ae028b2039cf63f044467eaa18847d713adbd
CleverClogs <github@cleverclogs.org>
2017-08-03 18:19:31 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17511,
-	"created_at": "Thu Aug 03 15:19:29 +0000 2017",
+	"tweets": 17512,
+	"created_at": "Thu Aug 03 16:19:29 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,187 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Thu Aug 03 15:21:18 +0000 2017",
+		"id": 893129658958532600,
+		"id_str": "893129658958532608",
+		"text": "@BhavneetSingh12 @townesy77 @mimasaunders @brookssensei @jarradstrain @superdavey @pipcleaves @standouted… https://t.co/6AZl4h9qTF",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BhavneetSingh12",
+					"name": "Bhavneet",
+					"id": 524001932,
+					"id_str": "524001932",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "townesy77",
+					"name": "Megan Townes",
+					"id": 27093634,
+					"id_str": "27093634",
+					"indices": [
+						17,
+						27
+					]
+				},
+				{
+					"screen_name": "mimasaunders",
+					"name": "Jemima Saunders",
+					"id": 17763374,
+					"id_str": "17763374",
+					"indices": [
+						28,
+						41
+					]
+				},
+				{
+					"screen_name": "brookssensei",
+					"name": "Adam Brooks",
+					"id": 1591118918,
+					"id_str": "1591118918",
+					"indices": [
+						42,
+						55
+					]
+				},
+				{
+					"screen_name": "jarradstrain",
+					"name": "Jarrad Strain",
+					"id": 1208867881,
+					"id_str": "1208867881",
+					"indices": [
+						56,
+						69
+					]
+				},
+				{
+					"screen_name": "superdavey",
+					"name": "David Brown",
+					"id": 18545434,
+					"id_str": "18545434",
+					"indices": [
+						70,
+						81
+					]
+				},
+				{
+					"screen_name": "pipcleaves",
+					"name": "Pip Cleaves",
+					"id": 30757847,
+					"id_str": "30757847",
+					"indices": [
+						82,
+						93
+					]
+				},
+				{
+					"screen_name": "standouted",
+					"name": "Steven Payne",
+					"id": 2212322413,
+					"id_str": "2212322413",
+					"indices": [
+						94,
+						105
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6AZl4h9qTF",
+					"expanded_url": "https://twitter.com/i/web/status/893129658958532608",
+					"display_url": "twitter.com/i/web/status/8…",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 893120435843235800,
+		"in_reply_to_status_id_str": "893120435843235840",
+		"in_reply_to_user_id": 524001932,
+		"in_reply_to_user_id_str": "524001932",
+		"in_reply_to_screen_name": "BhavneetSingh12",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Microsoft #OneNote tips, news and resources. Curation: Marjolein Hoekstra – OneNote MVP, Microsoft Partner Product Expert, OneNote trainer & consultant.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12685,
+			"friends_count": 4553,
+			"listed_count": 507,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 18319,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 17405,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/866031266528997376/EIvkBQMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/866031266528997376/EIvkBQMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Aug 03 14:36:44 +0000 2017",
 		"id": 893118443716792300,
 		"id_str": "893118443716792320",
