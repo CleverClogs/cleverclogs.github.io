42c7d5f9951cf96f952738b55091abe36b7acfb4
CleverClogs <github@cleverclogs.org>
2017-09-13 20:19:33 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18232,
-	"created_at": "Wed Sep 13 16:19:33 +0000 2017",
+	"tweets": 18233,
+	"created_at": "Wed Sep 13 18:19:31 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_09.js
+++ data/js/tweets/2017_09.js
@@ -1,6 +1,321 @@
 Grailbird.data.tweets_2017_09 = 
 [
 	{
+		"created_at": "Wed Sep 13 17:48:39 +0000 2017",
+		"id": 908024642320904200,
+		"id_str": "908024642320904205",
+		"text": "Game of Notes is on soon with #OneNote VP @LauraCatPJs via @OfficeInsider!\nhttps://t.co/o2cYHpwjkF\n#Road2Cookies\nhttps://t.co/CmR4vtA5qo",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						30,
+						38
+					]
+				},
+				{
+					"text": "Road2Cookies",
+					"indices": [
+						99,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LauraCatPJs",
+					"name": "Laura Butler",
+					"id": 760954730612797400,
+					"id_str": "760954730612797440",
+					"indices": [
+						42,
+						54
+					]
+				},
+				{
+					"screen_name": "OfficeInsider",
+					"name": "MS Office Insiders",
+					"id": 4444936638,
+					"id_str": "4444936638",
+					"indices": [
+						59,
+						73
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/o2cYHpwjkF",
+					"expanded_url": "https://support.office.com/en-us/article/OneNote-Cookie-Challenge-c06699b6-207e-4d21-8211-ef858dce6c24",
+					"display_url": "support.office.com/en-us/article/…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 907967112165163000,
+					"id_str": "907967112165163008",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/907967112165163008/img/E96Ld3ESBI5l0bTn.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/907967112165163008/img/E96Ld3ESBI5l0bTn.jpg",
+					"url": "https://t.co/CmR4vtA5qo",
+					"display_url": "pic.twitter.com/CmR4vtA5qo",
+					"expanded_url": "https://twitter.com/OfficeInsider/status/907967240275972097/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 191,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 907967240275972100,
+					"source_status_id_str": "907967240275972097",
+					"source_user_id": 4444936638,
+					"source_user_id_str": "4444936638"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 907967112165163000,
+					"id_str": "907967112165163008",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/907967112165163008/img/E96Ld3ESBI5l0bTn.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/907967112165163008/img/E96Ld3ESBI5l0bTn.jpg",
+					"url": "https://t.co/CmR4vtA5qo",
+					"display_url": "pic.twitter.com/CmR4vtA5qo",
+					"expanded_url": "https://twitter.com/OfficeInsider/status/907967240275972097/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 191,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 907967240275972100,
+					"source_status_id_str": "907967240275972097",
+					"source_user_id": 4444936638,
+					"source_user_id_str": "4444936638",
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 29696,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/907967112165163008/pl/_rAjhBQtIXeBFwao.m3u8"
+							},
+							{
+								"bitrate": 320000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/907967112165163008/vid/320x180/hWzonOz-nqixSyzV.mp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/907967112165163008/vid/640x360/o3iTtNCSEMQqsghH.mp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/907967112165163008/vid/1280x720/R9k8_WMB5hoKHIWL.mp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 4444936638,
+							"id_str": "4444936638",
+							"name": "MS Office Insiders",
+							"screen_name": "OfficeInsider",
+							"location": "Redmond, WA",
+							"description": "Join a growing community of experts and enthusiasts who are helping shape the future of Microsoft Office.",
+							"url": "https://t.co/5zCfJcacyb",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/5zCfJcacyb",
+											"expanded_url": "http://off365.ms/vOIO8F",
+											"display_url": "off365.ms/vOIO8F",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 10785,
+							"friends_count": 204,
+							"listed_count": 172,
+							"created_at": "Thu Dec 03 19:30:06 +0000 2015",
+							"favourites_count": 366,
+							"utc_offset": -25200,
+							"time_zone": "Pacific Time (US & Canada)",
+							"geo_enabled": false,
+							"verified": true,
+							"statuses_count": 1821,
+							"lang": "en",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "000000",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/861875312971218944/yrN7QyT6_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/861875312971218944/yrN7QyT6_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/4444936638/1479325626",
+							"profile_link_color": "3B94D9",
+							"profile_sidebar_border_color": "000000",
+							"profile_sidebar_fill_color": "000000",
+							"profile_text_color": "000000",
+							"profile_use_background_image": false,
+							"has_extended_profile": false,
+							"default_profile": false,
+							"default_profile_image": false,
+							"following": true,
+							"follow_request_sent": false,
+							"notifications": true,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13343,
+			"friends_count": 4950,
+			"listed_count": 524,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 19557,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18114,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Sep 13 15:46:33 +0000 2017",
 		"id": 907993913071472600,
 		"id_str": "907993913071472641",
