f9496abce0442756b9f427219a6ab33da2fcb179
CleverClogs <github@cleverclogs.org>
2017-10-06 16:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18604,
-	"created_at": "Fri Oct 06 13:19:30 +0000 2017",
+	"tweets": 18605,
+	"created_at": "Fri Oct 06 14:19:28 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_10.js
+++ data/js/tweets/2017_10.js
@@ -1,6 +1,264 @@
 Grailbird.data.tweets_2017_10 = 
 [
 	{
+		"created_at": "Fri Oct 06 14:16:24 +0000 2017",
+		"id": 916306148277571600,
+		"id_str": "916306148277571584",
+		"text": "RT @ArtGelwicks: Updated: How well does OneNote work on a Chromebook?  https://t.co/Z9zqGqX80w",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ArtGelwicks",
+					"name": "Art Gelwicks",
+					"id": 1778471,
+					"id_str": "1778471",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Z9zqGqX80w",
+					"expanded_url": "https://www.theideapump.com/articles/onenote-and-chromebook",
+					"display_url": "theideapump.com/articles/oneno…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13648,
+			"friends_count": 5047,
+			"listed_count": 527,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 20115,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18483,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Oct 06 12:50:52 +0000 2017",
+			"id": 916284624531345400,
+			"id_str": "916284624531345408",
+			"text": "Updated: How well does OneNote work on a Chromebook?  https://t.co/Z9zqGqX80w",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Z9zqGqX80w",
+						"expanded_url": "https://www.theideapump.com/articles/onenote-and-chromebook",
+						"display_url": "theideapump.com/articles/oneno…",
+						"indices": [
+							54,
+							77
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.squarespace.com\" rel=\"nofollow\">Squarespace</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1778471,
+				"id_str": "1778471",
+				"name": "Art Gelwicks",
+				"screen_name": "ArtGelwicks",
+				"location": "40.248285,-75.672641",
+				"description": "Being Productive is doing the right things at the right times in the right ways.",
+				"url": "https://t.co/mY2okO27BK",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mY2okO27BK",
+								"expanded_url": "http://www.theideapump.com",
+								"display_url": "theideapump.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 704,
+				"friends_count": 636,
+				"listed_count": 42,
+				"created_at": "Wed Mar 21 18:19:25 +0000 2007",
+				"favourites_count": 489,
+				"utc_offset": -14400,
+				"time_zone": "America/New_York",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6505,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/634562449992126468/dsqvNaPn_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/634562449992126468/dsqvNaPn_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1778471/1491521354",
+				"profile_link_color": "A6032C",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": {
+				"type": "Point",
+				"coordinates": [
+					40.249364,
+					-75.664056
+				]
+			},
+			"coordinates": {
+				"type": "Point",
+				"coordinates": [
+					-75.664056,
+					40.249364
+				]
+			},
+			"place": {
+				"id": "2154ed12cb3dded5",
+				"url": "https://api.twitter.com/1.1/geo/id/2154ed12cb3dded5.json",
+				"place_type": "city",
+				"name": "Pottstown",
+				"full_name": "Pottstown, PA",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-75.678348,
+								40.232967
+							],
+							[
+								-75.610724,
+								40.232967
+							],
+							[
+								-75.610724,
+								40.267647
+							],
+							[
+								-75.678348,
+								40.267647
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Oct 06 12:26:32 +0000 2017",
 		"id": 916278498859323400,
 		"id_str": "916278498859323397",
@@ -742,7 +1000,7 @@ Grailbird.data.tweets_2017_10 =
 				"name": "Jennifer Morgan",
 				"screen_name": "JennMorganSTS",
 				"location": "Ogden, UT",
-				"description": "#NCCE Coordinator of Professional Development. #Microsoftedu OneNote Superhero​, MIE Expert, Surface Expert & Skype Master Teacher. Microsoft Master Trainer.",
+				"description": "#NCCE Coordinator of Professional Development. #Microsoftedu OneNote Superhero, MIE Expert, Surface Expert & Skype Master Teacher. Microsoft Master Trainer.",
 				"url": "https://t.co/0IXNDHPHFr",
 				"entities": {
 					"url": {
