62e726d74885b2aa4b995dc6855a63c215908229
CleverClogs <github@cleverclogs.org>
2017-10-09 12:19:31 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18627,
-	"created_at": "Mon Oct 09 08:19:29 +0000 2017",
+	"tweets": 18628,
+	"created_at": "Mon Oct 09 10:19:30 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_10.js
+++ data/js/tweets/2017_10.js
@@ -1,6 +1,284 @@
 Grailbird.data.tweets_2017_10 = 
 [
 	{
+		"created_at": "Mon Oct 09 09:53:00 +0000 2017",
+		"id": 917327025685303300,
+		"id_str": "917327025685303296",
+		"text": "Take a bite! Look at the tasty cupcakes cake baker @StephJayBakes creates in #OneNote #Windows10 on her #Surface!\nc… https://t.co/6NICayAxLm",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						77,
+						85
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						86,
+						96
+					]
+				},
+				{
+					"text": "Surface",
+					"indices": [
+						104,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StephJayBakes",
+					"name": "Stephenie Jay",
+					"id": 904470236862308400,
+					"id_str": "904470236862308353",
+					"indices": [
+						51,
+						65
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6NICayAxLm",
+					"expanded_url": "https://twitter.com/i/web/status/917327025685303296",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13673,
+			"friends_count": 5060,
+			"listed_count": 525,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 20152,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18506,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 917118276156739600,
+		"quoted_status_id_str": "917118276156739585",
+		"quoted_status": {
+			"created_at": "Sun Oct 08 20:03:30 +0000 2017",
+			"id": 917118276156739600,
+			"id_str": "917118276156739585",
+			"text": "@KeeleyHelen having far too much fun creating my branding #baking #onenote #surfacepro #cupcakes #icecream #pastel… https://t.co/k3ctTR3xI0",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "baking",
+						"indices": [
+							58,
+							65
+						]
+					},
+					{
+						"text": "onenote",
+						"indices": [
+							66,
+							74
+						]
+					},
+					{
+						"text": "surfacepro",
+						"indices": [
+							75,
+							86
+						]
+					},
+					{
+						"text": "cupcakes",
+						"indices": [
+							87,
+							96
+						]
+					},
+					{
+						"text": "icecream",
+						"indices": [
+							97,
+							106
+						]
+					},
+					{
+						"text": "pastel",
+						"indices": [
+							107,
+							114
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "KeeleyHelen",
+						"name": "Helen Keeley",
+						"id": 572002604,
+						"id_str": "572002604",
+						"indices": [
+							0,
+							12
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/k3ctTR3xI0",
+						"expanded_url": "https://twitter.com/i/web/status/917118276156739585",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							116,
+							139
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 572002604,
+			"in_reply_to_user_id_str": "572002604",
+			"in_reply_to_screen_name": "KeeleyHelen",
+			"user": {
+				"id": 904470236862308400,
+				"id_str": "904470236862308353",
+				"name": "Stephenie Jay",
+				"screen_name": "StephJayBakes",
+				"location": "Reading, England",
+				"description": "A cake maker, baker and Mum. I like doing things with sugar. Bespoke cakes and bakes to order out of RG1. Email: stepheniemjay@gmail.com",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7,
+				"friends_count": 7,
+				"listed_count": 0,
+				"created_at": "Sun Sep 03 22:24:43 +0000 2017",
+				"favourites_count": 2,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/917117940545347584/nfaN8oM6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/917117940545347584/nfaN8oM6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/904470236862308353/1507492856",
+				"profile_link_color": "DDD8C4",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Oct 09 08:16:55 +0000 2017",
 		"id": 917302844784508900,
 		"id_str": "917302844784508929",
