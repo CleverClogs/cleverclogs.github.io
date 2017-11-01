c777d48f966a954ff9228abb9ee5cfaa0e443dff
CleverClogs <github@cleverclogs.org>
2017-11-01 01:19:33 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19017,
-	"created_at": "Tue Oct 31 23:19:33 +0000 2017",
+	"tweets": 19019,
+	"created_at": "Wed Nov 01 00:19:32 +0000 2017",
 	"lang": "en"
 }
---
+++ data/js/tweets/2017_11.js
@@ -0,0 +1,658 @@
+Grailbird.data.tweets_2017_11 = 
+[
+	{
+		"created_at": "Wed Nov 01 00:03:52 +0000 2017",
+		"id": 925513683891052500,
+		"id_str": "925513683891052544",
+		"text": "RT @jess_pilsner: Reading Poe’s the Raven, from iambic octameter to a 4/4 breakbeat! @_6_RPCS #msftedu #showcaseschool @OneNoteEDU @rpcs5th…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "msftedu",
+					"indices": [
+						94,
+						102
+					]
+				},
+				{
+					"text": "showcaseschool",
+					"indices": [
+						103,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jess_pilsner",
+					"name": "Jessica Pilsner",
+					"id": 4085280258,
+					"id_str": "4085280258",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "_6_RPCS",
+					"name": "6th Renton Prep",
+					"id": 913157447400824800,
+					"id_str": "913157447400824833",
+					"indices": [
+						85,
+						93
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						119,
+						130
+					]
+				},
+				{
+					"screen_name": "rpcs5th",
+					"name": "RentonPrep 5th grade",
+					"id": 791488028409667600,
+					"id_str": "791488028409667584",
+					"indices": [
+						131,
+						139
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13907,
+			"friends_count": 5176,
+			"listed_count": 530,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 20575,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18894,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Oct 31 23:45:55 +0000 2017",
+			"id": 925509166260666400,
+			"id_str": "925509166260666368",
+			"text": "Reading Poe’s the Raven, from iambic octameter to a 4/4 breakbeat! @_6_RPCS #msftedu #showcaseschool @OneNoteEDU… https://t.co/4cuotsa8W4",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "msftedu",
+						"indices": [
+							76,
+							84
+						]
+					},
+					{
+						"text": "showcaseschool",
+						"indices": [
+							85,
+							100
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "_6_RPCS",
+						"name": "6th Renton Prep",
+						"id": 913157447400824800,
+						"id_str": "913157447400824833",
+						"indices": [
+							67,
+							75
+						]
+					},
+					{
+						"screen_name": "OneNoteEDU",
+						"name": "OneNote Education",
+						"id": 3096566760,
+						"id_str": "3096566760",
+						"indices": [
+							101,
+							112
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/4cuotsa8W4",
+						"expanded_url": "https://twitter.com/i/web/status/925509166260666368",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							114,
+							137
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4085280258,
+				"id_str": "4085280258",
+				"name": "Jessica Pilsner",
+				"screen_name": "jess_pilsner",
+				"location": "",
+				"description": "To empower, and to encourage creativity and learning. #MIEExpert #minecraftmentor #surfaceexpert",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 285,
+				"friends_count": 169,
+				"listed_count": 56,
+				"created_at": "Sun Nov 01 00:55:17 +0000 2015",
+				"favourites_count": 586,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 804,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/748293376831684608/uf_irlnd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/748293376831684608/uf_irlnd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4085280258/1467242020",
+				"profile_link_color": "91D2FA",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
+		"created_at": "Wed Nov 01 00:00:08 +0000 2017",
+		"id": 925512745235173400,
+		"id_str": "925512745235173377",
+		"text": "RT @susietinker5: Happy Halloween from our Bellevue Office! (Sadly only one in costume) @MicrosoftEDU @OneNoteEDU https://t.co/LKtpHEnnhC",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "susietinker5",
+					"name": "Susie Tinker",
+					"id": 803681576932388900,
+					"id_str": "803681576932388864",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "MicrosoftEDU",
+					"name": "Microsoft Education",
+					"id": 17826187,
+					"id_str": "17826187",
+					"indices": [
+						88,
+						101
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						102,
+						113
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 925497051101593600,
+					"id_str": "925497051101593600",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+					"url": "https://t.co/LKtpHEnnhC",
+					"display_url": "pic.twitter.com/LKtpHEnnhC",
+					"expanded_url": "https://twitter.com/susietinker5/status/925497059674697728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1538,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 925497059674697700,
+					"source_status_id_str": "925497059674697728",
+					"source_user_id": 803681576932388900,
+					"source_user_id_str": "803681576932388864"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 925497051101593600,
+					"id_str": "925497051101593600",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+					"url": "https://t.co/LKtpHEnnhC",
+					"display_url": "pic.twitter.com/LKtpHEnnhC",
+					"expanded_url": "https://twitter.com/susietinker5/status/925497059674697728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1538,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 925497059674697700,
+					"source_status_id_str": "925497059674697728",
+					"source_user_id": 803681576932388900,
+					"source_user_id_str": "803681576932388864"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13907,
+			"friends_count": 5176,
+			"listed_count": 530,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 20575,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18894,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Oct 31 22:57:48 +0000 2017",
+			"id": 925497059674697700,
+			"id_str": "925497059674697728",
+			"text": "Happy Halloween from our Bellevue Office! (Sadly only one in costume) @MicrosoftEDU @OneNoteEDU https://t.co/LKtpHEnnhC",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "MicrosoftEDU",
+						"name": "Microsoft Education",
+						"id": 17826187,
+						"id_str": "17826187",
+						"indices": [
+							70,
+							83
+						]
+					},
+					{
+						"screen_name": "OneNoteEDU",
+						"name": "OneNote Education",
+						"id": 3096566760,
+						"id_str": "3096566760",
+						"indices": [
+							84,
+							95
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 925497051101593600,
+						"id_str": "925497051101593600",
+						"indices": [
+							96,
+							119
+						],
+						"media_url": "http://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+						"url": "https://t.co/LKtpHEnnhC",
+						"display_url": "pic.twitter.com/LKtpHEnnhC",
+						"expanded_url": "https://twitter.com/susietinker5/status/925497059674697728/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1538,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 901,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 511,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 925497051101593600,
+						"id_str": "925497051101593600",
+						"indices": [
+							96,
+							119
+						],
+						"media_url": "http://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DNgGqhuVAAAfKe1.jpg",
+						"url": "https://t.co/LKtpHEnnhC",
+						"display_url": "pic.twitter.com/LKtpHEnnhC",
+						"expanded_url": "https://twitter.com/susietinker5/status/925497059674697728/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1538,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 901,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 511,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 803681576932388900,
+				"id_str": "803681576932388864",
+				"name": "Susie Tinker",
+				"screen_name": "susietinker5",
+				"location": "Seattle, WA",
+				"description": "Managing Microsoft relationships in districts and colleges in KY, OK, and TX! @MicrosoftEDU",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 120,
+				"friends_count": 131,
+				"listed_count": 8,
+				"created_at": "Tue Nov 29 19:26:34 +0000 2016",
+				"favourites_count": 479,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 145,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/803682296679186432/9zaoYdE-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/803682296679186432/9zaoYdE-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/803681576932388864/1480537757",
+				"profile_link_color": "7FDBB6",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
