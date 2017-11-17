972175dc45e97283d3c7cf84820a09873fa80ef7
CleverClogs <github@cleverclogs.org>
2017-11-17 11:19:33 +0100

    Added 1 tweets

--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,404 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Fri Nov 17 09:57:07 +0000 2017",
+		"id": 931461187308523500,
+		"id_str": "931461187308523520",
+		"text": "RT @SatArora: Thanks @mtholfsen for \"All the important links of @OneNoteEDU\" https://t.co/EvKp35sjmr",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SatArora",
+					"name": "Satyendra Arora",
+					"id": 34522744,
+					"id_str": "34522744",
+					"indices": [
+						3,
+						12
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						21,
+						31
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						64,
+						75
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 931438909682155500,
+					"id_str": "931438909682155520",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+					"url": "https://t.co/EvKp35sjmr",
+					"display_url": "pic.twitter.com/EvKp35sjmr",
+					"expanded_url": "https://twitter.com/SatArora/status/931439414936444929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1098,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 643,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 364,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"source_status_id": 931439414936444900,
+					"source_status_id_str": "931439414936444929",
+					"source_user_id": 34522744,
+					"source_user_id_str": "34522744"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 931438909682155500,
+					"id_str": "931438909682155520",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+					"url": "https://t.co/EvKp35sjmr",
+					"display_url": "pic.twitter.com/EvKp35sjmr",
+					"expanded_url": "https://twitter.com/SatArora/status/931439414936444929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1098,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 643,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 364,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"source_status_id": 931439414936444900,
+					"source_status_id_str": "931439414936444929",
+					"source_user_id": 34522744,
+					"source_user_id_str": "34522744"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14140,
+			"friends_count": 5237,
+			"listed_count": 538,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 20918,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19231,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Nov 17 08:30:36 +0000 2017",
+			"id": 931439414936444900,
+			"id_str": "931439414936444929",
+			"text": "Thanks @mtholfsen for \"All the important links of @OneNoteEDU\" https://t.co/EvKp35sjmr",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "mtholfsen",
+						"name": "Mike Tholfsen",
+						"id": 27673327,
+						"id_str": "27673327",
+						"indices": [
+							7,
+							17
+						]
+					},
+					{
+						"screen_name": "OneNoteEDU",
+						"name": "OneNote Education",
+						"id": 3096566760,
+						"id_str": "3096566760",
+						"indices": [
+							50,
+							61
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 931438909682155500,
+						"id_str": "931438909682155520",
+						"indices": [
+							63,
+							86
+						],
+						"media_url": "http://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+						"url": "https://t.co/EvKp35sjmr",
+						"display_url": "pic.twitter.com/EvKp35sjmr",
+						"expanded_url": "https://twitter.com/SatArora/status/931439414936444929/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1098,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 643,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 364,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 931438909682155500,
+						"id_str": "931438909682155520",
+						"indices": [
+							63,
+							86
+						],
+						"media_url": "http://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DO0iwN2VAAA6ch5.jpg",
+						"url": "https://t.co/EvKp35sjmr",
+						"display_url": "pic.twitter.com/EvKp35sjmr",
+						"expanded_url": "https://twitter.com/SatArora/status/931439414936444929/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1098,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 643,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 364,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 34522744,
+				"id_str": "34522744",
+				"name": "Satyendra Arora",
+				"screen_name": "SatArora",
+				"location": "People's Republic of China",
+				"description": "Technology Integrator @YCISBJ, Examiner and Internal Assessment Moderator @IBOrganization,  #AppleTeacher, #MIEExpert #MSFTEduChat Host",
+				"url": "http://t.co/9h3on30O0x",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/9h3on30O0x",
+								"expanded_url": "http://linkedin.com/in/satarora",
+								"display_url": "linkedin.com/in/satarora",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 218,
+				"friends_count": 21,
+				"listed_count": 12,
+				"created_at": "Thu Apr 23 03:51:37 +0000 2009",
+				"favourites_count": 533,
+				"utc_offset": 19800,
+				"time_zone": "New Delhi",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 8225,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EDECE9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/914852066970832897/FlJZ_4nb_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/914852066970832897/FlJZ_4nb_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/34522744/1494299587",
+				"profile_link_color": "088253",
+				"profile_sidebar_border_color": "D3D2CF",
+				"profile_sidebar_fill_color": "E3E2DE",
+				"profile_text_color": "634047",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Nov 17 06:54:58 +0000 2017",
 		"id": 931415347714019300,
 		"id_str": "931415347714019333",
