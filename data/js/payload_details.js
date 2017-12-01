ba216bb86918570e09678124ec51e13d3b341bf3
CleverClogs <github@cleverclogs.org>
2017-12-01 08:19:34 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19587,
-	"created_at": "Thu Nov 30 20:19:36 +0000 2017",
+	"tweets": 19588,
+	"created_at": "Fri Dec 01 07:19:33 +0000 2017",
 	"lang": "en"
 }
---
+++ data/js/tweets/2017_12.js
@@ -0,0 +1,477 @@
+Grailbird.data.tweets_2017_12 = 
+[
+	{
+		"created_at": "Fri Dec 01 07:12:13 +0000 2017",
+		"id": 936493120442781700,
+		"id_str": "936493120442781696",
+		"text": "RT @stevesingapore: Younger students getting very excited by pen-enabled devices at the Xmas fair @OneNoteC @swaps06 https://t.co/7KxEJUXIOa",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stevesingapore",
+					"name": "Steve Morgan",
+					"id": 200186236,
+					"id_str": "200186236",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						98,
+						107
+					]
+				},
+				{
+					"screen_name": "swaps06",
+					"name": "Swapna",
+					"id": 808830001520877600,
+					"id_str": "808830001520877568",
+					"indices": [
+						108,
+						116
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 936492436787830800,
+					"id_str": "936492436787830784",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+					"url": "https://t.co/7KxEJUXIOa",
+					"display_url": "pic.twitter.com/7KxEJUXIOa",
+					"expanded_url": "https://twitter.com/stevesingapore/status/936492447680442368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 936492447680442400,
+					"source_status_id_str": "936492447680442368",
+					"source_user_id": 200186236,
+					"source_user_id_str": "200186236"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 936492436787830800,
+					"id_str": "936492436787830784",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+					"url": "https://t.co/7KxEJUXIOa",
+					"display_url": "pic.twitter.com/7KxEJUXIOa",
+					"expanded_url": "https://twitter.com/stevesingapore/status/936492447680442368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 936492447680442400,
+					"source_status_id_str": "936492447680442368",
+					"source_user_id": 200186236,
+					"source_user_id_str": "200186236"
+				},
+				{
+					"id": 936492436787884000,
+					"id_str": "936492436787884032",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/DP8W6NWU8AASjAJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DP8W6NWU8AASjAJ.jpg",
+					"url": "https://t.co/7KxEJUXIOa",
+					"display_url": "pic.twitter.com/7KxEJUXIOa",
+					"expanded_url": "https://twitter.com/stevesingapore/status/936492447680442368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 936492447680442400,
+					"source_status_id_str": "936492447680442368",
+					"source_user_id": 200186236,
+					"source_user_id_str": "200186236"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14267,
+			"friends_count": 5275,
+			"listed_count": 541,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 21223,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19457,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Dec 01 07:09:33 +0000 2017",
+			"id": 936492447680442400,
+			"id_str": "936492447680442368",
+			"text": "Younger students getting very excited by pen-enabled devices at the Xmas fair @OneNoteC @swaps06 https://t.co/7KxEJUXIOa",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "OneNoteC",
+						"name": "OneNote Central",
+						"id": 2582768738,
+						"id_str": "2582768738",
+						"indices": [
+							78,
+							87
+						]
+					},
+					{
+						"screen_name": "swaps06",
+						"name": "Swapna",
+						"id": 808830001520877600,
+						"id_str": "808830001520877568",
+						"indices": [
+							88,
+							96
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 936492436787830800,
+						"id_str": "936492436787830784",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+						"url": "https://t.co/7KxEJUXIOa",
+						"display_url": "pic.twitter.com/7KxEJUXIOa",
+						"expanded_url": "https://twitter.com/stevesingapore/status/936492447680442368/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1600,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 936492436787830800,
+						"id_str": "936492436787830784",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DP8W6NWUIAAitPC.jpg",
+						"url": "https://t.co/7KxEJUXIOa",
+						"display_url": "pic.twitter.com/7KxEJUXIOa",
+						"expanded_url": "https://twitter.com/stevesingapore/status/936492447680442368/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1600,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 936492436787884000,
+						"id_str": "936492436787884032",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/DP8W6NWU8AASjAJ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DP8W6NWU8AASjAJ.jpg",
+						"url": "https://t.co/7KxEJUXIOa",
+						"display_url": "pic.twitter.com/7KxEJUXIOa",
+						"expanded_url": "https://twitter.com/stevesingapore/status/936492447680442368/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1600,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 200186236,
+				"id_str": "200186236",
+				"name": "Steve Morgan",
+				"screen_name": "stevesingapore",
+				"location": "Singapore",
+				"description": "Head of Tech Integration. French/Spanish teacher. Microsoft Trainer. Apple Foundations Trainer. Flipgrid ambassador. Golf Coach.",
+				"url": "https://t.co/RafoZoKsYQ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/RafoZoKsYQ",
+								"expanded_url": "http://www.stevesingapore.com",
+								"display_url": "stevesingapore.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 941,
+				"friends_count": 807,
+				"listed_count": 54,
+				"created_at": "Fri Oct 08 17:17:31 +0000 2010",
+				"favourites_count": 2421,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2914,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/841292685679906816/LGU-wIIN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/841292685679906816/LGU-wIIN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/200186236/1425900016",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
