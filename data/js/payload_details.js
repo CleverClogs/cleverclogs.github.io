32914c7ad7f143790c2df4114b0aec1b4d054351
CleverClogs <github@cleverclogs.org>
2017-12-20 18:19:35 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19880,
-	"created_at": "Wed Dec 20 11:19:36 +0000 2017",
+	"tweets": 19881,
+	"created_at": "Wed Dec 20 17:19:34 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Wed Dec 20 17:08:12 +0000 2017",
+		"id": 943528473867571200,
+		"id_str": "943528473867571201",
+		"text": "The #LDChat about #accessibility and #dyslexia has just started.\nwith @ATDyslexia and @UnderstoodOrg\n\n#AssistiveTech https://t.co/BR0myVlYbp",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LDChat",
+					"indices": [
+						4,
+						11
+					]
+				},
+				{
+					"text": "accessibility",
+					"indices": [
+						18,
+						32
+					]
+				},
+				{
+					"text": "dyslexia",
+					"indices": [
+						37,
+						46
+					]
+				},
+				{
+					"text": "AssistiveTech",
+					"indices": [
+						102,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ATDyslexia",
+					"name": "Jamie Martin ⌨",
+					"id": 149224105,
+					"id_str": "149224105",
+					"indices": [
+						70,
+						81
+					]
+				},
+				{
+					"screen_name": "UnderstoodOrg",
+					"name": "Understood",
+					"id": 43344979,
+					"id_str": "43344979",
+					"indices": [
+						86,
+						100
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BR0myVlYbp",
+					"expanded_url": "https://twitter.com/UnderstoodOrg/status/943526515727327234",
+					"display_url": "twitter.com/UnderstoodOrg/…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14444,
+			"friends_count": 5322,
+			"listed_count": 543,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 21517,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19743,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 943526515727327200,
+		"quoted_status_id_str": "943526515727327234",
+		"quoted_status": {
+			"created_at": "Wed Dec 20 17:00:25 +0000 2017",
+			"id": 943526515727327200,
+			"id_str": "943526515727327234",
+			"text": "LIVE NOW \nWelcome to today’s #ldchat! This is Dan with the Understood team. We’re being joined today by… https://t.co/J3WlFWCbdA",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "ldchat",
+						"indices": [
+							29,
+							36
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/J3WlFWCbdA",
+						"expanded_url": "https://twitter.com/i/web/status/943526515727327234",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							105,
+							128
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 43344979,
+				"id_str": "43344979",
+				"name": "Understood",
+				"screen_name": "UnderstoodOrg",
+				"location": "",
+				"description": "Understood is a free, online resource for parents of children with learning and attention issues. Join #LDchat every Wednesday at 12pm ET.",
+				"url": "https://t.co/jIlAF85oMV",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jIlAF85oMV",
+								"expanded_url": "http://www.understood.org",
+								"display_url": "understood.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 32187,
+				"friends_count": 2978,
+				"listed_count": 697,
+				"created_at": "Fri May 29 15:01:18 +0000 2009",
+				"favourites_count": 8398,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 26850,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F4F2F1",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/514880467087028224/8d7UK5oC.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/514880467087028224/8d7UK5oC.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/514777983806996482/gCY4NlHL_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/514777983806996482/gCY4NlHL_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/43344979/1510931786",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "2D434A",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Dec 20 10:28:42 +0000 2017",
 		"id": 943427935607943200,
 		"id_str": "943427935607943168",
