90af3c3c76cadc56fadb60a793759e874a27044e
CleverClogs <github@cleverclogs.org>
2017-12-28 18:19:35 +0100

    Added 1 tweets

--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Thu Dec 28 17:05:35 +0000 2017",
+		"id": 946426916323713000,
+		"id_str": "946426916323713024",
+		"text": "RT @danmillest: This last year I’ve been doing all my Bible reading and study in OneNote. It’s one of the best tools I’ve found to help me…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "danmillest",
+					"name": "Dan Millest",
+					"id": 18227752,
+					"id_str": "18227752",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant and #MSFTEduChat coordinator.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14509,
+			"friends_count": 5338,
+			"listed_count": 542,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 21583,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19809,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1500128051",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Dec 28 15:40:30 +0000 2017",
+			"id": 946405503315230700,
+			"id_str": "946405503315230722",
+			"text": "This last year I’ve been doing all my Bible reading and study in OneNote. It’s one of the best tools I’ve found to… https://t.co/ILbSSFNk2s",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ILbSSFNk2s",
+						"expanded_url": "https://twitter.com/i/web/status/946405503315230722",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							116,
+							139
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18227752,
+				"id_str": "18227752",
+				"name": "Dan Millest",
+				"screen_name": "danmillest",
+				"location": "Kuala Lumpur",
+				"description": "⠀ ⠀ ⠀ ⠀ ⠀⠀⠀⠀⠀∳ ♫̅͟♪̅͟♩̅͟♪̅͟♩̅͟♪̅͟♩̅͟♬̅͟ ⠀⠀⠀⠀♩̅͟♪̅͟♩̅͟♪̅͟♬̅͟♪̅͟♩̅͟♪̅͟♬̅͟ ⠀⠀⠀♪̅͟♩̅͟♪̅͟♬̅͟♪̅͟♬̅͟♬̅͟♩̅͟♪̅͟ ⠀⠀♩̅͟♪̅͟♩̅͟♪̅͟♪̅͟♩̅͟♪̅͟♬̅͟♮  ⠀ ⠀ ⠀ ⠀",
+				"url": "https://t.co/pNawWbhI1n",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pNawWbhI1n",
+								"expanded_url": "http://www.danmillest.com",
+								"display_url": "danmillest.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 980,
+				"friends_count": 825,
+				"listed_count": 8,
+				"created_at": "Thu Dec 18 22:44:09 +0000 2008",
+				"favourites_count": 2587,
+				"utc_offset": -36000,
+				"time_zone": "Hawaii",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4837,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/581323008641404928/6zMpMtQq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/581323008641404928/6zMpMtQq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18227752/1437767354",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "7b02fbddf4d9f2c6",
+				"url": "https://api.twitter.com/1.1/geo/id/7b02fbddf4d9f2c6.json",
+				"place_type": "city",
+				"name": "Kuala Lumpur City",
+				"full_name": "Kuala Lumpur City, Kuala Lumpur Federal Territory",
+				"country_code": "MY",
+				"country": "Malaysia",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								101.6682318,
+								3.1049064
+							],
+							[
+								101.7423782,
+								3.1049064
+							],
+							[
+								101.7423782,
+								3.1921549
+							],
+							[
+								101.6682318,
+								3.1921549
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 1,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Dec 28 14:55:01 +0000 2017",
 		"id": 946394058741645300,
 		"id_str": "946394058741645313",
