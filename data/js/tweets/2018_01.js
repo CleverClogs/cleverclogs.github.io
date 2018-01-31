ffa14109ede395361706a7b81b18246ac29ec3ef
CleverClogs <github@cleverclogs.org>
2018-01-31 19:19:36 +0100

    Added 2 tweets

--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,681 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Wed Jan 31 17:59:52 +0000 2018",
+		"id": 958761765369729000,
+		"id_str": "958761765369729024",
+		"text": "RT @libraryladySBH: OneNote gets me.  #MicrosoftEdu #Auburn #AJHSonDean #unicorns https://t.co/630SRW79lO",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftEdu",
+					"indices": [
+						38,
+						51
+					]
+				},
+				{
+					"text": "Auburn",
+					"indices": [
+						52,
+						59
+					]
+				},
+				{
+					"text": "AJHSonDean",
+					"indices": [
+						60,
+						71
+					]
+				},
+				{
+					"text": "unicorns",
+					"indices": [
+						72,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "libraryladySBH",
+					"name": "Sarah Hillyer",
+					"id": 917911732504449000,
+					"id_str": "917911732504449025",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 958755905184718800,
+					"id_str": "958755905184718854",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+					"url": "https://t.co/630SRW79lO",
+					"display_url": "pic.twitter.com/630SRW79lO",
+					"expanded_url": "https://twitter.com/libraryladySBH/status/958755917616599045/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 958755917616599000,
+					"source_status_id_str": "958755917616599045",
+					"source_user_id": 917911732504449000,
+					"source_user_id_str": "917911732504449025"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 958755905184718800,
+					"id_str": "958755905184718854",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+					"url": "https://t.co/630SRW79lO",
+					"display_url": "pic.twitter.com/630SRW79lO",
+					"expanded_url": "https://twitter.com/libraryladySBH/status/958755917616599045/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 958755917616599000,
+					"source_status_id_str": "958755917616599045",
+					"source_user_id": 917911732504449000,
+					"source_user_id_str": "917911732504449025"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14977,
+			"friends_count": 5439,
+			"listed_count": 551,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 22221,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 20335,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1516193102",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jan 31 17:36:38 +0000 2018",
+			"id": 958755917616599000,
+			"id_str": "958755917616599045",
+			"text": "OneNote gets me.  #MicrosoftEdu #Auburn #AJHSonDean #unicorns https://t.co/630SRW79lO",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MicrosoftEdu",
+						"indices": [
+							18,
+							31
+						]
+					},
+					{
+						"text": "Auburn",
+						"indices": [
+							32,
+							39
+						]
+					},
+					{
+						"text": "AJHSonDean",
+						"indices": [
+							40,
+							51
+						]
+					},
+					{
+						"text": "unicorns",
+						"indices": [
+							52,
+							61
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 958755905184718800,
+						"id_str": "958755905184718854",
+						"indices": [
+							62,
+							85
+						],
+						"media_url": "http://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+						"url": "https://t.co/630SRW79lO",
+						"display_url": "pic.twitter.com/630SRW79lO",
+						"expanded_url": "https://twitter.com/libraryladySBH/status/958755917616599045/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 958755905184718800,
+						"id_str": "958755905184718854",
+						"indices": [
+							62,
+							85
+						],
+						"media_url": "http://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DU4vaoLX4AYAXdI.jpg",
+						"url": "https://t.co/630SRW79lO",
+						"display_url": "pic.twitter.com/630SRW79lO",
+						"expanded_url": "https://twitter.com/libraryladySBH/status/958755917616599045/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 917911732504449000,
+				"id_str": "917911732504449025",
+				"name": "Sarah Hillyer",
+				"screen_name": "libraryladySBH",
+				"location": "Auburn, AL",
+				"description": "Coffee, books, and cats. Teacher Librarian at AJHS.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 48,
+				"friends_count": 56,
+				"listed_count": 2,
+				"created_at": "Wed Oct 11 00:36:25 +0000 2017",
+				"favourites_count": 39,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 17,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/917915187180720129/PNMrlVNE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/917915187180720129/PNMrlVNE_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Wed Jan 31 17:59:11 +0000 2018",
+		"id": 958761593688547300,
+		"id_str": "958761593688547328",
+		"text": "RT @YakubovMath: We are #OBSESSED with #OneNote thanks to @sdtitmas 🙌🏼 Our digital math notebooks really ADD up!  #MicrosoftTeams #EdTechOB…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OBSESSED",
+					"indices": [
+						24,
+						33
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						39,
+						47
+					]
+				},
+				{
+					"text": "MicrosoftTeams",
+					"indices": [
+						114,
+						129
+					]
+				},
+				{
+					"text": "EdTechOB",
+					"indices": [
+						130,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YakubovMath",
+					"name": "Mrs. y=akubov 💜✏️",
+					"id": 2201978718,
+					"id_str": "2201978718",
+					"indices": [
+						3,
+						15
+					]
+				},
+				{
+					"screen_name": "sdtitmas",
+					"name": "Scott Titmas",
+					"id": 2341899519,
+					"id_str": "2341899519",
+					"indices": [
+						58,
+						67
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14977,
+			"friends_count": 5439,
+			"listed_count": 551,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 22221,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 20335,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1516193102",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jan 31 17:25:25 +0000 2018",
+			"id": 958753094019571700,
+			"id_str": "958753094019571712",
+			"text": "We are #OBSESSED with #OneNote thanks to @sdtitmas 🙌🏼 Our digital math notebooks really ADD up!  #MicrosoftTeams… https://t.co/JCXKtcysIq",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OBSESSED",
+						"indices": [
+							7,
+							16
+						]
+					},
+					{
+						"text": "OneNote",
+						"indices": [
+							22,
+							30
+						]
+					},
+					{
+						"text": "MicrosoftTeams",
+						"indices": [
+							97,
+							112
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "sdtitmas",
+						"name": "Scott Titmas",
+						"id": 2341899519,
+						"id_str": "2341899519",
+						"indices": [
+							41,
+							50
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/JCXKtcysIq",
+						"expanded_url": "https://twitter.com/i/web/status/958753094019571712",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							114,
+							137
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2201978718,
+				"id_str": "2201978718",
+				"name": "Mrs. y=akubov 💜✏️",
+				"screen_name": "YakubovMath",
+				"location": "Old Bridge, NJ",
+				"description": "OBHS GNC Algebra Teacher, living the #mathlife",
+				"url": "https://t.co/JML4UR6tmL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JML4UR6tmL",
+								"expanded_url": "http://www.oldbridgeadmin.org/webpages/ryakubov",
+								"display_url": "oldbridgeadmin.org/webpages/ryaku…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 248,
+				"friends_count": 35,
+				"listed_count": 1,
+				"created_at": "Mon Nov 18 21:04:06 +0000 2013",
+				"favourites_count": 976,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1295,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/678224271034961920/U1y_JnUJ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/678224271034961920/U1y_JnUJ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2201978718/1470612940",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 7,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jan 31 15:47:45 +0000 2018",
 		"id": 958728519214878700,
 		"id_str": "958728519214878720",
