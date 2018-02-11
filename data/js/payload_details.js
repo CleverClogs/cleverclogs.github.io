c4dc310ac4cbd711d8b4745f7e4882a803afd610
CleverClogs <github@cleverclogs.org>
2018-02-11 15:19:41 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20762,
-	"created_at": "Sun Feb 11 13:19:30 +0000 2018",
+	"tweets": 20763,
+	"created_at": "Sun Feb 11 14:19:35 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Sun Feb 11 13:49:27 +0000 2018",
+		"id": 962685014629548000,
+		"id_str": "962685014629548032",
+		"text": "This incredible Gijinka parody art work is going viral right now, and for good reason. Be amazed by this Microsoft… https://t.co/uck57ma7ds",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uck57ma7ds",
+					"expanded_url": "https://twitter.com/i/web/status/962685014629548032",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15128,
+			"friends_count": 5488,
+			"listed_count": 553,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 22510,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 20611,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1516193102",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 962415860269793300,
+		"quoted_status_id_str": "962415860269793280",
+		"quoted_status": {
+			"created_at": "Sat Feb 10 19:59:56 +0000 2018",
+			"id": 962415860269793300,
+			"id_str": "962415860269793280",
+			"text": "Microsoft Office Danshi Anime\n[Gijinka parody]\nGood Day! 😂 https://t.co/GFSAN45YhJ",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 962414709784760300,
+						"id_str": "962414709784760320",
+						"indices": [
+							59,
+							82
+						],
+						"media_url": "http://pbs.twimg.com/media/DVsvFFdU0AAcQWn.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVsvFFdU0AAcQWn.jpg",
+						"url": "https://t.co/GFSAN45YhJ",
+						"display_url": "pic.twitter.com/GFSAN45YhJ",
+						"expanded_url": "https://twitter.com/Rosel__d/status/962415860269793280/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1101,
+								"h": 1500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 499,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 881,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 962414709784760300,
+						"id_str": "962414709784760320",
+						"indices": [
+							59,
+							82
+						],
+						"media_url": "http://pbs.twimg.com/media/DVsvFFdU0AAcQWn.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DVsvFFdU0AAcQWn.jpg",
+						"url": "https://t.co/GFSAN45YhJ",
+						"display_url": "pic.twitter.com/GFSAN45YhJ",
+						"expanded_url": "https://twitter.com/Rosel__d/status/962415860269793280/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1101,
+								"h": 1500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 499,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 881,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2984401412,
+				"id_str": "2984401412",
+				"name": "Cioccolatodorima",
+				"screen_name": "Rosel__d",
+				"location": "Outer Space, Asia, Underworld",
+				"description": "Hi I'm Jin .  Pizza Alien .  ◘Cioccolatodorima on YT ◘ Pixiv: https://t.co/i6dklywPBI◘FB:https://t.co/jARtUMrxKD",
+				"url": "https://t.co/Rk3Hhl0tzC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Rk3Hhl0tzC",
+								"expanded_url": "https://www.patreon.com/cioccolatodorima",
+								"display_url": "patreon.com/cioccolatodori…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/i6dklywPBI",
+								"expanded_url": "https://pixiv.me/427_427",
+								"display_url": "pixiv.me/427_427",
+								"indices": [
+									62,
+									85
+								]
+							},
+							{
+								"url": "https://t.co/jARtUMrxKD",
+								"expanded_url": "https://web.facebook.com/cioccolatodorima/",
+								"display_url": "web.facebook.com/cioccolatodori…",
+								"indices": [
+									89,
+									112
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 19023,
+				"friends_count": 306,
+				"listed_count": 138,
+				"created_at": "Thu Jan 15 17:18:07 +0000 2015",
+				"favourites_count": 3011,
+				"utc_offset": 28800,
+				"time_zone": "Taipei",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3788,
+				"lang": "en-gb",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/643073839782428677/QgZRhAIk.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/643073839782428677/QgZRhAIk.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/962421173635096576/90r4HECS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/962421173635096576/90r4HECS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2984401412/1513245509",
+				"profile_link_color": "00C9FF",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1909,
+			"favorite_count": 3477,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Feb 11 12:50:04 +0000 2018",
 		"id": 962670068315443200,
 		"id_str": "962670068315443202",
