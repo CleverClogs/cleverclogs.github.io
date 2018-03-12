b2eabafcaa2cd90f6422df2330d77291d5370a60
CleverClogs <github@cleverclogs.org>
2018-03-12 22:19:32 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21326,
-	"created_at": "Mon Mar 12 20:19:32 +0000 2018",
+	"tweets": 21327,
+	"created_at": "Mon Mar 12 21:19:31 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Mon Mar 12 20:27:00 +0000 2018",
+		"id": 973294306562895900,
+		"id_str": "973294306562895872",
+		"text": "@gaberivera @brianmcc @jpschwinghamer @Techmeme @TechmemePodcast Hi Gabe, long time;-)\nAwesome. I've just added the… https://t.co/OTyePiypor",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gaberivera",
+					"name": "Gabe Rivera",
+					"id": 817288,
+					"id_str": "817288",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "brianmcc",
+					"name": "Brian McCullough",
+					"id": 1502,
+					"id_str": "1502",
+					"indices": [
+						12,
+						21
+					]
+				},
+				{
+					"screen_name": "jpschwinghamer",
+					"name": "Justin Schwinghamer",
+					"id": 21785659,
+					"id_str": "21785659",
+					"indices": [
+						22,
+						37
+					]
+				},
+				{
+					"screen_name": "Techmeme",
+					"name": "Techmeme",
+					"id": 817386,
+					"id_str": "817386",
+					"indices": [
+						38,
+						47
+					]
+				},
+				{
+					"screen_name": "TechmemePodcast",
+					"name": "Techmeme Ride Home",
+					"id": 971949394797850600,
+					"id_str": "971949394797850625",
+					"indices": [
+						48,
+						64
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OTyePiypor",
+					"expanded_url": "https://twitter.com/i/web/status/973294306562895872",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 973290581530046500,
+		"in_reply_to_status_id_str": "973290581530046464",
+		"in_reply_to_user_id": 817288,
+		"in_reply_to_user_id_str": "817288",
+		"in_reply_to_screen_name": "gaberivera",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15479,
+			"friends_count": 5608,
+			"listed_count": 559,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 22958,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21168,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1520577565",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Mar 12 19:26:47 +0000 2018",
 		"id": 973279152060198900,
 		"id_str": "973279152060198912",
