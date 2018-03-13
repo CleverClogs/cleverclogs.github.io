ce90b3b640d9ed01d921792dd166250448f39657
CleverClogs <github@cleverclogs.org>
2018-03-13 22:19:32 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21354,
-	"created_at": "Tue Mar 13 20:19:37 +0000 2018",
+	"tweets": 21355,
+	"created_at": "Tue Mar 13 21:19:31 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Tue Mar 13 20:35:59 +0000 2018",
+		"id": 973658956869627900,
+		"id_str": "973658956869627904",
+		"text": "Awesome infographic by the Science and Technology Facilities Council UK, quite fitting in the light of Microsoft Ed… https://t.co/lYgVr3pOiN",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lYgVr3pOiN",
+					"expanded_url": "https://twitter.com/i/web/status/973658956869627904",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15495,
+			"friends_count": 5617,
+			"listed_count": 558,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 22986,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21196,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1520577565",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 971656982070472700,
+		"quoted_status_id_str": "971656982070472705",
+		"quoted_status": {
+			"created_at": "Thu Mar 08 08:00:51 +0000 2018",
+			"id": 971656982070472700,
+			"id_str": "971656982070472705",
+			"text": "It’s International Women’s Day (#IWD2018)! Take a look at our infographic highlighting a brief history of women in… https://t.co/9k1MuKFzAS",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "IWD2018",
+						"indices": [
+							32,
+							40
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/9k1MuKFzAS",
+						"expanded_url": "https://twitter.com/i/web/status/971656982070472705",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							116,
+							139
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.socialsignin.co.uk\" rel=\"nofollow\">SocialSignIn Application</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 74697858,
+				"id_str": "74697858",
+				"name": "STFC",
+				"screen_name": "STFC_Matters",
+				"location": "UK",
+				"description": "Science and Technology Facilities Council: Pioneering science for a better future. \nSee @STFC_B2B for business news.",
+				"url": "http://t.co/J9rqV1RF67",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/J9rqV1RF67",
+								"expanded_url": "http://www.stfc.ac.uk",
+								"display_url": "stfc.ac.uk",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16477,
+				"friends_count": 980,
+				"listed_count": 605,
+				"created_at": "Wed Sep 16 10:13:05 +0000 2009",
+				"favourites_count": 2564,
+				"utc_offset": 0,
+				"time_zone": "London",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 11784,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0B0B61",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000066008120/28d32c5c83e2171b01a89b45246df2f0.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000066008120/28d32c5c83e2171b01a89b45246df2f0.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/462241833884467201/MZK5zqcr_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/462241833884467201/MZK5zqcr_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/74697858/1515430551",
+				"profile_link_color": "0B2161",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "7AC3EE",
+				"profile_text_color": "3D1957",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 122,
+			"favorite_count": 125,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Mar 13 19:45:46 +0000 2018",
 		"id": 973646317695193100,
 		"id_str": "973646317695193088",
