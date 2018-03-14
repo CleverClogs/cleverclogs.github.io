063c177bae1b665e3d375c8f8cefbab1f8b047a4
CleverClogs <github@cleverclogs.org>
2018-03-14 06:19:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21359,
-	"created_at": "Tue Mar 13 23:19:30 +0000 2018",
+	"tweets": 21360,
+	"created_at": "Wed Mar 14 05:19:35 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,456 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Wed Mar 14 05:18:59 +0000 2018",
+		"id": 973790574254977000,
+		"id_str": "973790574254977025",
+		"text": "RT @mtholfsen: “Hack the Brain” with @APappaILD at #E2 #mieexpert #dyslexia #AssistiveTech #MicrosoftEdu https://t.co/yRFsUNlBwc",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "E2",
+					"indices": [
+						51,
+						54
+					]
+				},
+				{
+					"text": "mieexpert",
+					"indices": [
+						55,
+						65
+					]
+				},
+				{
+					"text": "dyslexia",
+					"indices": [
+						66,
+						75
+					]
+				},
+				{
+					"text": "AssistiveTech",
+					"indices": [
+						76,
+						90
+					]
+				},
+				{
+					"text": "MicrosoftEdu",
+					"indices": [
+						91,
+						104
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "APappaILD",
+					"name": "Aggeliki Pappa",
+					"id": 322836984,
+					"id_str": "322836984",
+					"indices": [
+						37,
+						47
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 973746797267923000,
+					"id_str": "973746797267922944",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+					"url": "https://t.co/yRFsUNlBwc",
+					"display_url": "pic.twitter.com/yRFsUNlBwc",
+					"expanded_url": "https://twitter.com/mtholfsen/status/973746820789682177/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1432,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 839,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 475,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973746820789682200,
+					"source_status_id_str": "973746820789682177",
+					"source_user_id": 27673327,
+					"source_user_id_str": "27673327"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 973746797267923000,
+					"id_str": "973746797267922944",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+					"url": "https://t.co/yRFsUNlBwc",
+					"display_url": "pic.twitter.com/yRFsUNlBwc",
+					"expanded_url": "https://twitter.com/mtholfsen/status/973746820789682177/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1432,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 839,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 475,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973746820789682200,
+					"source_status_id_str": "973746820789682177",
+					"source_user_id": 27673327,
+					"source_user_id_str": "27673327"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15499,
+			"friends_count": 5617,
+			"listed_count": 559,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 22988,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21201,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1520577565",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Mar 14 02:25:08 +0000 2018",
+			"id": 973746820789682200,
+			"id_str": "973746820789682177",
+			"text": "“Hack the Brain” with @APappaILD at #E2 #mieexpert #dyslexia #AssistiveTech #MicrosoftEdu https://t.co/yRFsUNlBwc",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "E2",
+						"indices": [
+							36,
+							39
+						]
+					},
+					{
+						"text": "mieexpert",
+						"indices": [
+							40,
+							50
+						]
+					},
+					{
+						"text": "dyslexia",
+						"indices": [
+							51,
+							60
+						]
+					},
+					{
+						"text": "AssistiveTech",
+						"indices": [
+							61,
+							75
+						]
+					},
+					{
+						"text": "MicrosoftEdu",
+						"indices": [
+							76,
+							89
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "APappaILD",
+						"name": "Aggeliki Pappa",
+						"id": 322836984,
+						"id_str": "322836984",
+						"indices": [
+							22,
+							32
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 973746797267923000,
+						"id_str": "973746797267922944",
+						"indices": [
+							90,
+							113
+						],
+						"media_url": "http://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+						"url": "https://t.co/yRFsUNlBwc",
+						"display_url": "pic.twitter.com/yRFsUNlBwc",
+						"expanded_url": "https://twitter.com/mtholfsen/status/973746820789682177/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1432,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 839,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 475,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 973746797267923000,
+						"id_str": "973746797267922944",
+						"indices": [
+							90,
+							113
+						],
+						"media_url": "http://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYNxjZBUMAAehpZ.jpg",
+						"url": "https://t.co/yRFsUNlBwc",
+						"display_url": "pic.twitter.com/yRFsUNlBwc",
+						"expanded_url": "https://twitter.com/mtholfsen/status/973746820789682177/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1432,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 839,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 475,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 27673327,
+				"id_str": "27673327",
+				"name": "Mike Tholfsen",
+				"screen_name": "mtholfsen",
+				"location": "Redmond, WA",
+				"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, Learning Tools & #MicrosoftTeams #edtech #E2  @OneNoteEDU @MicrosoftEDU",
+				"url": "https://t.co/ANIFpSE2FB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ANIFpSE2FB",
+								"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+								"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11311,
+				"friends_count": 2524,
+				"listed_count": 656,
+				"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+				"favourites_count": 27880,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 19786,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+				"profile_link_color": "9266CC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Mar 13 22:26:15 +0000 2018",
 		"id": 973686706816905200,
 		"id_str": "973686706816905217",
