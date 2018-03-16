caa0ccb7fb5f55f771ab475aad9bc0ae27cb7657
CleverClogs <github@cleverclogs.org>
2018-03-16 13:19:32 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21409,
-	"created_at": "Thu Mar 15 23:19:32 +0000 2018",
+	"tweets": 21410,
+	"created_at": "Fri Mar 16 12:19:31 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Fri Mar 16 12:18:52 +0000 2018",
+		"id": 974621016541589500,
+		"id_str": "974621016541589504",
+		"text": "@MicrosoftAT @LadyNaggaga @WeAreDevs @MicrosoftAT followers may be interested to participate in the #MSFTEduChat Tw… https://t.co/wnLPZa32kC",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						100,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MicrosoftAT",
+					"name": "Microsoft Österreich",
+					"id": 69602617,
+					"id_str": "69602617",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "LadyNaggaga",
+					"name": "Maria Naggaga",
+					"id": 863095946,
+					"id_str": "863095946",
+					"indices": [
+						13,
+						25
+					]
+				},
+				{
+					"screen_name": "WeAreDevs",
+					"name": "WeAreDevelopers",
+					"id": 3318642904,
+					"id_str": "3318642904",
+					"indices": [
+						26,
+						36
+					]
+				},
+				{
+					"screen_name": "MicrosoftAT",
+					"name": "Microsoft Österreich",
+					"id": 69602617,
+					"id_str": "69602617",
+					"indices": [
+						37,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wnLPZa32kC",
+					"expanded_url": "https://twitter.com/i/web/status/974621016541589504",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 971790653486256100,
+		"in_reply_to_status_id_str": "971790653486256129",
+		"in_reply_to_user_id": 69602617,
+		"in_reply_to_user_id_str": "69602617",
+		"in_reply_to_screen_name": "MicrosoftAT",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15524,
+			"friends_count": 5641,
+			"listed_count": 560,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 23067,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21250,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1520577565",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Mar 15 22:20:58 +0000 2018",
 		"id": 974410153805205500,
 		"id_str": "974410153805205504",
