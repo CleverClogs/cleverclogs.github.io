2f680d3e88a1aa30fbe2781c1aaae4d53402a0e3
CleverClogs <github@cleverclogs.org>
2018-03-18 12:19:32 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21445,
-	"created_at": "Sat Mar 17 22:19:31 +0000 2018",
+	"tweets": 21446,
+	"created_at": "Sun Mar 18 11:19:31 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Sun Mar 18 11:10:47 +0000 2018",
+		"id": 975328658398249000,
+		"id_str": "975328658398248960",
+		"text": "RT @TeacherPrize: The school bell has gone, and it's time for a lesson. Top 10 #TeacherPrize Finalist @zelfstudie talks through his teachin…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TeacherPrize",
+					"indices": [
+						79,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeacherPrize",
+					"name": "Global Teacher Prize",
+					"id": 2345263466,
+					"id_str": "2345263466",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "zelfstudie",
+					"name": "Koen Timmers",
+					"id": 149593681,
+					"id_str": "149593681",
+					"indices": [
+						102,
+						113
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15554,
+			"friends_count": 5648,
+			"listed_count": 561,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 23103,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21286,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1520577565",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Mar 18 11:00:07 +0000 2018",
+			"id": 975325974769426400,
+			"id_str": "975325974769426433",
+			"text": "The school bell has gone, and it's time for a lesson. Top 10 #TeacherPrize Finalist @zelfstudie talks through his t… https://t.co/23p6KE8XAX",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "TeacherPrize",
+						"indices": [
+							61,
+							74
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "zelfstudie",
+						"name": "Koen Timmers",
+						"id": 149593681,
+						"id_str": "149593681",
+						"indices": [
+							84,
+							95
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/23p6KE8XAX",
+						"expanded_url": "https://twitter.com/i/web/status/975325974769426433",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2345263466,
+				"id_str": "2345263466",
+				"name": "Global Teacher Prize",
+				"screen_name": "TeacherPrize",
+				"location": "London, UK",
+				"description": "The @VarkeyFdn Global Teacher Prize is a $1million award to recognise exceptional teaching! #TeacherPrize IG: https://t.co/Rvjy1U859H",
+				"url": "https://t.co/CMl42CPnQo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/CMl42CPnQo",
+								"expanded_url": "http://www.globalteacherprize.org",
+								"display_url": "globalteacherprize.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/Rvjy1U859H",
+								"expanded_url": "http://instagram.com/teacherprize",
+								"display_url": "instagram.com/teacherprize",
+								"indices": [
+									110,
+									133
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 22181,
+				"friends_count": 3234,
+				"listed_count": 375,
+				"created_at": "Sat Feb 15 15:04:26 +0000 2014",
+				"favourites_count": 28906,
+				"utc_offset": 0,
+				"time_zone": "London",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 16330,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "091F44",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/451851160547119104/A_2kIX5r.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/451851160547119104/A_2kIX5r.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/441671450743693312/_Mqd36OK_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/441671450743693312/_Mqd36OK_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2345263466/1518566640",
+				"profile_link_color": "03C9FA",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Mar 17 21:19:47 +0000 2018",
 		"id": 975119531269029900,
 		"id_str": "975119531269029888",
