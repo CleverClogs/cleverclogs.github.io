f5338670f1f096c9baa30e4f9c129c90468e04b5
CleverClogs <github@cleverclogs.org>
2018-03-24 17:19:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21575,
-	"created_at": "Sat Mar 24 15:19:33 +0000 2018",
+	"tweets": 21576,
+	"created_at": "Sat Mar 24 16:19:34 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,273 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Sat Mar 24 15:54:04 +0000 2018",
+		"id": 977574275786924000,
+		"id_str": "977574275786924032",
+		"text": "ROFL.\nThat almost makes me want to switch to every supported #OneNote client language just to figure out if there a… https://t.co/eQLeOnuwdy",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						61,
+						69
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eQLeOnuwdy",
+					"expanded_url": "https://twitter.com/i/web/status/977574275786924032",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15609,
+			"friends_count": 5666,
+			"listed_count": 558,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 23211,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21413,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1521738605",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 977558915868176400,
+		"quoted_status_id_str": "977558915868176384",
+		"quoted_status": {
+			"created_at": "Sat Mar 24 14:53:02 +0000 2018",
+			"id": 977558915868176400,
+			"id_str": "977558915868176384",
+			"text": "Whoa microsoft onenote really hates me https://t.co/vHZ44I2oZE",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 977558791616155600,
+						"id_str": "977558791616155648",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/DZD8iqlXUAAM3lC.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DZD8iqlXUAAM3lC.jpg",
+						"url": "https://t.co/vHZ44I2oZE",
+						"display_url": "pic.twitter.com/vHZ44I2oZE",
+						"expanded_url": "https://twitter.com/Kirinodere/status/977558915868176384/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 173,
+								"h": 62,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 62,
+								"h": 62,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 173,
+								"h": 62,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 173,
+								"h": 62,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 977558791616155600,
+						"id_str": "977558791616155648",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/DZD8iqlXUAAM3lC.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DZD8iqlXUAAM3lC.jpg",
+						"url": "https://t.co/vHZ44I2oZE",
+						"display_url": "pic.twitter.com/vHZ44I2oZE",
+						"expanded_url": "https://twitter.com/Kirinodere/status/977558915868176384/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 173,
+								"h": 62,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 62,
+								"h": 62,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 173,
+								"h": 62,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 173,
+								"h": 62,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 939351600,
+				"id_str": "939351600",
+				"name": "Kiririn",
+				"screen_name": "Kirinodere",
+				"location": "Inside my closet",
+				"description": "Cute online persona or something. I probably watch too much anime and you guys are all hentai bakas!",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 33640,
+				"friends_count": 180,
+				"listed_count": 159,
+				"created_at": "Sat Nov 10 16:02:00 +0000 2012",
+				"favourites_count": 32209,
+				"utc_offset": 3600,
+				"time_zone": "Amsterdam",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 59145,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": true,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/938590748370337792/WhSmjQHk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/938590748370337792/WhSmjQHk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/939351600/1431282533",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "moderator"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 40,
+			"favorite_count": 177,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Mar 24 14:39:42 +0000 2018",
 		"id": 977555559246331900,
 		"id_str": "977555559246331909",
