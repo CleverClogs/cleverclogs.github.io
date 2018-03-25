898c47eff12f0bc5e6bc953a4049f1f9482001a3
CleverClogs <github@cleverclogs.org>
2018-03-26 00:19:35 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21588,
-	"created_at": "Sun Mar 25 21:19:34 +0000 2018",
+	"tweets": 21589,
+	"created_at": "Sun Mar 25 22:19:34 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Sun Mar 25 21:23:26 +0000 2018",
+		"id": 978019549894336500,
+		"id_str": "978019549894336514",
+		"text": "@hitechgazette Time for an updated review, team? The user interface of the #OneNote app for #Windows10 has been upd… https://t.co/uE3vGajoXV",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						75,
+						83
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						92,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hitechgazette",
+					"name": "HiTechGazette",
+					"id": 823067654973857800,
+					"id_str": "823067654973857794",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uE3vGajoXV",
+					"expanded_url": "https://twitter.com/i/web/status/978019549894336514",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 977871453734559700,
+		"in_reply_to_status_id_str": "977871453734559745",
+		"in_reply_to_user_id": 823067654973857800,
+		"in_reply_to_user_id_str": "823067654973857794",
+		"in_reply_to_screen_name": "hitechgazette",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra – OneNote MVP, trainer, consultant, #MSFTEduChat coordinator",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15621,
+			"friends_count": 5664,
+			"listed_count": 558,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 23218,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21426,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1521738605",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Mar 25 21:14:33 +0000 2018",
 		"id": 978017317912305700,
 		"id_str": "978017317912305664",
@@ -328,7 +454,7 @@ Grailbird.data.tweets_2018_03 =
 				"name": "Sheila Fisher, Ed.S.",
 				"screen_name": "shefish52",
 				"location": "Georgia, USA",
-				"description": "Third grade teacher | loves to ride 🚴🏾‍♀️ | Microsoft Innovative Educator |@BookCreatorApp and @Seesaw Ambassador. Follow our class @friends_fisher",
+				"description": "Third grade teacher | loves to ride 🚴🏾♀️ | Microsoft Innovative Educator |@BookCreatorApp and @Seesaw Ambassador. Follow our class @friends_fisher",
 				"url": "https://t.co/rIGkfedOtf",
 				"entities": {
 					"url": {
