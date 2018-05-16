09440cc56dfe9a2a25d483246e3c39d376243a8f
CleverClogs <github@cleverclogs.org>
2018-05-16 16:19:33 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22575,
-	"created_at": "Wed May 16 13:19:32 +0000 2018",
+	"tweets": 22576,
+	"created_at": "Wed May 16 14:19:32 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_05.js
+++ data/js/tweets/2018_05.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Wed May 16 14:00:01 +0000 2018",
+		"id": 996752129850265600,
+		"id_str": "996752129850265600",
+		"text": "Another phenomenal example of #SketchNotes created in #OneNote for #Windows10. That Rainbow special ink speaks to t… https://t.co/sdEwWm4tSp",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SketchNotes",
+					"indices": [
+						30,
+						42
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						54,
+						62
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						67,
+						77
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sdEwWm4tSp",
+					"expanded_url": "https://twitter.com/i/web/status/996752129850265600",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16412,
+			"friends_count": 6046,
+			"listed_count": 563,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 24123,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 22384,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 996549384262238200,
+		"quoted_status_id_str": "996549384262238208",
+		"quoted_status": {
+			"created_at": "Wed May 16 00:34:22 +0000 2018",
+			"id": 996549384262238200,
+			"id_str": "996549384262238208",
+			"text": "Practicing sketchnoting while listening to @JeffSandersTV interview @SusanDavid_PhD on his podcast...such a fun way… https://t.co/vIsU3bRW7h",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "JeffSandersTV",
+						"name": "Jeff Sanders",
+						"id": 16878222,
+						"id_str": "16878222",
+						"indices": [
+							43,
+							57
+						]
+					},
+					{
+						"screen_name": "SusanDavid_PhD",
+						"name": "Susan A David, Ph.D.",
+						"id": 228628039,
+						"id_str": "228628039",
+						"indices": [
+							68,
+							83
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/vIsU3bRW7h",
+						"expanded_url": "https://twitter.com/i/web/status/996549384262238208",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2220740071,
+				"id_str": "2220740071",
+				"name": "Marianne McGah",
+				"screen_name": "mariannemcgah",
+				"location": "Seattle, WA",
+				"description": "Learner. Teacher. Leader. Optimist. Collaborator. Ed.D. Wife and mom. Catholic education supporter. #MIEExpert",
+				"url": "https://t.co/7bnMtV38ye",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7bnMtV38ye",
+								"expanded_url": "http://www.mariannemcgah.com",
+								"display_url": "mariannemcgah.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 107,
+				"friends_count": 239,
+				"listed_count": 12,
+				"created_at": "Fri Nov 29 03:24:30 +0000 2013",
+				"favourites_count": 1754,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 266,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/884100663927427072/HxjfCPVX_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/884100663927427072/HxjfCPVX_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2220740071/1407813926",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed May 16 13:13:06 +0000 2018",
 		"id": 996740324042174500,
 		"id_str": "996740324042174465",
