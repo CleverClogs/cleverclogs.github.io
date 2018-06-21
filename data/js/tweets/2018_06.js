c512c94bf78079b800008dd0a2fa31233c8c3285
CleverClogs <github@cleverclogs.org>
2018-06-21 07:19:33 +0200

    Added 1 tweets

--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Thu Jun 21 05:09:37 +0000 2018",
+		"id": 1009664612898361300,
+		"id_str": "1009664612898361344",
+		"text": "@bakerhhhs @MrDeissler @kristinaulmer @drs116 @ZipGrade @OneNoteEDU Hi Chris, curious what the context was of your… https://t.co/qKLOqp8QUF",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bakerhhhs",
+					"name": "Chris Baker",
+					"id": 344474523,
+					"id_str": "344474523",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "MrDeissler",
+					"name": "Mr. Deissler",
+					"id": 948183012,
+					"id_str": "948183012",
+					"indices": [
+						11,
+						22
+					]
+				},
+				{
+					"screen_name": "kristinaulmer",
+					"name": "𝙺𝚛𝚒𝚜𝚝𝚒𝚗𝚊 𝚄𝚕𝚖𝚎𝚛",
+					"id": 98403373,
+					"id_str": "98403373",
+					"indices": [
+						23,
+						37
+					]
+				},
+				{
+					"screen_name": "drs116",
+					"name": "Dennis Steinly",
+					"id": 51621678,
+					"id_str": "51621678",
+					"indices": [
+						38,
+						45
+					]
+				},
+				{
+					"screen_name": "ZipGrade",
+					"name": "ZipGrade",
+					"id": 1920762679,
+					"id_str": "1920762679",
+					"indices": [
+						46,
+						55
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						56,
+						67
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qKLOqp8QUF",
+					"expanded_url": "https://twitter.com/i/web/status/1009664612898361344",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1009638502043209700,
+		"in_reply_to_status_id_str": "1009638502043209728",
+		"in_reply_to_user_id": 344474523,
+		"in_reply_to_user_id_str": "344474523",
+		"in_reply_to_screen_name": "bakerhhhs",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16976,
+			"friends_count": 6201,
+			"listed_count": 574,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 24860,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23004,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jun 20 22:12:46 +0000 2018",
 		"id": 1009559711740563500,
 		"id_str": "1009559711740563459",
