ce832ce360f8cf019f4ed05381cb90e5d32a6307
CleverClogs <github@cleverclogs.org>
2018-06-24 11:19:34 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23290,
-	"created_at": "Sat Jun 23 21:19:33 +0000 2018",
+	"tweets": 23291,
+	"created_at": "Sun Jun 24 09:19:33 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,155 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Sun Jun 24 08:35:55 +0000 2018",
+		"id": 1010803694009573400,
+		"id_str": "1010803694009573377",
+		"text": "@SavageYetiGames @BlankSphere4 @WinsmithGames @moregamespls @msonenote So much to Share. Point is: are we all Intune on #OneNote?",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						120,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SavageYetiGames",
+					"name": "Savage Yeti Games",
+					"id": 869028082421702700,
+					"id_str": "869028082421702656",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "BlankSphere4",
+					"name": "Caleb Anderson",
+					"id": 788079051281985500,
+					"id_str": "788079051281985538",
+					"indices": [
+						17,
+						30
+					]
+				},
+				{
+					"screen_name": "WinsmithGames",
+					"name": "Winsmith Games",
+					"id": 800805598421872600,
+					"id_str": "800805598421872640",
+					"indices": [
+						31,
+						45
+					]
+				},
+				{
+					"screen_name": "moregamespls",
+					"name": "Ross 🇬🇧 More Games Please",
+					"id": 861948472588722200,
+					"id_str": "861948472588722176",
+					"indices": [
+						46,
+						59
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						60,
+						70
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1010719431532556300,
+		"in_reply_to_status_id_str": "1010719431532556289",
+		"in_reply_to_user_id": 869028082421702700,
+		"in_reply_to_user_id_str": "869028082421702656",
+		"in_reply_to_screen_name": "SavageYetiGames",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17007,
+			"friends_count": 6216,
+			"listed_count": 572,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 24930,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23081,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jun 23 20:50:41 +0000 2018",
 		"id": 1010626215806427100,
 		"id_str": "1010626215806427137",
