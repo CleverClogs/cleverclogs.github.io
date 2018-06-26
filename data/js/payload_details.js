e243b7b7b09f26630068d52049d6dc2b8aaebafc
CleverClogs <github@cleverclogs.org>
2018-06-26 16:19:37 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23336,
-	"created_at": "Tue Jun 26 12:19:35 +0000 2018",
+	"tweets": 23337,
+	"created_at": "Tue Jun 26 14:19:36 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,285 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Tue Jun 26 14:14:59 +0000 2018",
+		"id": 1011613798996824000,
+		"id_str": "1011613798996824064",
+		"text": "RT @YBB_DigitalDept: Puoils from @YBB_Yr5 sharing at @TELCAVC #PHYGITAL event how @OneNoteEDU has had a huge impact on their learning in th…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PHYGITAL",
+					"indices": [
+						62,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YBB_DigitalDept",
+					"name": "YBB Digital Learning Department",
+					"id": 996659030239141900,
+					"id_str": "996659030239141888",
+					"indices": [
+						3,
+						19
+					]
+				},
+				{
+					"screen_name": "YBB_Yr5",
+					"name": "Yr5/6 Mr Westermark",
+					"id": 775027293471305700,
+					"id_str": "775027293471305729",
+					"indices": [
+						33,
+						41
+					]
+				},
+				{
+					"screen_name": "TELCAVC",
+					"name": "TEL @ CAVC",
+					"id": 798105270513705000,
+					"id_str": "798105270513704961",
+					"indices": [
+						53,
+						61
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						82,
+						93
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17067,
+			"friends_count": 6259,
+			"listed_count": 572,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25042,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23125,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Jun 26 14:11:22 +0000 2018",
+			"id": 1011612889617137700,
+			"id_str": "1011612889617137664",
+			"text": "Puoils from @YBB_Yr5 sharing at @TELCAVC #PHYGITAL event how @OneNoteEDU has had a huge impact on their learning in… https://t.co/85sM28Pmwv",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "PHYGITAL",
+						"indices": [
+							41,
+							50
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "YBB_Yr5",
+						"name": "Yr5/6 Mr Westermark",
+						"id": 775027293471305700,
+						"id_str": "775027293471305729",
+						"indices": [
+							12,
+							20
+						]
+					},
+					{
+						"screen_name": "TELCAVC",
+						"name": "TEL @ CAVC",
+						"id": 798105270513705000,
+						"id_str": "798105270513704961",
+						"indices": [
+							32,
+							40
+						]
+					},
+					{
+						"screen_name": "OneNoteEDU",
+						"name": "OneNote Education",
+						"id": 3096566760,
+						"id_str": "3096566760",
+						"indices": [
+							61,
+							72
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/85sM28Pmwv",
+						"expanded_url": "https://twitter.com/i/web/status/1011612889617137664",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 996659030239141900,
+				"id_str": "996659030239141888",
+				"name": "YBB Digital Learning Department",
+				"screen_name": "YBB_DigitalDept",
+				"location": "Port Talbot, Wales",
+				"description": "Sharing news and events from Ysgol Bae Baglan's Digital Learning Department. We're not responsible for our followers views.",
+				"url": "https://t.co/1Y3H4vWjuQ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/1Y3H4vWjuQ",
+								"expanded_url": "http://www.baebaglan.school",
+								"display_url": "baebaglan.school",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 45,
+				"friends_count": 74,
+				"listed_count": 0,
+				"created_at": "Wed May 16 07:50:04 +0000 2018",
+				"favourites_count": 28,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 55,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/996673713427943424/vXasWXzV_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/996673713427943424/vXasWXzV_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/996659030239141888/1526457997",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Jun 26 12:10:04 +0000 2018",
 		"id": 1011582365305655300,
 		"id_str": "1011582365305655296",
