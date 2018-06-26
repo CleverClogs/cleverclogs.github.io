f621fc2c7da1fc0fb3722f91209046d132537b9e
CleverClogs <github@cleverclogs.org>
2018-06-26 10:19:34 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23332,
-	"created_at": "Tue Jun 26 07:19:35 +0000 2018",
+	"tweets": 23333,
+	"created_at": "Tue Jun 26 08:19:33 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Tue Jun 26 08:09:54 +0000 2018",
+		"id": 1011521922855825400,
+		"id_str": "1011521922855825408",
+		"text": "RT @MissPuglisi: Oral Language is the foundation to written discourse. ✍🏻 #professionaldevelopment #writing #OneNote #orallanguagedevelopme…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "professionaldevelopment",
+					"indices": [
+						74,
+						98
+					]
+				},
+				{
+					"text": "writing",
+					"indices": [
+						99,
+						107
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						108,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MissPuglisi",
+					"name": "Chloe Puglisi",
+					"id": 808750006949777400,
+					"id_str": "808750006949777409",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17060,
+			"friends_count": 6252,
+			"listed_count": 572,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25029,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23121,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Jun 26 08:06:01 +0000 2018",
+			"id": 1011520947172438000,
+			"id_str": "1011520947172438016",
+			"text": "Oral Language is the foundation to written discourse. ✍🏻 #professionaldevelopment #writing #OneNote… https://t.co/20C8hNchzW",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "professionaldevelopment",
+						"indices": [
+							57,
+							81
+						]
+					},
+					{
+						"text": "writing",
+						"indices": [
+							82,
+							90
+						]
+					},
+					{
+						"text": "OneNote",
+						"indices": [
+							91,
+							99
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/20C8hNchzW",
+						"expanded_url": "https://twitter.com/i/web/status/1011520947172438016",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							101,
+							124
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 808750006949777400,
+				"id_str": "808750006949777409",
+				"name": "Chloe Puglisi",
+				"screen_name": "MissPuglisi",
+				"location": "",
+				"description": "Aussie Primary School Teacher. 26 years young. Lifelong Learner. Pinterest Addict. Foodie. Year 2 - 2018. Check out my blog - That Teacher Life in link below.",
+				"url": "https://t.co/LPI7ReGBhC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/LPI7ReGBhC",
+								"expanded_url": "http://misspuglisisclassroom.weebly.com",
+								"display_url": "misspuglisisclassroom.weebly.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 309,
+				"friends_count": 1010,
+				"listed_count": 3,
+				"created_at": "Tue Dec 13 19:06:42 +0000 2016",
+				"favourites_count": 186,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 78,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/954085744892559360/VUdlkt8U_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/954085744892559360/VUdlkt8U_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/808750006949777409/1488137799",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Jun 26 06:44:23 +0000 2018",
 		"id": 1011500402339131400,
 		"id_str": "1011500402339131393",
