2ef92fef5e695280cc2d6522289a65e6221e049e
CleverClogs <github@cleverclogs.org>
2018-06-28 17:19:36 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23410,
-	"created_at": "Thu Jun 28 12:19:37 +0000 2018",
+	"tweets": 23411,
+	"created_at": "Thu Jun 28 15:19:35 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Thu Jun 28 15:07:30 +0000 2018",
+		"id": 1012351793815347200,
+		"id_str": "1012351793815347202",
+		"text": "Remember that heart-warming video with @merljgravey, explaining how she differentiates her lessons for all her stud… https://t.co/e7rC1Wfeyh",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "merljgravey",
+					"name": "Merlyne Graves",
+					"id": 122795096,
+					"id_str": "122795096",
+					"indices": [
+						39,
+						51
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/e7rC1Wfeyh",
+					"expanded_url": "https://twitter.com/i/web/status/1012351793815347202",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17120,
+			"friends_count": 6279,
+			"listed_count": 572,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25148,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23198,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1005201613445886000,
+		"quoted_status_id_str": "1005201613445885955",
+		"quoted_status": {
+			"created_at": "Fri Jun 08 21:35:15 +0000 2018",
+			"id": 1005201613445886000,
+			"id_str": "1005201613445885955",
+			"text": "Our #bestof for the week is this #video we created for @MicrosoftEDU. S/O to our #SaintLouis office for making this… https://t.co/dp2cZKC8T7",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "bestof",
+						"indices": [
+							4,
+							11
+						]
+					},
+					{
+						"text": "video",
+						"indices": [
+							33,
+							39
+						]
+					},
+					{
+						"text": "SaintLouis",
+						"indices": [
+							81,
+							92
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "MicrosoftEDU",
+						"name": "Microsoft Education",
+						"id": 17826187,
+						"id_str": "17826187",
+						"indices": [
+							55,
+							68
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/dp2cZKC8T7",
+						"expanded_url": "https://twitter.com/i/web/status/1005201613445885955",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 384533568,
+				"id_str": "384533568",
+				"name": "SnapShot Interactive",
+				"screen_name": "SnapShot_Nash",
+				"location": "Nashville, TN & St. Louis, MO",
+				"description": "SnapShot Interactive is an agency that specializes in creating high-quality video, custom animation, and award-winning websites.",
+				"url": "http://t.co/pjd9aodca7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/pjd9aodca7",
+								"expanded_url": "http://snapshotinteractive.com",
+								"display_url": "snapshotinteractive.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1786,
+				"friends_count": 2473,
+				"listed_count": 114,
+				"created_at": "Mon Oct 03 21:21:16 +0000 2011",
+				"favourites_count": 2426,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1300,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "2A2B2E",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/785581238564315136/RlNRXB56_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/785581238564315136/RlNRXB56_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/384533568/1495219640",
+				"profile_link_color": "EDAC2C",
+				"profile_sidebar_border_color": "423718",
+				"profile_sidebar_fill_color": "584920",
+				"profile_text_color": "C6C6CA",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jun 28 11:22:01 +0000 2018",
 		"id": 1012295045406449700,
 		"id_str": "1012295045406449666",
