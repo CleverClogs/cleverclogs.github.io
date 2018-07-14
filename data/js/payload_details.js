c71391f0fd231b0f601d64adcb0b26abf5492c73
CleverClogs <github@cleverclogs.org>
2018-07-14 16:19:31 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23590,
-	"created_at": "Sat Jul 14 13:19:30 +0000 2018",
+	"tweets": 23591,
+	"created_at": "Sat Jul 14 14:19:29 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_07.js
+++ data/js/tweets/2018_07.js
@@ -1,6 +1,266 @@
 Grailbird.data.tweets_2018_07 = 
 [
 	{
+		"created_at": "Sat Jul 14 13:34:07 +0000 2018",
+		"id": 1018126499352928300,
+		"id_str": "1018126499352928256",
+		"text": "RT @Tech_MD: Check out this week's #OMW to learn more about #collaboration in #Microsoft #OneNote! Find out how to share your notebooks wit…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OMW",
+					"indices": [
+						35,
+						39
+					]
+				},
+				{
+					"text": "collaboration",
+					"indices": [
+						60,
+						74
+					]
+				},
+				{
+					"text": "Microsoft",
+					"indices": [
+						78,
+						88
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						89,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Tech_MD",
+					"name": "TechMD",
+					"id": 274731581,
+					"id_str": "274731581",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17282,
+			"friends_count": 6319,
+			"listed_count": 572,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25344,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23376,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Jul 11 23:18:35 +0000 2018",
+			"id": 1017186418148307000,
+			"id_str": "1017186418148306944",
+			"text": "Check out this week's #OMW to learn more about #collaboration in #Microsoft #OneNote! Find out how to share your no… https://t.co/rLL7jJwYTQ",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OMW",
+						"indices": [
+							22,
+							26
+						]
+					},
+					{
+						"text": "collaboration",
+						"indices": [
+							47,
+							61
+						]
+					},
+					{
+						"text": "Microsoft",
+						"indices": [
+							65,
+							75
+						]
+					},
+					{
+						"text": "OneNote",
+						"indices": [
+							76,
+							84
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/rLL7jJwYTQ",
+						"expanded_url": "https://twitter.com/i/web/status/1017186418148306944",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 274731581,
+				"id_str": "274731581",
+				"name": "TechMD",
+				"screen_name": "Tech_MD",
+				"location": "California, USA",
+				"description": "We are an #IT firm based #SoCal who is leading #Businesses through #DigitalTransformation with #AI, #AR, #Cloud, #Security, #Compliance & #MSP services",
+				"url": "https://t.co/ROevXT2ec2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ROevXT2ec2",
+								"expanded_url": "http://www.techmd.com",
+								"display_url": "techmd.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1486,
+				"friends_count": 1284,
+				"listed_count": 16,
+				"created_at": "Wed Mar 30 23:03:49 +0000 2011",
+				"favourites_count": 998,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 915,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/969588229908254722/VcSC8oie_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/969588229908254722/VcSC8oie_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/274731581/1519932397",
+				"profile_link_color": "E81C4F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jul 14 12:41:16 +0000 2018",
 		"id": 1018113198522806300,
 		"id_str": "1018113198522806272",
