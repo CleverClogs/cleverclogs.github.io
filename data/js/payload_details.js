41deeca0608672a0e515d5cc7c99335edc48f5c1
CleverClogs <github@cleverclogs.org>
2018-07-21 16:19:31 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23671,
-	"created_at": "Sat Jul 21 19:19:34 +0000 2018",
+	"tweets": 23672,
+	"created_at": "Sat Jul 21 23:19:30 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_07.js
+++ data/js/tweets/2018_07.js
@@ -1,6 +1,365 @@
 Grailbird.data.tweets_2018_07 = 
 [
 	{
+		"created_at": "Sat Jul 21 23:05:24 +0000 2018",
+		"id": 1020806982423392300,
+		"id_str": "1020806982423392257",
+		"text": "RT @BitBender1: impressive, didn't know #OneNote could also calculate with matrices https://t.co/Iv7h7nEPl9",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						40,
+						48
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BitBender1",
+					"name": "TheBitBender",
+					"id": 2796910367,
+					"id_str": "2796910367",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1020752403845480400,
+					"id_str": "1020752403845480450",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+					"url": "https://t.co/Iv7h7nEPl9",
+					"display_url": "pic.twitter.com/Iv7h7nEPl9",
+					"expanded_url": "https://twitter.com/BitBender1/status/1020752432270323713/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 431,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 761,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1298,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1020752432270323700,
+					"source_status_id_str": "1020752432270323713",
+					"source_user_id": 2796910367,
+					"source_user_id_str": "2796910367"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1020752403845480400,
+					"id_str": "1020752403845480450",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+					"url": "https://t.co/Iv7h7nEPl9",
+					"display_url": "pic.twitter.com/Iv7h7nEPl9",
+					"expanded_url": "https://twitter.com/BitBender1/status/1020752432270323713/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 431,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 761,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1298,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1020752432270323700,
+					"source_status_id_str": "1020752432270323713",
+					"source_user_id": 2796910367,
+					"source_user_id_str": "2796910367"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17384,
+			"friends_count": 6336,
+			"listed_count": 574,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25456,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23457,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Jul 21 19:28:39 +0000 2018",
+			"id": 1020752432270323700,
+			"id_str": "1020752432270323713",
+			"text": "impressive, didn't know #OneNote could also calculate with matrices https://t.co/Iv7h7nEPl9",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OneNote",
+						"indices": [
+							24,
+							32
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1020752403845480400,
+						"id_str": "1020752403845480450",
+						"indices": [
+							68,
+							91
+						],
+						"media_url": "http://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+						"url": "https://t.co/Iv7h7nEPl9",
+						"display_url": "pic.twitter.com/Iv7h7nEPl9",
+						"expanded_url": "https://twitter.com/BitBender1/status/1020752432270323713/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 431,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 761,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1298,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1020752403845480400,
+						"id_str": "1020752403845480450",
+						"indices": [
+							68,
+							91
+						],
+						"media_url": "http://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dipw5zLWAAIjjFa.jpg",
+						"url": "https://t.co/Iv7h7nEPl9",
+						"display_url": "pic.twitter.com/Iv7h7nEPl9",
+						"expanded_url": "https://twitter.com/BitBender1/status/1020752432270323713/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 431,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 761,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1298,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2796910367,
+				"id_str": "2796910367",
+				"name": "TheBitBender",
+				"screen_name": "BitBender1",
+				"location": "",
+				"description": "Passionate software developer and computer enthusiast for more than 30 years, started with 10 on a commodore64, now luckily on dot net and c#",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 61,
+				"friends_count": 103,
+				"listed_count": 113,
+				"created_at": "Wed Oct 01 01:57:26 +0000 2014",
+				"favourites_count": 4725,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 8378,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/850973016989347840/qnZ1B1jz_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/850973016989347840/qnZ1B1jz_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jul 21 19:16:05 +0000 2018",
 		"id": 1020749272617308200,
 		"id_str": "1020749272617308160",
