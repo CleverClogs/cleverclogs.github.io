770a97ddff9c1ac129c76135f53babe2d06e1c89
CleverClogs <github@cleverclogs.org>
2018-07-23 12:19:35 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23691,
-	"created_at": "Mon Jul 23 18:19:34 +0000 2018",
+	"tweets": 23692,
+	"created_at": "Mon Jul 23 19:19:34 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_07.js
+++ data/js/tweets/2018_07.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2018_07 = 
 [
 	{
+		"created_at": "Mon Jul 23 18:20:07 +0000 2018",
+		"id": 1021459963393142800,
+		"id_str": "1021459963393142784",
+		"text": "Let's tip @Techmeme on today's launch of the Microsoft #AbilityHacks book in celebration of the company-wide commit… https://t.co/5tQoZZVUFJ",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AbilityHacks",
+					"indices": [
+						55,
+						68
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Techmeme",
+					"name": "Techmeme",
+					"id": 817386,
+					"id_str": "817386",
+					"indices": [
+						10,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5tQoZZVUFJ",
+					"expanded_url": "https://twitter.com/i/web/status/1021459963393142784",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17401,
+			"friends_count": 6351,
+			"listed_count": 576,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25496,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23477,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1021381506818699300,
+		"quoted_status_id_str": "1021381506818699265",
+		"quoted_status": {
+			"created_at": "Mon Jul 23 13:08:22 +0000 2018",
+			"id": 1021381506818699300,
+			"id_str": "1021381506818699265",
+			"text": "Proud of our journey with #accessibility. Some of our best innovations for people with disabilities, started with a… https://t.co/luDISA0pGc",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "accessibility",
+						"indices": [
+							26,
+							40
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/luDISA0pGc",
+						"expanded_url": "https://twitter.com/i/web/status/1021381506818699265",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 885414019,
+				"id_str": "885414019",
+				"name": "Jenny Lay-Flurrie",
+				"screen_name": "jennylayfluffy",
+				"location": "Seattle, USA",
+				"description": "Chief Accessibility Officer at Microsoft | Brit living in Seattle | consumer of tea, marmite | Passionate about #deaf #music #a11y #disability #gameon",
+				"url": "https://t.co/SGCWJrOkVr",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/SGCWJrOkVr",
+								"expanded_url": "http://www.microsoft.com/en-us/news/stories/people/jenny-lay-flurrie.html",
+								"display_url": "microsoft.com/en-us/news/sto…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5891,
+				"friends_count": 568,
+				"listed_count": 172,
+				"created_at": "Tue Oct 16 22:01:23 +0000 2012",
+				"favourites_count": 5396,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2655,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/989333597092892672/mlhiXilH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/989333597092892672/mlhiXilH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/885414019/1484675833",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 53,
+			"favorite_count": 95,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Jul 23 18:05:43 +0000 2018",
 		"id": 1021456339325706200,
 		"id_str": "1021456339325706240",
