c312c7289aadb98d0cbf3e862dea6c5d3cf4474d
CleverClogs <github@cleverclogs.org>
2018-08-01 17:19:29 -1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23758,
-	"created_at": "Thu Aug 02 02:19:28 +0000 2018",
+	"tweets": 23759,
+	"created_at": "Thu Aug 02 03:19:28 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,284 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Thu Aug 02 02:52:14 +0000 2018",
+		"id": 1024850332227121200,
+		"id_str": "1024850332227121153",
+		"text": "RT @Alex_n_wndrlnd: A BIG shoutout to @jessxbo and @annkozma723 as they join @Savvy_Educator on the @Flipgrid Innovator Team! #girlpower #F…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "girlpower",
+					"indices": [
+						126,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Alex_n_wndrlnd",
+					"name": "𝔸𝕝𝕖𝕩𝕒𝕟𝕕𝕣𝕒 𝕃𝕒𝕣𝕤𝕠𝕟 🎆",
+					"id": 3731840057,
+					"id_str": "3731840057",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "jessxbo",
+					"name": "Jess Boyce",
+					"id": 511231856,
+					"id_str": "511231856",
+					"indices": [
+						38,
+						46
+					]
+				},
+				{
+					"screen_name": "annkozma723",
+					"name": "Ann Kozma 📱🌎📖",
+					"id": 526629851,
+					"id_str": "526629851",
+					"indices": [
+						51,
+						63
+					]
+				},
+				{
+					"screen_name": "Savvy_Educator",
+					"name": "Jornea Armant Erwin",
+					"id": 1708895202,
+					"id_str": "1708895202",
+					"indices": [
+						77,
+						92
+					]
+				},
+				{
+					"screen_name": "Flipgrid",
+					"name": "Flipgrid",
+					"id": 630608702,
+					"id_str": "630608702",
+					"indices": [
+						100,
+						109
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17531,
+			"friends_count": 6449,
+			"listed_count": 577,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 25888,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23539,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Aug 02 01:28:52 +0000 2018",
+			"id": 1024829352452796400,
+			"id_str": "1024829352452796417",
+			"text": "A BIG shoutout to @jessxbo and @annkozma723 as they join @Savvy_Educator on the @Flipgrid Innovator Team!… https://t.co/PMNt5IH3cJ",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "jessxbo",
+						"name": "Jess Boyce",
+						"id": 511231856,
+						"id_str": "511231856",
+						"indices": [
+							18,
+							26
+						]
+					},
+					{
+						"screen_name": "annkozma723",
+						"name": "Ann Kozma 📱🌎📖",
+						"id": 526629851,
+						"id_str": "526629851",
+						"indices": [
+							31,
+							43
+						]
+					},
+					{
+						"screen_name": "Savvy_Educator",
+						"name": "Jornea Armant Erwin",
+						"id": 1708895202,
+						"id_str": "1708895202",
+						"indices": [
+							57,
+							72
+						]
+					},
+					{
+						"screen_name": "Flipgrid",
+						"name": "Flipgrid",
+						"id": 630608702,
+						"id_str": "630608702",
+						"indices": [
+							80,
+							89
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/PMNt5IH3cJ",
+						"expanded_url": "https://twitter.com/i/web/status/1024829352452796417",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							107,
+							130
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3731840057,
+				"id_str": "3731840057",
+				"name": "𝔸𝕝𝕖𝕩𝕒𝕟𝕕𝕣𝕒 𝕃𝕒𝕣𝕤𝕠𝕟 🎆",
+				"screen_name": "Alex_n_wndrlnd",
+				"location": "Atlanta, GA",
+				"description": "@APSInman Tech Teacher•🤓•Non-Fiction Junkie•🎬 Lover•🐶 Mom•Wifey•Vermonter at ❤•@i2eEDU PLS•#MIEExpert•@Flipgrid Ambassador•@brainpop Cert Educator•#SBexpert",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 726,
+				"friends_count": 788,
+				"listed_count": 42,
+				"created_at": "Mon Sep 21 20:50:07 +0000 2015",
+				"favourites_count": 3376,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3130,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1021508343121305600/U3cUw6rg_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021508343121305600/U3cUw6rg_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3731840057/1472090850",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Aug 02 01:30:51 +0000 2018",
 		"id": 1024829850392174600,
 		"id_str": "1024829850392174593",
