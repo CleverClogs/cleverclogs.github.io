050aad22863fdbc28c430038bc2deaba08a70e61
CleverClogs <github@cleverclogs.org>
2018-08-16 01:19:32 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23933,
-	"created_at": "Wed Aug 15 17:19:31 +0000 2018",
+	"tweets": 23934,
+	"created_at": "Wed Aug 15 23:19:31 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,369 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Wed Aug 15 22:29:33 +0000 2018",
+		"id": 1029857654246699000,
+		"id_str": "1029857654246699008",
+		"text": "RT @ICTEvangelist: Mentions for @Mr_ALNCO, @OneNoteC, @OneNoteEDU, @clive_Gibson, @clcsimon, @Sfm36, @AnthonySalcito and @IanFordham in my…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ICTEvangelist",
+					"name": "Mark Anderson 💬",
+					"id": 114015711,
+					"id_str": "114015711",
+					"indices": [
+						3,
+						17
+					]
+				},
+				{
+					"screen_name": "Mr_ALNCo",
+					"name": "James Donaldson",
+					"id": 753738419046678500,
+					"id_str": "753738419046678528",
+					"indices": [
+						32,
+						41
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						43,
+						52
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						54,
+						65
+					]
+				},
+				{
+					"screen_name": "clive_gibson",
+					"name": "Clive Gibson",
+					"id": 953902682,
+					"id_str": "953902682",
+					"indices": [
+						67,
+						80
+					]
+				},
+				{
+					"screen_name": "clcsimon",
+					"name": "Simon Johnson",
+					"id": 551974058,
+					"id_str": "551974058",
+					"indices": [
+						82,
+						91
+					]
+				},
+				{
+					"screen_name": "Sfm36",
+					"name": "Sarah Clark",
+					"id": 384551986,
+					"id_str": "384551986",
+					"indices": [
+						93,
+						99
+					]
+				},
+				{
+					"screen_name": "AnthonySalcito",
+					"name": "Anthony Salcito",
+					"id": 424333470,
+					"id_str": "424333470",
+					"indices": [
+						101,
+						116
+					]
+				},
+				{
+					"screen_name": "ianfordham",
+					"name": "Ian Fordham",
+					"id": 17851117,
+					"id_str": "17851117",
+					"indices": [
+						121,
+						132
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17713,
+			"friends_count": 6561,
+			"listed_count": 581,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 26159,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23711,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Aug 15 22:16:03 +0000 2018",
+			"id": 1029854259372871700,
+			"id_str": "1029854259372871682",
+			"text": "Mentions for @Mr_ALNCO, @OneNoteC, @OneNoteEDU, @clive_Gibson, @clcsimon, @Sfm36, @AnthonySalcito and @IanFordham i… https://t.co/3fYWIyO407",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Mr_ALNCo",
+						"name": "James Donaldson",
+						"id": 753738419046678500,
+						"id_str": "753738419046678528",
+						"indices": [
+							13,
+							22
+						]
+					},
+					{
+						"screen_name": "OneNoteC",
+						"name": "OneNote Central",
+						"id": 2582768738,
+						"id_str": "2582768738",
+						"indices": [
+							24,
+							33
+						]
+					},
+					{
+						"screen_name": "OneNoteEDU",
+						"name": "OneNote Education",
+						"id": 3096566760,
+						"id_str": "3096566760",
+						"indices": [
+							35,
+							46
+						]
+					},
+					{
+						"screen_name": "clive_gibson",
+						"name": "Clive Gibson",
+						"id": 953902682,
+						"id_str": "953902682",
+						"indices": [
+							48,
+							61
+						]
+					},
+					{
+						"screen_name": "clcsimon",
+						"name": "Simon Johnson",
+						"id": 551974058,
+						"id_str": "551974058",
+						"indices": [
+							63,
+							72
+						]
+					},
+					{
+						"screen_name": "Sfm36",
+						"name": "Sarah Clark",
+						"id": 384551986,
+						"id_str": "384551986",
+						"indices": [
+							74,
+							80
+						]
+					},
+					{
+						"screen_name": "AnthonySalcito",
+						"name": "Anthony Salcito",
+						"id": 424333470,
+						"id_str": "424333470",
+						"indices": [
+							82,
+							97
+						]
+					},
+					{
+						"screen_name": "ianfordham",
+						"name": "Ian Fordham",
+						"id": 17851117,
+						"id_str": "17851117",
+						"indices": [
+							102,
+							113
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/3fYWIyO407",
+						"expanded_url": "https://twitter.com/i/web/status/1029854259372871682",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 114015711,
+				"id_str": "114015711",
+				"name": "Mark Anderson 💬",
+				"screen_name": "ICTEvangelist",
+				"location": "Cardiff, Wales",
+				"description": "Award-winning Educator, Speaker, Consultant, Blogger & Author. ITL/ADE/GCI/MIE/MCCT.",
+				"url": "https://t.co/mLHOBlDBH1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mLHOBlDBH1",
+								"expanded_url": "http://ictevangelist.com",
+								"display_url": "ictevangelist.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 49469,
+				"friends_count": 1817,
+				"listed_count": 1734,
+				"created_at": "Sat Feb 13 20:23:23 +0000 2010",
+				"favourites_count": 19539,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 114300,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "3B94D9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1022273376725950474/r1itwf4C_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1022273376725950474/r1itwf4C_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/114015711/1521615334",
+				"profile_link_color": "E81C4F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 5,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Aug 15 16:31:21 +0000 2018",
 		"id": 1029767510822989800,
 		"id_str": "1029767510822989824",
