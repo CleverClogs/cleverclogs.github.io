0228ca7bc5aaa28be7c4d570223597da675033f6
CleverClogs <github@cleverclogs.org>
2018-08-28 19:19:33 +0200

    Added 1 tweets

--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Tue Aug 28 16:28:30 +0000 2018",
+		"id": 1034477837896634400,
+		"id_str": "1034477837896634368",
+		"text": "Cool time-lapse demo of the drawing capabilities of #OneNote for #Windows10 on the new @Surface Go.\nby the one and… https://t.co/Ws49PEDntH",
+		"truncated": true,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						52,
+						60
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						65,
+						75
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "surface",
+					"name": "Microsoft Surface",
+					"id": 612076511,
+					"id_str": "612076511",
+					"indices": [
+						87,
+						95
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ws49PEDntH",
+					"expanded_url": "https://twitter.com/i/web/status/1034477837896634368",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17882,
+			"friends_count": 6620,
+			"listed_count": 584,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 26226,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23798,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1034409277006463000,
+		"quoted_status_id_str": "1034409277006462977",
+		"quoted_status": {
+			"created_at": "Tue Aug 28 11:56:04 +0000 2018",
+			"id": 1034409277006463000,
+			"id_str": "1034409277006462977",
+			"text": "Took a few days but finally had a play with #OneNote on #SurfaceGo :) https://t.co/JBnZmCXOLQ (plus lots of space ink because @JenMsft :))",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OneNote",
+						"indices": [
+							44,
+							52
+						]
+					},
+					{
+						"text": "SurfaceGo",
+						"indices": [
+							56,
+							66
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "JenMsft",
+						"name": "Jen Gentleman 🌺",
+						"id": 3309105596,
+						"id_str": "3309105596",
+						"indices": [
+							126,
+							134
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/JBnZmCXOLQ",
+						"expanded_url": "https://www.instagram.com/p/BnBXEZbh0lr/?utm_source=ig_share_sheet&igshid=1mkbsfiznjxdz",
+						"display_url": "instagram.com/p/BnBXEZbh0lr/…",
+						"indices": [
+							70,
+							93
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19986805,
+				"id_str": "19986805",
+				"name": "Totes AmazePauls",
+				"screen_name": "Kercal",
+				"location": "Surrey, UK.",
+				"description": "Touchscreen, IWB and VR Artist / Writer\\ Teacher (A level/BTEC). Artist in Residence for hire. Team #DigitalArt #Paint3D #ArtRage #Edtech #VR #MIEexpert",
+				"url": "https://t.co/NPgvRkunis",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/NPgvRkunis",
+								"expanded_url": "http://www.kercal.co.uk",
+								"display_url": "kercal.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1701,
+				"friends_count": 2564,
+				"listed_count": 170,
+				"created_at": "Tue Feb 03 17:30:24 +0000 2009",
+				"favourites_count": 13446,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 30963,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/855533724766785536/0sPbD3pi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/855533724766785536/0sPbD3pi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19986805/1488025369",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Aug 28 12:14:32 +0000 2018",
 		"id": 1034413921485111300,
 		"id_str": "1034413921485111298",
