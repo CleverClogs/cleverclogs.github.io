742179d8d8996654ff47f94a43edbada9eab7279
CleverClogs <github@cleverclogs.org>
2018-09-01 11:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24081,
-	"created_at": "Sat Sep 01 08:19:27 +0000 2018",
+	"tweets": 24082,
+	"created_at": "Sat Sep 01 09:19:27 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,326 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Sat Sep 01 09:00:39 +0000 2018",
+		"id": 1035814683415113700,
+		"id_str": "1035814683415113733",
+		"text": "RT @tallstarz: When you decide that 4 non-corresponding To Do lists need reorganising! Try a new way!! #OneNote #organising https://t.co/Vn…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						103,
+						111
+					]
+				},
+				{
+					"text": "organising",
+					"indices": [
+						112,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tallstarz",
+					"name": "Sara B",
+					"id": 163950379,
+					"id_str": "163950379",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17921,
+			"friends_count": 6659,
+			"listed_count": 583,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 26350,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23853,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1524564118",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Sep 01 08:28:58 +0000 2018",
+			"id": 1035806708373893100,
+			"id_str": "1035806708373893120",
+			"text": "When you decide that 4 non-corresponding To Do lists need reorganising! Try a new way!! #OneNote #organising https://t.co/VnSCekNBo9",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OneNote",
+						"indices": [
+							88,
+							96
+						]
+					},
+					{
+						"text": "organising",
+						"indices": [
+							97,
+							108
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1035806703202259000,
+						"id_str": "1035806703202258944",
+						"indices": [
+							109,
+							132
+						],
+						"media_url": "http://pbs.twimg.com/media/Dl_stWhW0AARb06.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dl_stWhW0AARb06.jpg",
+						"url": "https://t.co/VnSCekNBo9",
+						"display_url": "pic.twitter.com/VnSCekNBo9",
+						"expanded_url": "https://twitter.com/tallstarz/status/1035806708373893120/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 257,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1309,
+								"h": 495,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 454,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1035806703202259000,
+						"id_str": "1035806703202258944",
+						"indices": [
+							109,
+							132
+						],
+						"media_url": "http://pbs.twimg.com/media/Dl_stWhW0AARb06.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dl_stWhW0AARb06.jpg",
+						"url": "https://t.co/VnSCekNBo9",
+						"display_url": "pic.twitter.com/VnSCekNBo9",
+						"expanded_url": "https://twitter.com/tallstarz/status/1035806708373893120/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 257,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1309,
+								"h": 495,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 454,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 163950379,
+				"id_str": "163950379",
+				"name": "Sara B",
+				"screen_name": "tallstarz",
+				"location": "",
+				"description": "SEN and Inclusion in NI! ❤️travel!! Progress, creativity, inspire children, lifelong learner. MIEExpert, Seesaw Ambassador, Apple Teacher.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 139,
+				"friends_count": 223,
+				"listed_count": 5,
+				"created_at": "Wed Jul 07 17:34:35 +0000 2010",
+				"favourites_count": 4965,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 444,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1024664000380973057/XRizz0KO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1024664000380973057/XRizz0KO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/163950379/1376729656",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "78e9729ff12a648e",
+				"url": "https://api.twitter.com/1.1/geo/id/78e9729ff12a648e.json",
+				"place_type": "admin",
+				"name": "Northern Ireland",
+				"full_name": "Northern Ireland, United Kingdom",
+				"country_code": "GB",
+				"country": "United Kingdom",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-8.19417899950185,
+								54.0227179827604
+							],
+							[
+								-5.43284099988887,
+								54.0227179827604
+							],
+							[
+								-5.43284099988887,
+								55.3116860009633
+							],
+							[
+								-8.19417899950185,
+								55.3116860009633
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Sep 01 08:16:35 +0000 2018",
 		"id": 1035803590852526100,
 		"id_str": "1035803590852526080",
