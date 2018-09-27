2d245e23dd92783e6bf310e85232cebe9e961f56
CleverClogs <github@cleverclogs.org>
2018-09-27 13:19:38 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24429,
-	"created_at": "Thu Sep 27 10:19:32 +0000 2018",
+	"tweets": 24430,
+	"created_at": "Thu Sep 27 11:19:36 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,276 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Thu Sep 27 10:45:49 +0000 2018",
+		"id": 1045263233161023500,
+		"id_str": "1045263233161023488",
+		"text": "RT @emmalvnn: my bored ass started screwing around on microsoft onenote and after half a day, I ended up with this and ya https://t.co/HwGr…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "emmalvnn",
+					"name": "Chief Queef",
+					"id": 2365135658,
+					"id_str": "2365135658",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18321,
+			"friends_count": 6867,
+			"listed_count": 581,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 27042,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24197,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Sep 27 04:12:11 +0000 2018",
+			"id": 1045164171380191200,
+			"id_str": "1045164171380191233",
+			"text": "my bored ass started screwing around on microsoft onenote and after half a day, I ended up with this and ya https://t.co/HwGrHNHfb1",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1045164169266221000,
+						"id_str": "1045164169266221056",
+						"indices": [
+							108,
+							131
+						],
+						"media_url": "http://pbs.twimg.com/media/DoErRkEUUAA-J14.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DoErRkEUUAA-J14.jpg",
+						"url": "https://t.co/HwGrHNHfb1",
+						"display_url": "pic.twitter.com/HwGrHNHfb1",
+						"expanded_url": "https://twitter.com/emmalvnn/status/1045164171380191233/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 849,
+								"h": 970,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 849,
+								"h": 970,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 595,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1045164169266221000,
+						"id_str": "1045164169266221056",
+						"indices": [
+							108,
+							131
+						],
+						"media_url": "http://pbs.twimg.com/media/DoErRkEUUAA-J14.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DoErRkEUUAA-J14.jpg",
+						"url": "https://t.co/HwGrHNHfb1",
+						"display_url": "pic.twitter.com/HwGrHNHfb1",
+						"expanded_url": "https://twitter.com/emmalvnn/status/1045164171380191233/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 849,
+								"h": 970,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 849,
+								"h": 970,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 595,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2365135658,
+				"id_str": "2365135658",
+				"name": "Chief Queef",
+				"screen_name": "emmalvnn",
+				"location": "oh no baby what!¡ is u doin",
+				"description": "header artwork by @henrikaau on instagram",
+				"url": "https://t.co/B540Nm3RJV",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/B540Nm3RJV",
+								"expanded_url": "http://www.instagram.com/emmalvnn",
+								"display_url": "instagram.com/emmalvnn",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1247,
+				"friends_count": 216,
+				"listed_count": 3,
+				"created_at": "Fri Feb 28 05:43:05 +0000 2014",
+				"favourites_count": 2931,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1822,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/756996364786171905/RlPOEdVl_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/756996364786171905/RlPOEdVl_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2365135658/1492320487",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 0,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Sep 27 09:57:38 +0000 2018",
 		"id": 1045251107080032300,
 		"id_str": "1045251107080032256",
