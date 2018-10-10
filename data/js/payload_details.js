54f77cf1bf85cc2a12649a837826b29b480deff7
CleverClogs <github@cleverclogs.org>
2018-10-10 12:19:30 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24573,
-	"created_at": "Wed Oct 10 09:19:29 +0000 2018",
+	"tweets": 24574,
+	"created_at": "Wed Oct 10 10:19:29 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Wed Oct 10 09:53:09 +0000 2018",
+		"id": 1049961021107052500,
+		"id_str": "1049961021107052544",
+		"text": "@Ezzerharden @cryrid @ContornoTony @Steam_Tinkerer @imm0rtalreaper Meant to tag @boyzierpg",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Ezzerharden",
+					"name": "Victor Diaz Jr",
+					"id": 3284525928,
+					"id_str": "3284525928",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "cryrid",
+					"name": "Michael Taylor 🎃",
+					"id": 22715776,
+					"id_str": "22715776",
+					"indices": [
+						13,
+						20
+					]
+				},
+				{
+					"screen_name": "ContornoTony",
+					"name": "Tony Contorno",
+					"id": 1381029433,
+					"id_str": "1381029433",
+					"indices": [
+						21,
+						34
+					]
+				},
+				{
+					"screen_name": "Steam_Tinkerer",
+					"name": "SteamTinkerer🎲🏳️‍🌈",
+					"id": 3405287777,
+					"id_str": "3405287777",
+					"indices": [
+						35,
+						50
+					]
+				},
+				{
+					"screen_name": "imm0rtalreaper",
+					"name": "Imm0rtal Reaper",
+					"id": 1090449019,
+					"id_str": "1090449019",
+					"indices": [
+						51,
+						66
+					]
+				},
+				{
+					"screen_name": "boyzierpg",
+					"name": "boyzierpg",
+					"id": 1043283158681174000,
+					"id_str": "1043283158681174016",
+					"indices": [
+						80,
+						90
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1049949807224209400,
+		"in_reply_to_status_id_str": "1049949807224209409",
+		"in_reply_to_user_id": 2582768738,
+		"in_reply_to_user_id_str": "2582768738",
+		"in_reply_to_screen_name": "OneNoteC",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18501,
+			"friends_count": 6916,
+			"listed_count": 581,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 27218,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24336,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Oct 10 09:08:35 +0000 2018",
 		"id": 1049949807224209400,
 		"id_str": "1049949807224209409",
