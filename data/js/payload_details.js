e433c366efd66b11d6ea04bd30ffc10659901198
CleverClogs <github@cleverclogs.org>
2018-11-07 21:19:29 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27818,
-	"created_at": "Wed Nov 07 19:19:28 +0000 2018",
+	"tweets": 27820,
+	"created_at": "Wed Nov 07 20:19:28 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Wed Nov 07 19:59:21 +0000 2018",
+		"id": 1060260435147010000,
+		"id_str": "1060260435147010048",
+		"full_text": "@fabiancastro @MicrosoftAyuda @msonenote Hi Fabian, \nCould you please report your case of #OneNote data loss through https://t.co/KUbMMzgEot?\n\nInclude details about the version of OneNote, the device you used, what type of account you used (personal Microsoft ID or school/work Office 365 ID), etc.\n\ncc @MTholfsen",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			313
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						90,
+						98
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fabiancastro",
+					"name": "Fabian Castro Campos",
+					"id": 18638616,
+					"id_str": "18638616",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "MicrosoftAyuda",
+					"name": "Soporte de Microsoft",
+					"id": 122163176,
+					"id_str": "122163176",
+					"indices": [
+						14,
+						29
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						30,
+						40
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						303,
+						313
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/KUbMMzgEot",
+					"expanded_url": "https://aka.ms/edusupport",
+					"display_url": "aka.ms/edusupport",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1060062367671828500,
+		"in_reply_to_status_id_str": "1060062367671828480",
+		"in_reply_to_user_id": 18638616,
+		"in_reply_to_user_id_str": "18638616",
+		"in_reply_to_screen_name": "fabiancastro",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18944,
+			"friends_count": 7063,
+			"listed_count": 588,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 27817,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24748,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@fabiancastro @MicrosoftAyuda @msonenote Hi Fabian, \nCould you please report your case of #OneNote data loss through https://t.co/KUbMMzgEot?\n\nInclude details about the version of OneNote, the device you used, what type of account you used (personal Microsoft ID or school/work Office 365 ID), etc.\n\ncc @MTholfsen"
+	},
+	{
+		"created_at": "Wed Nov 07 17:36:35 +0000 2018",
+		"id": 1060224506818715600,
+		"id_str": "1060224506818715652",
+		"full_text": "I want to release a version of Bldg a Second Brain book printed inside a notebook with large margins and double spaced so it’s ready to take notes on",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1909232666,
+			"id_str": "1909232666",
+			"name": "Tiago Forte",
+			"screen_name": "fortelabs",
+			"location": "San Francisco",
+			"description": "Founder of Forte Labs, editor at Praxis blog: https://t.co/W2mPB6gHtn. Sign up for my newsletter: https://t.co/JEhBT1A4r3",
+			"url": "https://t.co/N0Y8HMOn9q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/N0Y8HMOn9q",
+							"expanded_url": "http://fortelabs.co",
+							"display_url": "fortelabs.co",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/W2mPB6gHtn",
+							"expanded_url": "http://praxis.fortelabs.co",
+							"display_url": "praxis.fortelabs.co",
+							"indices": [
+								46,
+								69
+							]
+						},
+						{
+							"url": "https://t.co/JEhBT1A4r3",
+							"expanded_url": "https://www.fortelabs.co/newsletter",
+							"display_url": "fortelabs.co/newsletter",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6464,
+			"friends_count": 1694,
+			"listed_count": 325,
+			"created_at": "Thu Sep 26 21:12:43 +0000 2013",
+			"favourites_count": 11392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14724,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ACDED6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/872149960262692864/oGrZ0sNY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/872149960262692864/oGrZ0sNY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1909232666/1529987738",
+			"profile_link_color": "038543",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I want to release a version of Bldg a Second Brain book printed inside a notebook with large margins and double spaced so it’s ready to take notes on"
+	},
+	{
 		"created_at": "Wed Nov 07 17:36:35 +0000 2018",
 		"id": 1060224506818715600,
 		"id_str": "1060224506818715652",
