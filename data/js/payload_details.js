b0f6a243e43416a8c85549dde24efe5feca62aac
CleverClogs <github@cleverclogs.org>
2018-11-09 14:19:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27830,
-	"created_at": "Fri Nov 09 10:19:28 +0000 2018",
+	"tweets": 27831,
+	"created_at": "Fri Nov 09 13:19:29 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Fri Nov 09 13:10:52 +0000 2018",
+		"id": 1060882415202328600,
+		"id_str": "1060882415202328576",
+		"full_text": "Want to live-embed an animated GIF in #Microsoft #OneNote? \nFirst upload the GIF to @GIPHY. \nThen copy the link to your GIF and paste it directly into a OneNote page. \n\nTry the gorgeous Happy Angler Fish:\nhttps://t.co/JZJPUE8QUF\nby @MicrosoftEDU\n#oEmbed\n\nhttps://t.co/HR6y7YSdrg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Microsoft",
+					"indices": [
+						38,
+						48
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						49,
+						57
+					]
+				},
+				{
+					"text": "oEmbed",
+					"indices": [
+						246,
+						253
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GIPHY",
+					"name": "GIPHY",
+					"id": 1020383864,
+					"id_str": "1020383864",
+					"indices": [
+						84,
+						90
+					]
+				},
+				{
+					"screen_name": "MicrosoftEDU",
+					"name": "Microsoft Education",
+					"id": 17826187,
+					"id_str": "17826187",
+					"indices": [
+						232,
+						245
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JZJPUE8QUF",
+					"expanded_url": "https://giphy.com/gifs/microsoftedu-education-microsoft-edu-1ymLvcZ6yuGlvf8tSS",
+					"display_url": "giphy.com/gifs/microsoft…",
+					"indices": [
+						205,
+						228
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1018237464996143100,
+					"id_str": "1018237464996143106",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DiGBlF4W4AIESEp.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DiGBlF4W4AIESEp.jpg",
+					"url": "https://t.co/HR6y7YSdrg",
+					"display_url": "pic.twitter.com/HR6y7YSdrg",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1018237885378711552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1018237885378711600,
+					"source_status_id_str": "1018237885378711552",
+					"source_user_id": 2582768738,
+					"source_user_id_str": "2582768738"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1018237464996143100,
+					"id_str": "1018237464996143106",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DiGBlF4W4AIESEp.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DiGBlF4W4AIESEp.jpg",
+					"url": "https://t.co/HR6y7YSdrg",
+					"display_url": "pic.twitter.com/HR6y7YSdrg",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1018237885378711552/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1018237885378711600,
+					"source_status_id_str": "1018237885378711552",
+					"source_user_id": 2582768738,
+					"source_user_id_str": "2582768738",
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DiGBlF4W4AIESEp.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18950,
+			"friends_count": 7065,
+			"listed_count": 588,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 27837,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24756,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Want to live-embed an animated GIF in #Microsoft #OneNote? \nFirst upload the GIF to @GIPHY. \nThen copy the link to your GIF and paste it directly into a OneNote page. \n\nTry the gorgeous Happy Angler Fish:\nhttps://t.co/JZJPUE8QUF\nby @MicrosoftEDU\n#oEmbed\n\nhttps://t.co/HR6y7YSdrg"
+	},
+	{
 		"created_at": "Fri Nov 09 09:28:36 +0000 2018",
 		"id": 1060826479427240000,
 		"id_str": "1060826479427239936",
