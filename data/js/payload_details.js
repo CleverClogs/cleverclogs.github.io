3e34b72ee38b4d462e2993c833f7095c0139be4b
CleverClogs <github@cleverclogs.org>
2018-11-11 20:19:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27909,
-	"created_at": "Sun Nov 11 18:19:29 +0000 2018",
+	"tweets": 27910,
+	"created_at": "Sun Nov 11 19:19:29 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sun Nov 11 19:10:07 +0000 2018",
+		"id": 1061697598912979000,
+		"id_str": "1061697598912978950",
+		"full_text": "@Agredo10 @msonenote To print all pages in a #OneNote notebook section group from the #Windows10 app, activate the first section in the section group and open the Print command. \nThe drop-down menu under Pages will now let you select Current Section Group. \nThe page preview on the right will adjust. https://t.co/rypCpYTdDQ",
+		"truncated": false,
+		"display_text_range": [
+			21,
+			300
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						45,
+						53
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						86,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Agredo10",
+					"name": "Christian Agredo",
+					"id": 56345759,
+					"id_str": "56345759",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						10,
+						20
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1061697571020857300,
+					"id_str": "1061697571020857349",
+					"indices": [
+						301,
+						324
+					],
+					"media_url": "http://pbs.twimg.com/media/DrvoUNOX4AUaYoU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrvoUNOX4AUaYoU.jpg",
+					"url": "https://t.co/rypCpYTdDQ",
+					"display_url": "pic.twitter.com/rypCpYTdDQ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1061697598912978950/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1498,
+							"h": 1126,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 902,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1061697571020857300,
+					"id_str": "1061697571020857349",
+					"indices": [
+						301,
+						324
+					],
+					"media_url": "http://pbs.twimg.com/media/DrvoUNOX4AUaYoU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrvoUNOX4AUaYoU.jpg",
+					"url": "https://t.co/rypCpYTdDQ",
+					"display_url": "pic.twitter.com/rypCpYTdDQ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1061697598912978950/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1498,
+							"h": 1126,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 902,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1061665246383874000,
+		"in_reply_to_status_id_str": "1061665246383874048",
+		"in_reply_to_user_id": 56345759,
+		"in_reply_to_user_id_str": "56345759",
+		"in_reply_to_screen_name": "Agredo10",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18975,
+			"friends_count": 7075,
+			"listed_count": 589,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 27849,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24773,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Agredo10 @msonenote To print all pages in a #OneNote notebook section group from the #Windows10 app, activate the first section in the section group and open the Print command. \nThe drop-down menu under Pages will now let you select Current Section Group. \nThe page preview on the right will adjust. https://t.co/rypCpYTdDQ"
+	},
+	{
 		"created_at": "Sun Nov 11 17:31:45 +0000 2018",
 		"id": 1061672844885745700,
 		"id_str": "1061672844885745669",
