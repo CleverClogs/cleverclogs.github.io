a976af90b3883edf8bac5c6def5e69122a57e230
CleverClogs <github@cleverclogs.org>
2018-11-11 19:19:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27908,
-	"created_at": "Sun Nov 11 17:19:29 +0000 2018",
+	"tweets": 27909,
+	"created_at": "Sun Nov 11 18:19:29 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sun Nov 11 17:31:45 +0000 2018",
+		"id": 1061672844885745700,
+		"id_str": "1061672844885745669",
+		"full_text": "Just in: during her welcome message for this week's Online OneNote Conference, CVP Laura Butler in passing (!) announced NoTa – the new combined name for the #OneNote, Sticky Notes and Tasks teams. \n\nVideo embedded here: \nhttps://t.co/AQFE2jooaI\nwith @LauraCatPJs\n#LONC18 https://t.co/XQOAn5zZIv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						158,
+						166
+					]
+				},
+				{
+					"text": "LONC18",
+					"indices": [
+						264,
+						271
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LauraCatPJs",
+					"name": "Laura Butler, Goddess of Engineers",
+					"id": 760954730612797400,
+					"id_str": "760954730612797440",
+					"indices": [
+						251,
+						263
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AQFE2jooaI",
+					"expanded_url": "https://learnonconference.com/kick-off-party-2018/",
+					"display_url": "learnonconference.com/kick-off-party…",
+					"indices": [
+						222,
+						245
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1061672223373738000,
+					"id_str": "1061672223373737984",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/DrvRQx4XQAAWK-f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrvRQx4XQAAWK-f.jpg",
+					"url": "https://t.co/XQOAn5zZIv",
+					"display_url": "pic.twitter.com/XQOAn5zZIv",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1061672844885745669/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 385,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1038,
+							"h": 588,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1038,
+							"h": 588,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1061672223373738000,
+					"id_str": "1061672223373737984",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/DrvRQx4XQAAWK-f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrvRQx4XQAAWK-f.jpg",
+					"url": "https://t.co/XQOAn5zZIv",
+					"display_url": "pic.twitter.com/XQOAn5zZIv",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1061672844885745669/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 385,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1038,
+							"h": 588,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1038,
+							"h": 588,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18974,
+			"friends_count": 7075,
+			"listed_count": 589,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 27849,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24772,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just in: during her welcome message for this week's Online OneNote Conference, CVP Laura Butler in passing (!) announced NoTa – the new combined name for the #OneNote, Sticky Notes and Tasks teams. \n\nVideo embedded here: \nhttps://t.co/AQFE2jooaI\nwith @LauraCatPJs\n#LONC18 https://t.co/XQOAn5zZIv"
+	},
+	{
 		"created_at": "Sun Nov 11 16:47:36 +0000 2018",
 		"id": 1061661733608144900,
 		"id_str": "1061661733608144898",
