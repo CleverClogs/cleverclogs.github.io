701ee6525cb1f701370165ad74e6261a3377796b
CleverClogs <github@cleverclogs.org>
2018-11-22 12:19:50 +0100

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29093,
-	"created_at": "Thu Nov 22 10:19:55 +0000 2018",
+	"tweets": 29097,
+	"created_at": "Thu Nov 22 11:19:49 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_11.js",
 		"year": 2018,
 		"var_name": "tweets_2018_11",
-		"tweet_count": 1304,
+		"tweet_count": 1308,
 		"month": 11
 	},
 	{
