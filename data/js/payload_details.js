93617a54f91923f1b8db0a97b681fcfa27dcddf8
CleverClogs <github@cleverclogs.org>
2018-12-02 15:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29263,
-	"created_at": "Sun Dec 02 12:19:52 +0000 2018",
+	"tweets": 29264,
+	"created_at": "Sun Dec 02 14:19:27 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,368 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sun Dec 02 13:44:52 +0000 2018",
+		"id": 1069225890687713300,
+		"id_str": "1069225890687713281",
+		"full_text": "This #BestOf2018 TweetMeet on Tuesday December 18 is going to be the most global #MSFTEduChat ever. \n\nThanks to @erinjurisich and 142 other international hosts we are going to blow the roof off of Twitter that day. \nWill you join?\n\n#MicrosoftEDU https://t.co/pfaAGZsjRi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BestOf2018",
+					"indices": [
+						5,
+						16
+					]
+				},
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						81,
+						93
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						232,
+						245
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erinjurisich",
+					"name": "Mrs. Erin Holland",
+					"id": 443760872,
+					"id_str": "443760872",
+					"indices": [
+						112,
+						125
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/pfaAGZsjRi",
+					"expanded_url": "https://twitter.com/erinjurisich/status/1068914431361523712",
+					"display_url": "twitter.com/erinjurisich/s…",
+					"indices": [
+						246,
+						269
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19242,
+			"friends_count": 7144,
+			"listed_count": 595,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28022,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24942,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1068914431361523700,
+		"quoted_status_id_str": "1068914431361523712",
+		"quoted_status_permalink": {
+			"url": "https://t.co/pfaAGZsjRi",
+			"expanded": "https://twitter.com/erinjurisich/status/1068914431361523712",
+			"display": "twitter.com/erinjurisich/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Dec 01 17:07:14 +0000 2018",
+			"id": 1068914431361523700,
+			"id_str": "1068914431361523712",
+			"full_text": "Just finished participating in a @MicrosoftTeams Meet Now to prepare for a #MSFTEduChat ‘Best Of 2018’ TweetMeet with over 140 global hosts in multiple languages! #OnslowDLT @TeamsCentral @OneNoteC @TweetMeet https://t.co/fIsE0qeiKR",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				208
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MSFTEduChat",
+						"indices": [
+							75,
+							87
+						]
+					},
+					{
+						"text": "OnslowDLT",
+						"indices": [
+							163,
+							173
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "MicrosoftTeams",
+						"name": "Microsoft Teams",
+						"id": 817461289081409500,
+						"id_str": "817461289081409536",
+						"indices": [
+							33,
+							48
+						]
+					},
+					{
+						"screen_name": "TeamsCentral",
+						"name": "Teams Central",
+						"id": 1020219219689033700,
+						"id_str": "1020219219689033729",
+						"indices": [
+							174,
+							187
+						]
+					},
+					{
+						"screen_name": "OneNoteC",
+						"name": "OneNote Central",
+						"id": 2582768738,
+						"id_str": "2582768738",
+						"indices": [
+							188,
+							197
+						]
+					},
+					{
+						"screen_name": "TweetMeet",
+						"name": "TweetMeet Central",
+						"id": 15665199,
+						"id_str": "15665199",
+						"indices": [
+							198,
+							208
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1068914424734339100,
+						"id_str": "1068914424734339072",
+						"indices": [
+							209,
+							232
+						],
+						"media_url": "http://pbs.twimg.com/media/DtWMAYWU0AALW1j.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DtWMAYWU0AALW1j.jpg",
+						"url": "https://t.co/fIsE0qeiKR",
+						"display_url": "pic.twitter.com/fIsE0qeiKR",
+						"expanded_url": "https://twitter.com/erinjurisich/status/1068914431361523712/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1068914424734339100,
+						"id_str": "1068914424734339072",
+						"indices": [
+							209,
+							232
+						],
+						"media_url": "http://pbs.twimg.com/media/DtWMAYWU0AALW1j.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DtWMAYWU0AALW1j.jpg",
+						"url": "https://t.co/fIsE0qeiKR",
+						"display_url": "pic.twitter.com/fIsE0qeiKR",
+						"expanded_url": "https://twitter.com/erinjurisich/status/1068914431361523712/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 443760872,
+				"id_str": "443760872",
+				"name": "Mrs. Erin Holland",
+				"screen_name": "erinjurisich",
+				"location": "North Carolina, USA",
+				"description": "DLTF @ SHS in Onslow County #onslowdlt",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 270,
+				"friends_count": 130,
+				"listed_count": 12,
+				"created_at": "Thu Dec 22 14:23:32 +0000 2011",
+				"favourites_count": 1638,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 941,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1037435929479311361/cj8lu06h_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1037435929479311361/cj8lu06h_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/443760872/1423937391",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This #BestOf2018 TweetMeet on Tuesday December 18 is going to be the most global #MSFTEduChat ever. \n\nThanks to @erinjurisich and 142 other international hosts we are going to blow the roof off of Twitter that day. \nWill you join?\n\n#MicrosoftEDU https://t.co/pfaAGZsjRi"
+	},
+	{
 		"created_at": "Sun Dec 02 12:19:13 +0000 2018",
 		"id": 1069204336922804200,
 		"id_str": "1069204336922804224",
