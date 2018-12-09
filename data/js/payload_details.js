41c2be93ef15f59a569799ba9719fdcbc74bdf5f
CleverClogs <github@cleverclogs.org>
2018-12-09 19:19:31 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29458,
-	"created_at": "Sat Dec 08 21:23:30 +0000 2018",
+	"tweets": 29459,
+	"created_at": "Sun Dec 09 18:19:30 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Fri Dec 07 17:13:02 +0000 2018",
+		"id": 1071090218596528100,
+		"id_str": "1071090218596528128",
+		"full_text": "Microsoft Whiteboard is getting more colorful and customizable than ever. Check out the latest updates here: https://t.co/S6FchhLX5n https://t.co/NCHO0W74WG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/S6FchhLX5n",
+					"expanded_url": "http://msft.social/Byud2I",
+					"display_url": "msft.social/Byud2I",
+					"indices": [
+						109,
+						132
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1071090213127110700,
+					"id_str": "1071090213127110656",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt1G37vWwAAz-ba.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt1G37vWwAAz-ba.jpg",
+					"url": "https://t.co/NCHO0W74WG",
+					"display_url": "pic.twitter.com/NCHO0W74WG",
+					"expanded_url": "https://twitter.com/OfficeNews/status/1071090218596528128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1071090213127110700,
+					"id_str": "1071090213127110656",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt1G37vWwAAz-ba.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt1G37vWwAAz-ba.jpg",
+					"url": "https://t.co/NCHO0W74WG",
+					"display_url": "pic.twitter.com/NCHO0W74WG",
+					"expanded_url": "https://twitter.com/OfficeNews/status/1071090218596528128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://prod1.sprinklr.com\" rel=\"nofollow\">Sprinklr Prod1</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740240040819753000,
+			"id_str": "740240040819752960",
+			"name": "Microsoft Office News",
+			"screen_name": "OfficeNews",
+			"location": "Redmond, WA",
+			"description": "We’re always improving Office to empower you to achieve the things that matter. Be the first to know about breaking news, updates, events, and more.",
+			"url": "https://t.co/5sH2lMuBJY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5sH2lMuBJY",
+							"expanded_url": "https://products.office.com/",
+							"display_url": "products.office.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 115901,
+			"friends_count": 1106,
+			"listed_count": 501,
+			"created_at": "Tue Jun 07 17:52:33 +0000 2016",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3170,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1065039796702068736/iblLs372_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1065039796702068736/iblLs372_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740240040819752960/1535488915",
+			"profile_link_color": "D83B01",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 63,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Microsoft Whiteboard is getting more colorful and customizable than ever. Check out the latest updates here: https://t.co/S6FchhLX5n https://t.co/NCHO0W74WG"
+	},
+	{
 		"created_at": "Sat Dec 08 20:55:54 +0000 2018",
 		"id": 1071508692959400000,
 		"id_str": "1071508692959399936",
