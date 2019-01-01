42bbc5feb7ecab7f643f81820e59ac4f321d0a02
CleverClogs <github@cleverclogs.org>
2019-01-01 20:19:28 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30029,
-	"created_at": "Tue Jan 01 18:19:27 +0000 2019",
+	"tweets": 30031,
+	"created_at": "Tue Jan 01 19:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,419 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 01 19:06:59 +0000 2019",
+		"id": 1080178589604008000,
+		"id_str": "1080178589604007936",
+		"full_text": "#Sketchnote made using @msonenote @OneNoteC of ‘Make It Stick: The Science of Successful Learning’ 🧠⚡️\n\nShare or print out for your school community.😊\n\nFor more info see @cultofpedagogy interview with Peter Brown 💭\nhttps://t.co/JYq86Ts7kt\n#brainbasedlearning #retrievalpractice https://t.co/LpCVEvg0pF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Sketchnote",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"text": "brainbasedlearning",
+					"indices": [
+						239,
+						258
+					]
+				},
+				{
+					"text": "retrievalpractice",
+					"indices": [
+						259,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						23,
+						33
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						34,
+						43
+					]
+				},
+				{
+					"screen_name": "cultofpedagogy",
+					"name": "Jennifer Gonzalez",
+					"id": 1881304950,
+					"id_str": "1881304950",
+					"indices": [
+						170,
+						185
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JYq86Ts7kt",
+					"expanded_url": "http://ow.ly/UAuG30n1i3o",
+					"display_url": "ow.ly/UAuG30n1i3o",
+					"indices": [
+						215,
+						238
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1080178576828162000,
+					"id_str": "1080178576828162051",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"url": "https://t.co/LpCVEvg0pF",
+					"display_url": "pic.twitter.com/LpCVEvg0pF",
+					"expanded_url": "https://twitter.com/BuLisaBisa/status/1080178589604007936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080178576828162000,
+					"id_str": "1080178576828162051",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"url": "https://t.co/LpCVEvg0pF",
+					"display_url": "pic.twitter.com/LpCVEvg0pF",
+					"expanded_url": "https://twitter.com/BuLisaBisa/status/1080178589604007936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 961518821302399000,
+			"id_str": "961518821302398977",
+			"name": "Lisa",
+			"screen_name": "BuLisaBisa",
+			"location": "",
+			"description": "👩🏻‍🏫 Teacher • Student Counsellor • Lifelong Learner 💞🌏 #Wellbeing | #Mentalhealth | #21stcenturyskills | #PositiveEducation 🌱🌿🌳",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 371,
+			"friends_count": 250,
+			"listed_count": 6,
+			"created_at": "Thu Feb 08 08:35:25 +0000 2018",
+			"favourites_count": 996,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 631,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/961524854284431360/bxnqKKNf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/961524854284431360/bxnqKKNf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/961518821302398977/1518079509",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Sketchnote made using @msonenote @OneNoteC of ‘Make It Stick: The Science of Successful Learning’ 🧠⚡️\n\nShare or print out for your school community.😊\n\nFor more info see @cultofpedagogy interview with Peter Brown 💭\nhttps://t.co/JYq86Ts7kt\n#brainbasedlearning #retrievalpractice https://t.co/LpCVEvg0pF"
+	},
+	{
+		"created_at": "Tue Jan 01 13:09:34 +0000 2019",
+		"id": 1080088644495573000,
+		"id_str": "1080088644495572992",
+		"full_text": "New Year = New OneNote Notebook. \nHappy New Year Everyone! 🎆 \n\nMay you make a positive difference in the lives of those around you &amp; beyond. Keep on Learning!\n#OneNote #MIEExpert https://t.co/3qGBIotew9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						163,
+						171
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						172,
+						182
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1080087442479173600,
+					"id_str": "1080087442479173632",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv09zm9U0AAqroR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv09zm9U0AAqroR.jpg",
+					"url": "https://t.co/3qGBIotew9",
+					"display_url": "pic.twitter.com/3qGBIotew9",
+					"expanded_url": "https://twitter.com/tomgrissom/status/1080088644495572992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 585,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1033,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1499,
+							"h": 1290,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080087442479173600,
+					"id_str": "1080087442479173632",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv09zm9U0AAqroR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv09zm9U0AAqroR.jpg",
+					"url": "https://t.co/3qGBIotew9",
+					"display_url": "pic.twitter.com/3qGBIotew9",
+					"expanded_url": "https://twitter.com/tomgrissom/status/1080088644495572992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 585,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1033,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1499,
+							"h": 1290,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15348616,
+			"id_str": "15348616",
+			"name": "Tom Grissom",
+			"screen_name": "tomgrissom",
+			"location": "Illinois",
+			"description": "Educator, Thinker, Maker, Doer, Farmer, Ph.D. #MIEExpert - Day Job: Growing Minds - Views my own",
+			"url": "http://t.co/BQMHLqzjG8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/BQMHLqzjG8",
+							"expanded_url": "http://eiu.edu/itc",
+							"display_url": "eiu.edu/itc",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2751,
+			"friends_count": 2133,
+			"listed_count": 365,
+			"created_at": "Tue Jul 08 01:20:29 +0000 2008",
+			"favourites_count": 80,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22889,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1075126410442928129/T6n1RKqC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1075126410442928129/T6n1RKqC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15348616/1545164577",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New Year = New OneNote Notebook. \nHappy New Year Everyone! 🎆 \n\nMay you make a positive difference in the lives of those around you &amp; beyond. Keep on Learning!\n#OneNote #MIEExpert https://t.co/3qGBIotew9"
+	},
+	{
 		"created_at": "Tue Jan 01 13:09:34 +0000 2019",
 		"id": 1080088644495573000,
 		"id_str": "1080088644495572992",
