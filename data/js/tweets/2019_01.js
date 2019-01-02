906dc95b4285c5d9e25bf8fe6bbd460cfc98dc78
CleverClogs <github@cleverclogs.org>
2019-01-02 18:19:28 +0100

    Added 3 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,348 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 02 17:07:31 +0000 2019",
+		"id": 1080510915303350300,
+		"id_str": "1080510915303350273",
+		"full_text": "Fantastic demonstration of the power of #OneNote's unlimited canvas, colorized #digitalink and zooming features in this Spanish-spoken video by Profesor Jano:\n\nhttps://t.co/l5YbyJ7p9S\nby @ProfesorJano",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			200
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						40,
+						48
+					]
+				},
+				{
+					"text": "digitalink",
+					"indices": [
+						79,
+						90
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "profesorjano",
+					"name": "PROFESOR JANO",
+					"id": 105863820,
+					"id_str": "105863820",
+					"indices": [
+						187,
+						200
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/l5YbyJ7p9S",
+					"expanded_url": "https://www.youtube.com/watch?v=xC7NEjqY9BA",
+					"display_url": "youtube.com/watch?v=xC7NEj…",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19540,
+			"friends_count": 7236,
+			"listed_count": 599,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28295,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25052,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fantastic demonstration of the power of #OneNote's unlimited canvas, colorized #digitalink and zooming features in this Spanish-spoken video by Profesor Jano:\n\nhttps://t.co/l5YbyJ7p9S\nby @ProfesorJano"
+	},
+	{
+		"created_at": "Wed Jan 02 16:25:47 +0000 2019",
+		"id": 1080500412430594000,
+		"id_str": "1080500412430594048",
+		"full_text": "What better way to start the new year than with 10 Best #OneNote Tips?\nAustralian Microsoft Learning Consultant Pip Cleaves offers you her collection here:\n\nhttps://t.co/HpcpbSnerU\nby @PipCleaves\n#MicrosoftEDU https://t.co/U637TWfn63",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			209
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						56,
+						64
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						196,
+						209
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pipcleaves",
+					"name": "Pip Cleaves",
+					"id": 30757847,
+					"id_str": "30757847",
+					"indices": [
+						184,
+						195
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HpcpbSnerU",
+					"expanded_url": "https://medium.com/@pipcleaves/pips-top-ten-onenote-tips-1c40481953f",
+					"display_url": "medium.com/@pipcleaves/pi…",
+					"indices": [
+						157,
+						180
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1080499804902441000,
+					"id_str": "1080499804902440960",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv602QsWwAAqs8-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv602QsWwAAqs8-.jpg",
+					"url": "https://t.co/U637TWfn63",
+					"display_url": "pic.twitter.com/U637TWfn63",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1080500412430594048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1082,
+							"h": 769,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 483,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 769,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080499804902441000,
+					"id_str": "1080499804902440960",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv602QsWwAAqs8-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv602QsWwAAqs8-.jpg",
+					"url": "https://t.co/U637TWfn63",
+					"display_url": "pic.twitter.com/U637TWfn63",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1080500412430594048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1082,
+							"h": 769,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 483,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 769,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19540,
+			"friends_count": 7236,
+			"listed_count": 599,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28295,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25052,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What better way to start the new year than with 10 Best #OneNote Tips?\nAustralian Microsoft Learning Consultant Pip Cleaves offers you her collection here:\n\nhttps://t.co/HpcpbSnerU\nby @PipCleaves\n#MicrosoftEDU https://t.co/U637TWfn63"
+	},
+	{
 		"created_at": "Tue Jan 01 19:06:59 +0000 2019",
 		"id": 1080178589604008000,
 		"id_str": "1080178589604007936",
@@ -170,6 +512,230 @@ Grailbird.data.tweets_2019_01 =
 			"screen_name": "BuLisaBisa",
 			"location": "",
 			"description": "👩🏻‍🏫 Teacher • Student Counsellor • Lifelong Learner 💞🌏 #Wellbeing | #Mentalhealth | #21stcenturyskills | #PositiveEducation 🌱🌿🌳",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 373,
+			"friends_count": 250,
+			"listed_count": 7,
+			"created_at": "Thu Feb 08 08:35:25 +0000 2018",
+			"favourites_count": 997,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 632,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/961524854284431360/bxnqKKNf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/961524854284431360/bxnqKKNf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/961518821302398977/1518079509",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Sketchnote made using @msonenote @OneNoteC of ‘Make It Stick: The Science of Successful Learning’ 🧠⚡️\n\nShare or print out for your school community.😊\n\nFor more info see @cultofpedagogy interview with Peter Brown 💭\nhttps://t.co/JYq86Ts7kt\n#brainbasedlearning #retrievalpractice https://t.co/LpCVEvg0pF"
+	},
+	{
+		"created_at": "Tue Jan 01 19:06:59 +0000 2019",
+		"id": 1080178589604008000,
+		"id_str": "1080178589604007936",
+		"full_text": "#Sketchnote made using @msonenote @OneNoteC of ‘Make It Stick: The Science of Successful Learning’ 🧠⚡️\n\nShare or print out for your school community.😊\n\nFor more info see @cultofpedagogy interview with Peter Brown 💭\nhttps://t.co/JYq86Ts7kt\n#brainbasedlearning #retrievalpractice https://t.co/LpCVEvg0pF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Sketchnote",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"text": "brainbasedlearning",
+					"indices": [
+						239,
+						258
+					]
+				},
+				{
+					"text": "retrievalpractice",
+					"indices": [
+						259,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						23,
+						33
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						34,
+						43
+					]
+				},
+				{
+					"screen_name": "cultofpedagogy",
+					"name": "Jennifer Gonzalez",
+					"id": 1881304950,
+					"id_str": "1881304950",
+					"indices": [
+						170,
+						185
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JYq86Ts7kt",
+					"expanded_url": "http://ow.ly/UAuG30n1i3o",
+					"display_url": "ow.ly/UAuG30n1i3o",
+					"indices": [
+						215,
+						238
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1080178576828162000,
+					"id_str": "1080178576828162051",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"url": "https://t.co/LpCVEvg0pF",
+					"display_url": "pic.twitter.com/LpCVEvg0pF",
+					"expanded_url": "https://twitter.com/BuLisaBisa/status/1080178589604007936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080178576828162000,
+					"id_str": "1080178576828162051",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv2QsU3UYAMTQp6.jpg",
+					"url": "https://t.co/LpCVEvg0pF",
+					"display_url": "pic.twitter.com/LpCVEvg0pF",
+					"expanded_url": "https://twitter.com/BuLisaBisa/status/1080178589604007936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 961518821302399000,
+			"id_str": "961518821302398977",
+			"name": "Lisa",
+			"screen_name": "BuLisaBisa",
+			"location": "",
+			"description": "👩🏻🏫 Teacher • Student Counsellor • Lifelong Learner 💞🌏 #Wellbeing | #Mentalhealth | #21stcenturyskills | #PositiveEducation 🌱🌿🌳",
 			"url": null,
 			"entities": {
 				"description": {
