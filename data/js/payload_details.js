ebb35c4a02c732f33b7be39e51c183962be91c56
CleverClogs <github@cleverclogs.org>
2019-01-08 17:19:32 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30189,
-	"created_at": "Tue Jan 08 15:19:31 +0000 2019",
+	"tweets": 30191,
+	"created_at": "Tue Jan 08 16:19:31 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,635 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 08 15:37:11 +0000 2019",
+		"id": 1082662506479390700,
+		"id_str": "1082662506479390725",
+		"full_text": "Team TweetMeet is immensely proud of the impact our #MSFTEduChat Professional Development program has on the hosts every month. \n\nThis month's host @LynneOakvik put her opinion about into words in the blog post below.\nColor us amazed and grateful.\n#OneNote #MicrosoftTeams https://t.co/HEAOBOtJMd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						52,
+						64
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						248,
+						256
+					]
+				},
+				{
+					"text": "MicrosoftTeams",
+					"indices": [
+						257,
+						272
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LynneOakvik",
+					"name": "Lynne Oakvik #MSFTEduChat",
+					"id": 22850935,
+					"id_str": "22850935",
+					"indices": [
+						148,
+						160
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HEAOBOtJMd",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1082381320305692677",
+					"display_url": "twitter.com/TweetMeet/stat…",
+					"indices": [
+						273,
+						296
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19586,
+			"friends_count": 7243,
+			"listed_count": 599,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28318,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25070,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1082381320305692700,
+		"quoted_status_id_str": "1082381320305692677",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HEAOBOtJMd",
+			"expanded": "https://twitter.com/TweetMeet/status/1082381320305692677",
+			"display": "twitter.com/TweetMeet/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jan 07 20:59:51 +0000 2019",
+			"id": 1082381320305692700,
+			"id_str": "1082381320305692677",
+			"full_text": "Becoming an #MSFTEduChat host is a unique form of Professional Development. \nRead what January 15 TweetMeet host Lynne Oakvik says about our guidance, in this wonderful piece on her brand-new blog:\n\nhttps://t.co/SA7wmez3tS\nby @LynneOakvik \n#MicrosoftEDU #ChangeMakers #SoProud #PD https://t.co/GDFusBXCCv",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MSFTEduChat",
+						"indices": [
+							12,
+							24
+						]
+					},
+					{
+						"text": "MicrosoftEDU",
+						"indices": [
+							240,
+							253
+						]
+					},
+					{
+						"text": "ChangeMakers",
+						"indices": [
+							254,
+							267
+						]
+					},
+					{
+						"text": "SoProud",
+						"indices": [
+							268,
+							276
+						]
+					},
+					{
+						"text": "PD",
+						"indices": [
+							277,
+							280
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "LynneOakvik",
+						"name": "Lynne Oakvik #MSFTEduChat",
+						"id": 22850935,
+						"id_str": "22850935",
+						"indices": [
+							226,
+							238
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/SA7wmez3tS",
+						"expanded_url": "http://loak147.edublogs.org/2019/01/07/transforming-classroom-time-who-owns-the-learning/",
+						"display_url": "loak147.edublogs.org/2019/01/07/tra…",
+						"indices": [
+							199,
+							222
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1082380929660784600,
+						"id_str": "1082380929660784640",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/DwVjuIgWkAAsZJ5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DwVjuIgWkAAsZJ5.jpg",
+						"url": "https://t.co/GDFusBXCCv",
+						"display_url": "pic.twitter.com/GDFusBXCCv",
+						"expanded_url": "https://twitter.com/TweetMeet/status/1082381320305692677/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1860,
+								"h": 1055,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 681,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 386,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1082380929660784600,
+						"id_str": "1082380929660784640",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/DwVjuIgWkAAsZJ5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DwVjuIgWkAAsZJ5.jpg",
+						"url": "https://t.co/GDFusBXCCv",
+						"display_url": "pic.twitter.com/GDFusBXCCv",
+						"expanded_url": "https://twitter.com/TweetMeet/status/1082381320305692677/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1860,
+								"h": 1055,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 681,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 386,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15665199,
+				"id_str": "15665199",
+				"name": "TweetMeet Central #MSFTEduChat",
+				"screen_name": "TweetMeet",
+				"location": "World",
+				"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU about topics relevant to educators. \nTweets by Marjolein Hoekstra @OneNoteC",
+				"url": "https://t.co/vTmAj2KFoN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/vTmAj2KFoN",
+								"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+								"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1543,
+				"friends_count": 976,
+				"listed_count": 14,
+				"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+				"favourites_count": 851,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 460,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1545855124",
+				"profile_link_color": "E3008C",
+				"profile_sidebar_border_color": "DFDFDF",
+				"profile_sidebar_fill_color": "F3F3F3",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 10,
+			"favorite_count": 29,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Team TweetMeet is immensely proud of the impact our #MSFTEduChat Professional Development program has on the hosts every month. \n\nThis month's host @LynneOakvik put her opinion about into words in the blog post below.\nColor us amazed and grateful.\n#OneNote #MicrosoftTeams https://t.co/HEAOBOtJMd"
+	},
+	{
+		"created_at": "Mon Jan 07 20:24:40 +0000 2019",
+		"id": 1082372468881453000,
+		"id_str": "1082372468881453058",
+		"full_text": "Check out the latest #onslowdlt blog post to find out why you should \"Give @MicrosoftEDU Tools A Try 2019?\" @OneNoteEDU @OneNoteC @MicrosoftTeams @TeamsCentral @Flipgrid @sway https://t.co/ChLI5dJXX6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "onslowdlt",
+					"indices": [
+						21,
+						31
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MicrosoftEDU",
+					"name": "Microsoft Education",
+					"id": 17826187,
+					"id_str": "17826187",
+					"indices": [
+						75,
+						88
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						108,
+						119
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						120,
+						129
+					]
+				},
+				{
+					"screen_name": "MicrosoftTeams",
+					"name": "Microsoft Teams",
+					"id": 817461289081409500,
+					"id_str": "817461289081409536",
+					"indices": [
+						130,
+						145
+					]
+				},
+				{
+					"screen_name": "TeamsCentral",
+					"name": "Teams Central",
+					"id": 1020219219689033700,
+					"id_str": "1020219219689033729",
+					"indices": [
+						146,
+						159
+					]
+				},
+				{
+					"screen_name": "Flipgrid",
+					"name": "Flipgrid",
+					"id": 630608702,
+					"id_str": "630608702",
+					"indices": [
+						160,
+						169
+					]
+				},
+				{
+					"screen_name": "sway",
+					"name": "Office Sway",
+					"id": 129937699,
+					"id_str": "129937699",
+					"indices": [
+						170,
+						175
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1082372349897437200,
+					"id_str": "1082372349897437184",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DwVb6uZXcAAAcX1.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DwVb6uZXcAAAcX1.jpg",
+					"url": "https://t.co/ChLI5dJXX6",
+					"display_url": "pic.twitter.com/ChLI5dJXX6",
+					"expanded_url": "https://twitter.com/SachelleD/status/1082372468881453058/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 252,
+							"h": 166,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 252,
+							"h": 166,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 252,
+							"h": 166,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1082372349897437200,
+					"id_str": "1082372349897437184",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DwVb6uZXcAAAcX1.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DwVb6uZXcAAAcX1.jpg",
+					"url": "https://t.co/ChLI5dJXX6",
+					"display_url": "pic.twitter.com/ChLI5dJXX6",
+					"expanded_url": "https://twitter.com/SachelleD/status/1082372468881453058/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 252,
+							"h": 166,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 252,
+							"h": 166,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 252,
+							"h": 166,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							126,
+							83
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DwVb6uZXcAAAcX1.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 572855104,
+			"id_str": "572855104",
+			"name": "Sachelle",
+			"screen_name": "SachelleD",
+			"location": "",
+			"description": "Digital Learning and Teaching Facilitator, Microsoft Education Fangirl, Certified MIE, MIE Master Trainer",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 580,
+			"friends_count": 513,
+			"listed_count": 26,
+			"created_at": "Sun May 06 17:39:02 +0000 2012",
+			"favourites_count": 5349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2259,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081354797327020033/CuWqDmBH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081354797327020033/CuWqDmBH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/572855104/1546789419",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out the latest #onslowdlt blog post to find out why you should \"Give @MicrosoftEDU Tools A Try 2019?\" @OneNoteEDU @OneNoteC @MicrosoftTeams @TeamsCentral @Flipgrid @sway https://t.co/ChLI5dJXX6"
+	},
+	{
 		"created_at": "Mon Jan 07 20:24:40 +0000 2019",
 		"id": 1082372468881453000,
 		"id_str": "1082372468881453058",
