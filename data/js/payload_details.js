0d767455646dd0482b80c3a674d1d742a33643f2
CleverClogs <github@cleverclogs.org>
2019-01-11 07:19:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30319,
-	"created_at": "Fri Jan 11 05:19:31 +0000 2019",
+	"tweets": 30320,
+	"created_at": "Fri Jan 11 06:19:33 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -166,6 +166,252 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
+			"followers_count": 10402,
+			"friends_count": 8089,
+			"listed_count": 403,
+			"created_at": "Sat May 29 17:52:29 +0000 2010",
+			"favourites_count": 19837,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9087,
+			"lang": "nl",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "D0D0D0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/963590632886489088/qxR2ZWWa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/963590632886489088/qxR2ZWWa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/149593681/1512550696",
+			"profile_link_color": "D9650D",
+			"profile_sidebar_border_color": "0D0202",
+			"profile_sidebar_fill_color": "EDA426",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "e00b597ba0e3bd75",
+			"url": "https://api.twitter.com/1.1/geo/id/e00b597ba0e3bd75.json",
+			"place_type": "city",
+			"name": "Genk",
+			"full_name": "Genk, Belgium",
+			"country_code": "BE",
+			"country": "Belgium",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							5.3845337,
+							50.9132817
+						],
+						[
+							5.5952508,
+							50.9132817
+						],
+						[
+							5.5952508,
+							51.0206825
+						],
+						[
+							5.3845337,
+							51.0206825
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Excited to be part of the 10 most memorable news facts of 2018 in Belgian press. https://t.co/qjIxKTga5e @HLN #teachersmatter https://t.co/Cmvc25q2XT"
+	},
+	{
+		"created_at": "Thu Jan 10 20:58:51 +0000 2019",
+		"id": 1083468234253324300,
+		"id_str": "1083468234253324288",
+		"full_text": "Excited to be part of the 10 most memorable news facts of 2018 in Belgian press. https://t.co/qjIxKTga5e @HLN #teachersmatter https://t.co/Cmvc25q2XT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "teachersmatter",
+					"indices": [
+						110,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HLN",
+					"name": "Het Laatste Nieuws",
+					"id": 89146409,
+					"id_str": "89146409",
+					"indices": [
+						105,
+						109
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qjIxKTga5e",
+					"expanded_url": "https://m.hln.be/nieuws/2018-samengevat-in-10-memorabele-nieuwsberichten~a06c6fa1/",
+					"display_url": "m.hln.be/nieuws/2018-sa…",
+					"indices": [
+						81,
+						104
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1083468198685626400,
+					"id_str": "1083468198685626369",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/DwlAlhQW0AEin22.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwlAlhQW0AEin22.jpg",
+					"url": "https://t.co/Cmvc25q2XT",
+					"display_url": "pic.twitter.com/Cmvc25q2XT",
+					"expanded_url": "https://twitter.com/zelfstudie/status/1083468234253324288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1985,
+							"h": 1489,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1083468198685626400,
+					"id_str": "1083468198685626369",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/DwlAlhQW0AEin22.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwlAlhQW0AEin22.jpg",
+					"url": "https://t.co/Cmvc25q2XT",
+					"display_url": "pic.twitter.com/Cmvc25q2XT",
+					"expanded_url": "https://twitter.com/zelfstudie/status/1083468234253324288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1985,
+							"h": 1489,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 149593681,
+			"id_str": "149593681",
+			"name": "Koen Timmers",
+			"screen_name": "zelfstudie",
+			"location": "Belgium",
+			"description": "Author | Speaker | Lecturer | @TeacherPrize Top10 17/18 | Founder https://t.co/sQfH7MJ819 & @SDGlabschools | #MIEexpert | @NatGeo Educator | ♡ #EdTech #Sdgs",
+			"url": "https://t.co/CMArT0rZSU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CMArT0rZSU",
+							"expanded_url": "http://www.timmers.me",
+							"display_url": "timmers.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/sQfH7MJ819",
+							"expanded_url": "http://ProjectKakuma.com",
+							"display_url": "ProjectKakuma.com",
+							"indices": [
+								66,
+								89
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
 			"followers_count": 10401,
 			"friends_count": 8089,
 			"listed_count": 403,
