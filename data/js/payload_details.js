c7d711ec2cf7039e1febc10a4cf22fbde83bef55
CleverClogs <github@cleverclogs.org>
2019-01-14 13:19:34 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30346,
-	"created_at": "Sun Jan 13 21:19:33 +0000 2019",
+	"tweets": 30347,
+	"created_at": "Mon Jan 14 12:19:33 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -3,6 +3,219 @@ Grailbird.data.tweets_2019_01 =
 	{
 		"created_at": "Sun Jan 13 20:50:35 +0000 2019",
 		"id": 1084553319136354300,
+		"id_str": "1084776324860989440",
+		"full_text": "Want a gorgeous-looking 2019 Teacher Planner notebook? \nAustralian Microsoft Learning Consultants Pip Cleaves and Megan Townes have just made theirs available, tailored to Australian educators.\nFetch your own copy:\n\nhttps://t.co/Iq68QWEZPe\nby @PipCleaves @Townesy77\n#MicrosoftEDU https://t.co/tmp1i4c9Ue",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						266,
+						279
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pipcleaves",
+					"name": "Pip Cleaves",
+					"id": 30757847,
+					"id_str": "30757847",
+					"indices": [
+						243,
+						254
+					]
+				},
+				{
+					"screen_name": "townesy77",
+					"name": "Megan Townes",
+					"id": 27093634,
+					"id_str": "27093634",
+					"indices": [
+						255,
+						265
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Iq68QWEZPe",
+					"expanded_url": "https://bit.ly/2019tp",
+					"display_url": "bit.ly/2019tp",
+					"indices": [
+						216,
+						239
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1084553184260116500,
+					"id_str": "1084553184260116480",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw0bX_fX0AATOkW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw0bX_fX0AATOkW.jpg",
+					"url": "https://t.co/tmp1i4c9Ue",
+					"display_url": "pic.twitter.com/tmp1i4c9Ue",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1084553319136354304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1208,
+							"h": 823,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 463,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 818,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1084553184260116500,
+					"id_str": "1084553184260116480",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw0bX_fX0AATOkW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw0bX_fX0AATOkW.jpg",
+					"url": "https://t.co/tmp1i4c9Ue",
+					"display_url": "pic.twitter.com/tmp1i4c9Ue",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1084553319136354304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1208,
+							"h": 823,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 463,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 818,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19633,
+			"friends_count": 7246,
+			"listed_count": 600,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25097,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21,
+		"favorite_count": 49,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Want a gorgeous-looking 2019 Teacher Planner notebook? \nAustralian Microsoft Learning Consultants Pip Cleaves and Megan Townes have just made theirs available, tailored to Australian educators.\nFetch your own copy:\n\nhttps://t.co/Iq68QWEZPe\nby @PipCleaves @Townesy77\n#MicrosoftEDU https://t.co/tmp1i4c9Ue"
+	},
+	{
+		"created_at": "Sun Jan 13 20:50:35 +0000 2019",
+		"id": 1084553319136354300,
 		"id_str": "1084553319136354304",
 		"full_text": "Want a gorgeous-looking 2019 Teacher Planner notebook? \nAustralian Microsoft Learning Consultants Pip Cleaves and Megan Townes have just made theirs available, tailored to Australian educators.\nFetch your own copy:\n\nhttps://t.co/Iq68QWEZPe\nby @PipCleaves @Townesy77\n#MicrosoftEDU https://t.co/tmp1i4c9Ue",
 		"truncated": false,
