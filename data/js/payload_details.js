95c6d930d954d6656b04e28b45819d9979aa36f8
CleverClogs <github@cleverclogs.org>
2019-01-27 21:19:35 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30385,
-	"created_at": "Sun Jan 27 12:19:33 +0000 2019",
+	"tweets": 30386,
+	"created_at": "Sun Jan 27 20:19:34 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,399 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sun Jan 27 20:13:03 +0000 2019",
+		"id": 1089617302310608900,
+		"id_str": "1089617302310608897",
+		"full_text": "4 Years in the making, @Zelfstudie and I were finally able to arrange being present in the same place, at the same time.\nGood luck with your global project, Koen. Looking forward to your updates very much.\nThank you @Eustace_Stephen for the lovely picture.\n#MicrosoftEDU #BETT2019 https://t.co/8BHEBAqlEa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						257,
+						270
+					]
+				},
+				{
+					"text": "BETT2019",
+					"indices": [
+						271,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "zelfstudie",
+					"name": "Koen Timmers",
+					"id": 149593681,
+					"id_str": "149593681",
+					"indices": [
+						23,
+						34
+					]
+				},
+				{
+					"screen_name": "eustace_stephen",
+					"name": "Stephen Eustace",
+					"id": 705409101220675600,
+					"id_str": "705409101220675584",
+					"indices": [
+						216,
+						232
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8BHEBAqlEa",
+					"expanded_url": "https://twitter.com/eustace_stephen/status/1089137261747363841",
+					"display_url": "twitter.com/eustace_stephe…",
+					"indices": [
+						281,
+						304
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19809,
+			"friends_count": 7257,
+			"listed_count": 600,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28390,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25142,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1089137261747363800,
+		"quoted_status_id_str": "1089137261747363841",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8BHEBAqlEa",
+			"expanded": "https://twitter.com/eustace_stephen/status/1089137261747363841",
+			"display": "twitter.com/eustace_stephe…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jan 26 12:25:33 +0000 2019",
+			"id": 1089137261747363800,
+			"id_str": "1089137261747363841",
+			"full_text": "Inspiring discussion about Global 🌍 Education 📚 #Onederful People coming together can make a real difference!\n\n#Bett2019 #MicrosoftEDU #FlipgridFever #GlobalEd #teachSDGs #GlobalGoals #ProjectKakuma #Skypeclassroom #InnovationSDGLab 🚀 https://t.co/auw9V4wLM3",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				234
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Onederful",
+						"indices": [
+							48,
+							58
+						]
+					},
+					{
+						"text": "Bett2019",
+						"indices": [
+							111,
+							120
+						]
+					},
+					{
+						"text": "MicrosoftEDU",
+						"indices": [
+							121,
+							134
+						]
+					},
+					{
+						"text": "FlipgridFever",
+						"indices": [
+							135,
+							149
+						]
+					},
+					{
+						"text": "GlobalEd",
+						"indices": [
+							150,
+							159
+						]
+					},
+					{
+						"text": "teachSDGs",
+						"indices": [
+							160,
+							170
+						]
+					},
+					{
+						"text": "GlobalGoals",
+						"indices": [
+							171,
+							183
+						]
+					},
+					{
+						"text": "ProjectKakuma",
+						"indices": [
+							184,
+							198
+						]
+					},
+					{
+						"text": "Skypeclassroom",
+						"indices": [
+							199,
+							214
+						]
+					},
+					{
+						"text": "InnovationSDGLab",
+						"indices": [
+							215,
+							232
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1089137255854338000,
+						"id_str": "1089137255854338050",
+						"indices": [
+							235,
+							258
+						],
+						"media_url": "http://pbs.twimg.com/media/Dx1kkE7XcAIQdVQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dx1kkE7XcAIQdVQ.jpg",
+						"url": "https://t.co/auw9V4wLM3",
+						"display_url": "pic.twitter.com/auw9V4wLM3",
+						"expanded_url": "https://twitter.com/eustace_stephen/status/1089137261747363841/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1089137255854338000,
+						"id_str": "1089137255854338050",
+						"indices": [
+							235,
+							258
+						],
+						"media_url": "http://pbs.twimg.com/media/Dx1kkE7XcAIQdVQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dx1kkE7XcAIQdVQ.jpg",
+						"url": "https://t.co/auw9V4wLM3",
+						"display_url": "pic.twitter.com/auw9V4wLM3",
+						"expanded_url": "https://twitter.com/eustace_stephen/status/1089137261747363841/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 705409101220675600,
+				"id_str": "705409101220675584",
+				"name": "Stephen Eustace",
+				"screen_name": "eustace_stephen",
+				"location": "Ireland",
+				"description": "Teacher of Business 📊, Maths 🎲 and ICT 💻 Assistant Principal 💼 #MIEExpert #SurfaceExpert #MIEFellow #Onederful #FlipGridFever 🦄🚀",
+				"url": "https://t.co/zHTbFylUH9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/zHTbFylUH9",
+								"expanded_url": "http://www.onederful.eu",
+								"display_url": "onederful.eu",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 12182,
+				"friends_count": 10560,
+				"listed_count": 144,
+				"created_at": "Thu Mar 03 15:06:50 +0000 2016",
+				"favourites_count": 10911,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 7490,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1089514071504883712/zgevcdma_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1089514071504883712/zgevcdma_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/705409101220675584/1548619203",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 19,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "4 Years in the making, @Zelfstudie and I were finally able to arrange being present in the same place, at the same time.\nGood luck with your global project, Koen. Looking forward to your updates very much.\nThank you @Eustace_Stephen for the lovely picture.\n#MicrosoftEDU #BETT2019 https://t.co/8BHEBAqlEa"
+	},
+	{
 		"created_at": "Sat Jan 26 11:47:23 +0000 2019",
 		"id": 1089127659425222700,
 		"id_str": "1089485448299319296",
