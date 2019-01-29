7a340f26db61e645f10675f4406a722a7bb27cf4
CleverClogs <github@cleverclogs.org>
2019-01-29 15:19:36 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30390,
-	"created_at": "Tue Jan 29 01:19:34 +0000 2019",
+	"tweets": 30391,
+	"created_at": "Tue Jan 29 14:19:34 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 29 13:31:21 +0000 2019",
+		"id": 1090240984343023600,
+		"id_str": "1090240984343023617",
+		"full_text": "Attending #FETC? \nHere's a dynamic and interactive Twitter wall, showcasing all the #Microsoft goodness at FETC. Just tap, click, hover – or simply sit back and enjoy the flow:\n\nhttps://t.co/icjtUUHdts\nIs your tweet on it yet? \n#MicrosoftEDU #FETC19 #FETC2019 #notatFETC https://t.co/pl704s2YFf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FETC",
+					"indices": [
+						10,
+						15
+					]
+				},
+				{
+					"text": "Microsoft",
+					"indices": [
+						84,
+						94
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						228,
+						241
+					]
+				},
+				{
+					"text": "FETC19",
+					"indices": [
+						242,
+						249
+					]
+				},
+				{
+					"text": "FETC2019",
+					"indices": [
+						250,
+						259
+					]
+				},
+				{
+					"text": "notatFETC",
+					"indices": [
+						260,
+						270
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/icjtUUHdts",
+					"expanded_url": "https://www.tweetbeam.com/show?id=J7AAh",
+					"display_url": "tweetbeam.com/show?id=J7AAh",
+					"indices": [
+						178,
+						201
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1090239576646602800,
+					"id_str": "1090239576646602755",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/DyFPHl4X0AMCXzN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyFPHl4X0AMCXzN.jpg",
+					"url": "https://t.co/pl704s2YFf",
+					"display_url": "pic.twitter.com/pl704s2YFf",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1090240984343023617/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 910,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 910,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1090239576646602800,
+					"id_str": "1090239576646602755",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/DyFPHl4X0AMCXzN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyFPHl4X0AMCXzN.jpg",
+					"url": "https://t.co/pl704s2YFf",
+					"display_url": "pic.twitter.com/pl704s2YFf",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1090240984343023617/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 910,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 910,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19820,
+			"friends_count": 7266,
+			"listed_count": 600,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28391,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25146,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Attending #FETC? \nHere's a dynamic and interactive Twitter wall, showcasing all the #Microsoft goodness at FETC. Just tap, click, hover – or simply sit back and enjoy the flow:\n\nhttps://t.co/icjtUUHdts\nIs your tweet on it yet? \n#MicrosoftEDU #FETC19 #FETC2019 #notatFETC https://t.co/pl704s2YFf"
+	},
+	{
 		"created_at": "Sat Jan 26 11:47:23 +0000 2019",
 		"id": 1089127659425222700,
 		"id_str": "1090044032363966464",
