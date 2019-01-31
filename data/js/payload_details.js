e6328497dc2903327c0c8e8fc4f2200c59c3f5cf
CleverClogs <github@cleverclogs.org>
2019-01-31 19:19:35 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30403,
-	"created_at": "Thu Jan 31 15:19:34 +0000 2019",
+	"tweets": 30404,
+	"created_at": "Thu Jan 31 18:19:34 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 31 17:07:41 +0000 2019",
+		"id": 1091020204203786200,
+		"id_str": "1091031783242649601",
+		"full_text": "Don't miss this episode of The #Jenallee Show to hear what @justinchando, the Principal Product Manager with Microsoft Education, has to say about @MicrosoftTeams! It's the way of the future! 👉 https://t.co/oZCsY3xHK8  #EMSProud #TeamsMania #MIEExpert #TeamsMania https://t.co/8LhA8QeSqe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Jenallee",
+					"indices": [
+						31,
+						40
+					]
+				},
+				{
+					"text": "EMSProud",
+					"indices": [
+						219,
+						228
+					]
+				},
+				{
+					"text": "TeamsMania",
+					"indices": [
+						229,
+						240
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						241,
+						251
+					]
+				},
+				{
+					"text": "TeamsMania",
+					"indices": [
+						252,
+						263
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "justinchando",
+					"name": "Justin Chando",
+					"id": 108410718,
+					"id_str": "108410718",
+					"indices": [
+						59,
+						72
+					]
+				},
+				{
+					"screen_name": "MicrosoftTeams",
+					"name": "Microsoft Teams",
+					"id": 817461289081409500,
+					"id_str": "817461289081409536",
+					"indices": [
+						147,
+						162
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oZCsY3xHK8",
+					"expanded_url": "https://youtu.be/mo07q1uhJo0",
+					"display_url": "youtu.be/mo07q1uhJo0",
+					"indices": [
+						194,
+						217
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1091019032881291300,
+					"id_str": "1091019032881291264",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DyQUB6oUYAAvV3G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyQUB6oUYAAvV3G.jpg",
+					"url": "https://t.co/8LhA8QeSqe",
+					"display_url": "pic.twitter.com/8LhA8QeSqe",
+					"expanded_url": "https://twitter.com/jlo731/status/1091020204203786240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091019032881291300,
+					"id_str": "1091019032881291264",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DyQUB6oUYAAvV3G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyQUB6oUYAAvV3G.jpg",
+					"url": "https://t.co/8LhA8QeSqe",
+					"display_url": "pic.twitter.com/8LhA8QeSqe",
+					"expanded_url": "https://twitter.com/jlo731/status/1091020204203786240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95272661,
+			"id_str": "95272661",
+			"name": "Jeni Long",
+			"screen_name": "jlo731",
+			"location": "Fort Worth, TX",
+			"description": "ITS who loves inspiring others & has a passion for teaching & lrng w/tech! MIE Master Trainer #MIEExpert #OneNote lover #FETC #TCEA #MSFTEduChat @MicrosoftEDU",
+			"url": "https://t.co/5Z58JNJrUa",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5Z58JNJrUa",
+							"expanded_url": "http://www.youtube.com/Jenallee",
+							"display_url": "youtube.com/Jenallee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1030,
+			"friends_count": 1328,
+			"listed_count": 25,
+			"created_at": "Mon Dec 07 20:32:16 +0000 2009",
+			"favourites_count": 10144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2465,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1076138427177828355/FdGYpuKD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1076138427177828355/FdGYpuKD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95272661/1545394749",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "55b4f9e5c516e0b6",
+			"url": "https://api.twitter.com/1.1/geo/id/55b4f9e5c516e0b6.json",
+			"place_type": "city",
+			"name": "Orlando",
+			"full_name": "Orlando, FL",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-81.507905,
+							28.3882177
+						],
+						[
+							-81.2276403,
+							28.3882177
+						],
+						[
+							-81.2276403,
+							28.615139
+						],
+						[
+							-81.507905,
+							28.615139
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Don't miss this episode of The #Jenallee Show to hear what @justinchando, the Principal Product Manager with Microsoft Education, has to say about @MicrosoftTeams! It's the way of the future! 👉 https://t.co/oZCsY3xHK8  #EMSProud #TeamsMania #MIEExpert #TeamsMania https://t.co/8LhA8QeSqe"
+	},
+	{
 		"created_at": "Thu Jan 31 14:49:50 +0000 2019",
 		"id": 1090985511865409500,
 		"id_str": "1090985607038468096",
