2bef316793349be6d51f624f78123801d258d6b7
CleverClogs <github@cleverclogs.org>
2019-02-01 19:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30406,
-	"created_at": "Fri Feb 01 13:19:31 +0000 2019",
+	"tweets": 30407,
+	"created_at": "Fri Feb 01 18:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 01 17:43:07 +0000 2019",
+		"id": 1091391508140511200,
+		"id_str": "1091391508140511232",
+		"full_text": "Immersive Reader show time 🗣\nHave this classic English-pronunciation rhyme read aloud in #OneNote or through @MicrosoftEdge Reading View:\n\nhttps://t.co/QYwkr5ut0k\nComic poem 'The Chaos' by Gerard Nolst Trenité (1922)\n\n💡Tip: try various English dialects. Which do you prefer? https://t.co/U6iZYipXVZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						89,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MicrosoftEdge",
+					"name": "Microsoft Edge",
+					"id": 19315174,
+					"id_str": "19315174",
+					"indices": [
+						109,
+						123
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QYwkr5ut0k",
+					"expanded_url": "https://web.archive.org/web/20050415131319/http://www.spellingsociety.org/journals/j17/caos.php",
+					"display_url": "web.archive.org/web/2005041513…",
+					"indices": [
+						139,
+						162
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1091391168888430600,
+					"id_str": "1091391168888430592",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/DyVmfFSX4AApiDF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyVmfFSX4AApiDF.jpg",
+					"url": "https://t.co/U6iZYipXVZ",
+					"display_url": "pic.twitter.com/U6iZYipXVZ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1091391508140511232/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1054,
+							"h": 650,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 419,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1054,
+							"h": 650,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091391168888430600,
+					"id_str": "1091391168888430592",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/DyVmfFSX4AApiDF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyVmfFSX4AApiDF.jpg",
+					"url": "https://t.co/U6iZYipXVZ",
+					"display_url": "pic.twitter.com/U6iZYipXVZ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1091391508140511232/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1054,
+							"h": 650,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 419,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1054,
+							"h": 650,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19843,
+			"friends_count": 7295,
+			"listed_count": 600,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28406,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25165,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Immersive Reader show time 🗣\nHave this classic English-pronunciation rhyme read aloud in #OneNote or through @MicrosoftEdge Reading View:\n\nhttps://t.co/QYwkr5ut0k\nComic poem 'The Chaos' by Gerard Nolst Trenité (1922)\n\n💡Tip: try various English dialects. Which do you prefer? https://t.co/U6iZYipXVZ"
+	},
+	{
 		"created_at": "Fri Feb 01 12:40:41 +0000 2019",
 		"id": 1091315398463705100,
 		"id_str": "1091315398463705088",
