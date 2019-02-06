bebc03888420ef369c536c45ea4242a878c12154
CleverClogs <github@cleverclogs.org>
2019-02-06 17:19:32 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30411,
-	"created_at": "Wed Feb 06 12:19:28 +0000 2019",
+	"tweets": 30412,
+	"created_at": "Wed Feb 06 16:19:31 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,147 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Wed Feb 06 15:26:29 +0000 2019",
+		"id": 1093169064351096800,
+		"id_str": "1093169064351096832",
+		"full_text": "@EdBellman @msonenote @LauraCatPJs In #OneNOte for #Windows10 pressing keyboard shortcut Ctrl+Shift+E lets you share a copy of a notebook page. This includes email. Look for the words \"Send a copy\" at the bottom of the pane.\n\nUpdating the page creation date and time is only supported in OneNote 2016 for Windows.",
+		"truncated": false,
+		"display_text_range": [
+			35,
+			313
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNOte",
+					"indices": [
+						38,
+						46
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						51,
+						61
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EdBellman",
+					"name": "Ed Bellman",
+					"id": 104555787,
+					"id_str": "104555787",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						11,
+						21
+					]
+				},
+				{
+					"screen_name": "LauraCatPJs",
+					"name": "Laura Butler, Goddess of Engineers",
+					"id": 760954730612797400,
+					"id_str": "760954730612797440",
+					"indices": [
+						22,
+						34
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1093143889559605200,
+		"in_reply_to_status_id_str": "1093143889559605249",
+		"in_reply_to_user_id": 104555787,
+		"in_reply_to_user_id_str": "104555787",
+		"in_reply_to_screen_name": "EdBellman",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19871,
+			"friends_count": 7296,
+			"listed_count": 601,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28403,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25169,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@EdBellman @msonenote @LauraCatPJs In #OneNOte for #Windows10 pressing keyboard shortcut Ctrl+Shift+E lets you share a copy of a notebook page. This includes email. Look for the words \"Send a copy\" at the bottom of the pane.\n\nUpdating the page creation date and time is only supported in OneNote 2016 for Windows."
+	},
+	{
 		"created_at": "Wed Feb 06 11:57:38 +0000 2019",
 		"id": 1093116503724052500,
 		"id_str": "1093116503724052480",
