4ee3ec1267de8455d075773b615a9b40c4f64f9b
CleverClogs <github@cleverclogs.org>
2019-02-11 20:19:29 +0100

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 11 18:40:17 +0000 2019",
+		"id": 1095029772135120900,
+		"id_str": "1095029772135120897",
+		"full_text": "Everyone knows YouTube videos can be live-embedded in a #OneNote page, right?\n\nNow try these:\n1. YouTube playlists\n2. YouTube channel auto-playlists. Yes, these work too! Exciting isn't it?\n\nPlay the #Microsoft YouTube channel from your notebook:\nhttps://t.co/YurdsET71r\n#oEmbed",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						56,
+						64
+					]
+				},
+				{
+					"text": "Microsoft",
+					"indices": [
+						200,
+						210
+					]
+				},
+				{
+					"text": "oEmbed",
+					"indices": [
+						271,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YurdsET71r",
+					"expanded_url": "https://www.youtube.com/playlist?index=1&playnext=1&list=UUFtEEv80fQVKkD4h1PF-Xqw",
+					"display_url": "youtube.com/playlist?index…",
+					"indices": [
+						247,
+						270
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19915,
+			"friends_count": 7305,
+			"listed_count": 601,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28409,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25177,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1537441563",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Everyone knows YouTube videos can be live-embedded in a #OneNote page, right?\n\nNow try these:\n1. YouTube playlists\n2. YouTube channel auto-playlists. Yes, these work too! Exciting isn't it?\n\nPlay the #Microsoft YouTube channel from your notebook:\nhttps://t.co/YurdsET71r\n#oEmbed"
+	},
+	{
 		"created_at": "Sun Feb 10 18:36:01 +0000 2019",
 		"id": 1094666311576227800,
 		"id_str": "1094733359329263619",
