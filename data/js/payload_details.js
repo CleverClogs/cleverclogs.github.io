5e29e725bc84bc9f18ec6cf7d85037a2faa98332
CleverClogs <github@cleverclogs.org>
2019-02-20 18:19:34 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30443,
-	"created_at": "Wed Feb 20 14:19:28 +0000 2019",
+	"tweets": 30444,
+	"created_at": "Wed Feb 20 17:19:33 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Wed Feb 20 16:42:29 +0000 2019",
+		"id": 1098261620759937000,
+		"id_str": "1098261620759937024",
+		"full_text": "@HollandEdTech @jlo731 @tiktok Hey Holly, I think you meant to tag @TikTok_US, right?",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HollandEdTech",
+					"name": "Holly Holland",
+					"id": 3390194687,
+					"id_str": "3390194687",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "jlo731",
+					"name": "Jeni Long",
+					"id": 95272661,
+					"id_str": "95272661",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"screen_name": "tiktok",
+					"name": "TikTok",
+					"id": 799407,
+					"id_str": "799407",
+					"indices": [
+						23,
+						30
+					]
+				},
+				{
+					"screen_name": "tiktok_us",
+					"name": "TikTok",
+					"id": 2401980110,
+					"id_str": "2401980110",
+					"indices": [
+						67,
+						77
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1098259381622374400,
+		"in_reply_to_status_id_str": "1098259381622374403",
+		"in_reply_to_user_id": 3390194687,
+		"in_reply_to_user_id_str": "3390194687",
+		"in_reply_to_screen_name": "HollandEdTech",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19992,
+			"friends_count": 7318,
+			"listed_count": 603,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28451,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25202,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1550491006",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@HollandEdTech @jlo731 @tiktok Hey Holly, I think you meant to tag @TikTok_US, right?"
+	},
+	{
 		"created_at": "Wed Feb 20 13:35:48 +0000 2019",
 		"id": 1098214640604065800,
 		"id_str": "1098214640604065792",
