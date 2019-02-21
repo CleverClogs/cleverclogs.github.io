69e851dd670c61acaca41175fc08dd86da2bbddd
CleverClogs <github@cleverclogs.org>
2019-02-22 00:19:29 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30449,
-	"created_at": "Thu Feb 21 19:19:28 +0000 2019",
+	"tweets": 30450,
+	"created_at": "Thu Feb 21 23:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,137 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Thu Feb 21 23:07:48 +0000 2019",
+		"id": 1098720975640760300,
+		"id_str": "1098720975640760321",
+		"full_text": "@Cloughglass @Its_CCHogan Ready for an undocumented #OneNote tip?\nUse OneNote 2016 to apply a page template to a section. Test your template. Now sync and wait. \nSwitch to OneNote for #Windows10. Open the same section where you applied the template earlier. Now create a new page.\n\nLet me know if it works.",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			306
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						52,
+						60
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						184,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Cloughglass",
+					"name": "Hugh Meenagh",
+					"id": 740927546,
+					"id_str": "740927546",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "Its_CCHogan",
+					"name": "C. C. Hogan, Author",
+					"id": 1416197972,
+					"id_str": "1416197972",
+					"indices": [
+						13,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1098719385328541700,
+		"in_reply_to_status_id_str": "1098719385328541696",
+		"in_reply_to_user_id": 740927546,
+		"in_reply_to_user_id_str": "740927546",
+		"in_reply_to_screen_name": "Cloughglass",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20003,
+			"friends_count": 7321,
+			"listed_count": 603,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28452,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25207,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1550768614",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Cloughglass @Its_CCHogan Ready for an undocumented #OneNote tip?\nUse OneNote 2016 to apply a page template to a section. Test your template. Now sync and wait. \nSwitch to OneNote for #Windows10. Open the same section where you applied the template earlier. Now create a new page.\n\nLet me know if it works."
+	},
+	{
 		"created_at": "Thu Feb 21 18:37:34 +0000 2019",
 		"id": 1098652970219126800,
 		"id_str": "1098652970219126786",
