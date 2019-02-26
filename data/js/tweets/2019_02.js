f944f6a4c0d0783bd6cfeec9ca29662c460acc60
CleverClogs <github@cleverclogs.org>
2019-02-26 22:19:29 +0100

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 26 17:24:14 +0000 2019",
+		"id": 1100446453669871600,
+		"id_str": "1100504430296907776",
+		"full_text": "I just published my 1️⃣st medium article on \"Working Remotely Together\" and how we have been working in the Sticky Notes team across five geo-locations 🥳. Comments are welcome and RTs are appriciated. https://t.co/R9lPtBqtTO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/R9lPtBqtTO",
+					"expanded_url": "https://link.medium.com/hBWXtAXWCU",
+					"display_url": "link.medium.com/hBWXtAXWCU",
+					"indices": [
+						201,
+						224
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2246117322,
+			"id_str": "2246117322",
+			"name": "Reza Jooyandeh 🌈",
+			"screen_name": "RezaJooyandeh",
+			"location": "Vancouver, British Columbia",
+			"description": "Engineering Manager for Microsoft Sticky Notes Windows. Opinions are my own.🌈\nHe/Him/His",
+			"url": "https://t.co/FqWzmR0oSe",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FqWzmR0oSe",
+							"expanded_url": "http://www.jooyandeh.com",
+							"display_url": "jooyandeh.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 545,
+			"friends_count": 135,
+			"listed_count": 22,
+			"created_at": "Sat Dec 14 20:37:36 +0000 2013",
+			"favourites_count": 642,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 374,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/921411519648227328/3P_t0ehm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/921411519648227328/3P_t0ehm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2246117322/1530074267",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I just published my 1️⃣st medium article on \"Working Remotely Together\" and how we have been working in the Sticky Notes team across five geo-locations 🥳. Comments are welcome and RTs are appriciated. https://t.co/R9lPtBqtTO"
+	},
+	{
 		"created_at": "Tue Feb 26 18:30:09 +0000 2019",
 		"id": 1100463041127346200,
 		"id_str": "1100470843707019264",
