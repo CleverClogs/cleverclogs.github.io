3d71eab6c47a4f27dab99f9b71ba7776bfab133a
CleverClogs <github@cleverclogs.org>
2019-03-01 12:19:29 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30465,
-	"created_at": "Wed Feb 27 14:19:29 +0000 2019",
+	"tweets": 30466,
+	"created_at": "Fri Mar 01 11:19:27 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,237 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Fri Mar 01 11:19:22 +0000 2019",
+		"id": 1101441795802742800,
+		"id_str": "1101441795802742790",
+		"full_text": "Did you know @Sketchfab Collections with interactive 3D models can be live-embedded in #OneNote?\nSimply copy the web address of your #Sketchfab Collection and paste it into a OneNote page.\nExample by Guillermo Medrano:\n\nhttps://t.co/Zlp93cn5zi\nby #MIEExpert @GMedranoTIC\n#oEmbed https://t.co/UoynaqSz75",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						87,
+						95
+					]
+				},
+				{
+					"text": "Sketchfab",
+					"indices": [
+						133,
+						143
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						247,
+						257
+					]
+				},
+				{
+					"text": "oEmbed",
+					"indices": [
+						271,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Sketchfab",
+					"name": "Sketchfab",
+					"id": 421923426,
+					"id_str": "421923426",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"screen_name": "GmedranoTIC",
+					"name": "Guillermo Medrano",
+					"id": 783287228,
+					"id_str": "783287228",
+					"indices": [
+						258,
+						270
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Zlp93cn5zi",
+					"expanded_url": "https://sketchfab.com/gmedranotic/collections/network-tools",
+					"display_url": "sketchfab.com/gmedranotic/co…",
+					"indices": [
+						220,
+						243
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1101440688158371800,
+					"id_str": "1101440688158371841",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/D0kaeF7XcAEayst.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0kaeF7XcAEayst.jpg",
+					"url": "https://t.co/UoynaqSz75",
+					"display_url": "pic.twitter.com/UoynaqSz75",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1101441795802742790/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 877,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 497,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2010,
+							"h": 1469,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101440688158371800,
+					"id_str": "1101440688158371841",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/D0kaeF7XcAEayst.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0kaeF7XcAEayst.jpg",
+					"url": "https://t.co/UoynaqSz75",
+					"display_url": "pic.twitter.com/UoynaqSz75",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1101441795802742790/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 877,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 497,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2010,
+							"h": 1469,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20075,
+			"friends_count": 7361,
+			"listed_count": 603,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28523,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25224,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1550768614",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Did you know @Sketchfab Collections with interactive 3D models can be live-embedded in #OneNote?\nSimply copy the web address of your #Sketchfab Collection and paste it into a OneNote page.\nExample by Guillermo Medrano:\n\nhttps://t.co/Zlp93cn5zi\nby #MIEExpert @GMedranoTIC\n#oEmbed https://t.co/UoynaqSz75"
+	}
+]
