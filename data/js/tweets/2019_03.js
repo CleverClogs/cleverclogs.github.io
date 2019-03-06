1e977d820b7d4b58823f87a67510e405b7e007ec
CleverClogs <github@cleverclogs.org>
2019-03-06 21:19:29 +0100

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,368 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 19:50:55 +0000 2019",
+		"id": 1103382469364142100,
+		"id_str": "1103382469364142086",
+		"full_text": "The thrill of Mike Tholfsen's presentations is that he always announces exciting new features in Learning Tools and #OneNote. \nTeacher feedback is the main source of input for improvements. \n\nSo if you're in for an exciting #CUE19 talk, select @MTholfsen's from #MicrosoftEDU. https://t.co/Z0IDFLAdHh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						116,
+						124
+					]
+				},
+				{
+					"text": "CUE19",
+					"indices": [
+						224,
+						230
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						262,
+						275
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						244,
+						254
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Z0IDFLAdHh",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1103334968866963457",
+					"display_url": "twitter.com/mtholfsen/stat…",
+					"indices": [
+						277,
+						300
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20121,
+			"friends_count": 7370,
+			"listed_count": 604,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28531,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25243,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1550768614",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1103334968866963500,
+		"quoted_status_id_str": "1103334968866963457",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Z0IDFLAdHh",
+			"expanded": "https://twitter.com/mtholfsen/status/1103334968866963457",
+			"display": "twitter.com/mtholfsen/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 06 16:42:10 +0000 2019",
+			"id": 1103334968866963500,
+			"id_str": "1103334968866963457",
+			"full_text": "I'll be presenting at #CUE19 next week! 🌞  My session is titled \"Want to build and inclusive classroom? Learning Tools is your superpower!\" 🚀 #mieexpert #edtech #AssistiveTech #accessibility #dyslexia\n\nDetails below 👇 https://t.co/EWWBiLHyQK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				217
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "CUE19",
+						"indices": [
+							22,
+							28
+						]
+					},
+					{
+						"text": "mieexpert",
+						"indices": [
+							142,
+							152
+						]
+					},
+					{
+						"text": "edtech",
+						"indices": [
+							153,
+							160
+						]
+					},
+					{
+						"text": "AssistiveTech",
+						"indices": [
+							161,
+							175
+						]
+					},
+					{
+						"text": "accessibility",
+						"indices": [
+							176,
+							190
+						]
+					},
+					{
+						"text": "dyslexia",
+						"indices": [
+							191,
+							200
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1103334570768789500,
+						"id_str": "1103334570768789504",
+						"indices": [
+							218,
+							241
+						],
+						"media_url": "http://pbs.twimg.com/media/D0_U8kcU8AAnI_n.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D0_U8kcU8AAnI_n.jpg",
+						"url": "https://t.co/EWWBiLHyQK",
+						"display_url": "pic.twitter.com/EWWBiLHyQK",
+						"expanded_url": "https://twitter.com/mtholfsen/status/1103334968866963457/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 678,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1580,
+								"h": 893,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1103334570768789500,
+						"id_str": "1103334570768789504",
+						"indices": [
+							218,
+							241
+						],
+						"media_url": "http://pbs.twimg.com/media/D0_U8kcU8AAnI_n.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D0_U8kcU8AAnI_n.jpg",
+						"url": "https://t.co/EWWBiLHyQK",
+						"display_url": "pic.twitter.com/EWWBiLHyQK",
+						"expanded_url": "https://twitter.com/mtholfsen/status/1103334968866963457/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 678,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1580,
+								"h": 893,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 27673327,
+				"id_str": "27673327",
+				"name": "Mike Tholfsen",
+				"screen_name": "mtholfsen",
+				"location": "Redmond, WA",
+				"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, Learning Tools & #MicrosoftTeams #edtech #AssistiveTech @OneNoteEDU @MicrosoftEDU",
+				"url": "https://t.co/ANIFpSE2FB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ANIFpSE2FB",
+								"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+								"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16741,
+				"friends_count": 3224,
+				"listed_count": 708,
+				"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+				"favourites_count": 55775,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 27236,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+				"profile_link_color": "9266CC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 24,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The thrill of Mike Tholfsen's presentations is that he always announces exciting new features in Learning Tools and #OneNote. \nTeacher feedback is the main source of input for improvements. \n\nSo if you're in for an exciting #CUE19 talk, select @MTholfsen's from #MicrosoftEDU. https://t.co/Z0IDFLAdHh"
+	},
+	{
 		"created_at": "Wed Mar 06 17:02:15 +0000 2019",
 		"id": 1103339465580396500,
 		"id_str": "1103340024588918784",
