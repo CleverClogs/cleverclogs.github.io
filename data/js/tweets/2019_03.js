38cd9a120ad49bc65f079b2015f302c5f0e0087c
CleverClogs <github@cleverclogs.org>
2019-03-11 14:19:29 +0100

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 11 12:22:17 +0000 2019",
+		"id": 1105081506312212500,
+		"id_str": "1105081506312212480",
+		"full_text": "True passion for #MakeWhatsNext &amp; #STEM drives all hosts for the #MSFTEduChat TweetMeet of Tue March 19.\nMeet them all through the animated GIF below.\n\n⏰ 💡 Tip: double-check your local event time, as it may be different for you this month:\nhttps://t.co/Bb11Izfqlu\n#MicrosoftEDU https://t.co/J2yZ7aqeOz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MakeWhatsNext",
+					"indices": [
+						17,
+						31
+					]
+				},
+				{
+					"text": "STEM",
+					"indices": [
+						38,
+						43
+					]
+				},
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						69,
+						81
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						268,
+						281
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Bb11Izfqlu",
+					"expanded_url": "https://www.timeanddate.com/worldclock/fixedtime.html?low=c&p1=234&iso=20190319T10&msg=%23MSFTEduChat%20TweetMeet%20%e2%80%93%20March%2019%20%e2%80%93%20%23MakeWhatsNext%20%26%20STEM&ah=1&sort=1",
+					"display_url": "timeanddate.com/worldclock/fix…",
+					"indices": [
+						244,
+						267
+					]
+				},
+				{
+					"url": "https://t.co/J2yZ7aqeOz",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1104819598644391937",
+					"display_url": "twitter.com/TweetMeet/stat…",
+					"indices": [
+						282,
+						305
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20172,
+			"friends_count": 7372,
+			"listed_count": 605,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28531,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25258,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1550768614",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1104819598644392000,
+		"quoted_status_id_str": "1104819598644391937",
+		"quoted_status_permalink": {
+			"url": "https://t.co/J2yZ7aqeOz",
+			"expanded": "https://twitter.com/TweetMeet/status/1104819598644391937",
+			"display": "twitter.com/TweetMeet/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Mar 10 19:01:33 +0000 2019",
+			"id": 1104819598644392000,
+			"id_str": "1104819598644391937",
+			"full_text": "So thrilled to present the hosts for the #MSFTEduChat TweetMeet about #MakeWhatsNext &amp; #STEM of Tuesday, March 19.\nWatch the gorgeous slideshow below. \n\nTo follow each of these very passionate hosts, open this Twitter List:\nhttps://t.co/i9HHXIJi5W\n\n#MicrosoftEDU\nAnimated GIF: https://t.co/YQ3Q42FUNK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MSFTEduChat",
+						"indices": [
+							41,
+							53
+						]
+					},
+					{
+						"text": "MakeWhatsNext",
+						"indices": [
+							70,
+							84
+						]
+					},
+					{
+						"text": "STEM",
+						"indices": [
+							91,
+							96
+						]
+					},
+					{
+						"text": "MicrosoftEDU",
+						"indices": [
+							253,
+							266
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/i9HHXIJi5W",
+						"expanded_url": "https://twitter.com/tweetmeet/msfteduchat-2019-03",
+						"display_url": "twitter.com/tweetmeet/msft…",
+						"indices": [
+							228,
+							251
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1104817478910308400,
+						"id_str": "1104817478910308354",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/D1UZpLwXcAIi_jk.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D1UZpLwXcAIi_jk.jpg",
+						"url": "https://t.co/YQ3Q42FUNK",
+						"display_url": "pic.twitter.com/YQ3Q42FUNK",
+						"expanded_url": "https://twitter.com/TweetMeet/status/1104819598644391937/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 381,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 874,
+								"h": 490,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 874,
+								"h": 490,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1104817478910308400,
+						"id_str": "1104817478910308354",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/D1UZpLwXcAIi_jk.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D1UZpLwXcAIi_jk.jpg",
+						"url": "https://t.co/YQ3Q42FUNK",
+						"display_url": "pic.twitter.com/YQ3Q42FUNK",
+						"expanded_url": "https://twitter.com/TweetMeet/status/1104819598644391937/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 381,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 874,
+								"h": 490,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 874,
+								"h": 490,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								437,
+								245
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/D1UZpLwXcAIi_jk.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15665199,
+				"id_str": "15665199",
+				"name": "TweetMeet Central #MSFTEduChat",
+				"screen_name": "TweetMeet",
+				"location": "World",
+				"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU on topics relevant to educators. Team TweetMeet: @OneNoteC @fcotexeira & @AnicaTrickovic",
+				"url": "https://t.co/vTmAj2KFoN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/vTmAj2KFoN",
+								"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+								"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1921,
+				"friends_count": 1351,
+				"listed_count": 18,
+				"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+				"favourites_count": 1098,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 598,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1551090552",
+				"profile_link_color": "FF0090",
+				"profile_sidebar_border_color": "DFDFDF",
+				"profile_sidebar_fill_color": "F3F3F3",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 25,
+			"favorite_count": 56,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "True passion for #MakeWhatsNext &amp; #STEM drives all hosts for the #MSFTEduChat TweetMeet of Tue March 19.\nMeet them all through the animated GIF below.\n\n⏰ 💡 Tip: double-check your local event time, as it may be different for you this month:\nhttps://t.co/Bb11Izfqlu\n#MicrosoftEDU https://t.co/J2yZ7aqeOz"
+	},
+	{
 		"created_at": "Sun Mar 10 20:52:27 +0000 2019",
 		"id": 1104674065049157600,
 		"id_str": "1104847509493567489",
