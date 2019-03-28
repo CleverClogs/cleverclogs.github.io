46ed7290c8d48fcd05ac4039d42d9465275b5b2e
CleverClogs <github@cleverclogs.org>
2019-03-28 20:19:30 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30525,
-	"created_at": "Wed Mar 27 17:19:28 +0000 2019",
+	"tweets": 30526,
+	"created_at": "Thu Mar 28 19:19:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,108 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 28 19:07:41 +0000 2019",
+		"id": 1111312241297494000,
+		"id_str": "1111344122202329088",
+		"full_text": "Yo peeps. One of our top user asks on Samsung Note devices was the ability to use spen button to erase ink on pages. @msonenote Android now supports this. Do try out and let us know how it improves your inking experience.\n@OneNoteC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			231
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						117,
+						127
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						222,
+						231
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2933785406,
+			"id_str": "2933785406",
+			"name": "Amrita Rout",
+			"screen_name": "amrita_rout",
+			"location": "",
+			"description": "Product manager on the awesome NoTa, Notes and Tasks team at Microsoft. My views are my own....",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 135,
+			"friends_count": 73,
+			"listed_count": 4,
+			"created_at": "Wed Dec 17 17:18:16 +0000 2014",
+			"favourites_count": 247,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 192,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/545267895358324736/dFb9aw_C_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/545267895358324736/dFb9aw_C_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Yo peeps. One of our top user asks on Samsung Note devices was the ability to use spen button to erase ink on pages. @msonenote Android now supports this. Do try out and let us know how it improves your inking experience.\n@OneNoteC"
+	},
+	{
 		"created_at": "Wed Mar 27 16:58:02 +0000 2019",
 		"id": 1110944049567940600,
 		"id_str": "1110949109442195456",
