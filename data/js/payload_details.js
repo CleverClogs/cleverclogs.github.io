7a28b432b7b1bbc6ec1de737723907825ff0a53c
CleverClogs <github@cleverclogs.org>
2019-03-29 21:19:29 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30527,
-	"created_at": "Thu Mar 28 23:19:30 +0000 2019",
+	"tweets": 30528,
+	"created_at": "Fri Mar 29 20:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 29 20:02:37 +0000 2019",
+		"id": 1111720336553320400,
+		"id_str": "1111720336553320450",
+		"full_text": "@Dreamteam_MJ @edu_daily @kurtsoeser @OneNoteDE @OneNoteEDU @msonenote You can share a read-only #OneNote notebook anonymously through OneDrive for Consumers. Next, before recipients open the share link, have them sign in to their their personal Microsoft ID, so not a work or school account. This will make the \"Open in OneNote\" link available.",
+		"truncated": false,
+		"display_text_range": [
+			71,
+			345
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						97,
+						105
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Dreamteam_MJ",
+					"name": "Michael Jeschke",
+					"id": 967831070640140300,
+					"id_str": "967831070640140288",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "edu_daily",
+					"name": "Rolf Helbling",
+					"id": 1112419026,
+					"id_str": "1112419026",
+					"indices": [
+						14,
+						24
+					]
+				},
+				{
+					"screen_name": "kurtsoeser",
+					"name": "Kurt Söser",
+					"id": 55684861,
+					"id_str": "55684861",
+					"indices": [
+						25,
+						36
+					]
+				},
+				{
+					"screen_name": "OneNoteDE",
+					"name": "OneNoteDE",
+					"id": 3883996702,
+					"id_str": "3883996702",
+					"indices": [
+						37,
+						47
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						48,
+						59
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						60,
+						70
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1111631434891051000,
+		"in_reply_to_status_id_str": "1111631434891051010",
+		"in_reply_to_user_id": 967831070640140300,
+		"in_reply_to_user_id_str": "967831070640140288",
+		"in_reply_to_screen_name": "Dreamteam_MJ",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20367,
+			"friends_count": 7396,
+			"listed_count": 605,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28537,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25292,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1553353146",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Dreamteam_MJ @edu_daily @kurtsoeser @OneNoteDE @OneNoteEDU @msonenote You can share a read-only #OneNote notebook anonymously through OneDrive for Consumers. Next, before recipients open the share link, have them sign in to their their personal Microsoft ID, so not a work or school account. This will make the \"Open in OneNote\" link available."
+	},
+	{
 		"created_at": "Thu Mar 28 23:18:12 +0000 2019",
 		"id": 1111407168324927500,
 		"id_str": "1111407168324927489",
