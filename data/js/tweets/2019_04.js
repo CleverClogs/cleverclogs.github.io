c3f70edf9aac660ec14edbd6c5c8a13b6d62a12a
CleverClogs <github@cleverclogs.org>
2019-04-01 17:19:29 +0200

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 01 14:53:57 +0000 2019",
+		"id": 1112113072653721600,
+		"id_str": "1112729821925253120",
+		"full_text": "Spot #E2 attendee @MIEE_Flopsie 🦙!\nDynamic and interactive, this Twitter wall is hours of fun, with tweets from #MSFTEduChat hosts and hundreds of other global attendees in Paris  🗼.\n\nhttps://t.co/YRwUj0AtaL \n#MicrosoftEDU #MIEExpert #Road2E2\n\n🔎 P.S: are your tweets on it yet? https://t.co/cgBpMCHivh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "E2",
+					"indices": [
+						5,
+						8
+					]
+				},
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						112,
+						124
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						209,
+						222
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						223,
+						233
+					]
+				},
+				{
+					"text": "Road2E2",
+					"indices": [
+						234,
+						242
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MIEE_Flopsie",
+					"name": "Flopsie Llama",
+					"id": 997166863917367300,
+					"id_str": "997166863917367296",
+					"indices": [
+						18,
+						31
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/YRwUj0AtaL",
+					"expanded_url": "https://www.tweetbeam.com/show?id=xHtN3",
+					"display_url": "tweetbeam.com/show?id=xHtN3",
+					"indices": [
+						184,
+						207
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1112112871259934700,
+					"id_str": "1112112871259934726",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D28Ew6GWkAYoqmr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D28Ew6GWkAYoqmr.jpg",
+					"url": "https://t.co/cgBpMCHivh",
+					"display_url": "pic.twitter.com/cgBpMCHivh",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1112113072653721601/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 676,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1153,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112112871259934700,
+					"id_str": "1112112871259934726",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D28Ew6GWkAYoqmr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D28Ew6GWkAYoqmr.jpg",
+					"url": "https://t.co/cgBpMCHivh",
+					"display_url": "pic.twitter.com/cgBpMCHivh",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1112113072653721601/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 676,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1153,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat #E2",
+			"screen_name": "TweetMeet",
+			"location": "World",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU on topics relevant to educators. Team TweetMeet: @OneNoteC @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/vTmAj2KFoN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vTmAj2KFoN",
+							"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+							"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2071,
+			"friends_count": 1544,
+			"listed_count": 19,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 1234,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 682,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1553290210",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Spot #E2 attendee @MIEE_Flopsie 🦙!\nDynamic and interactive, this Twitter wall is hours of fun, with tweets from #MSFTEduChat hosts and hundreds of other global attendees in Paris  🗼.\n\nhttps://t.co/YRwUj0AtaL \n#MicrosoftEDU #MIEExpert #Road2E2\n\n🔎 P.S: are your tweets on it yet? https://t.co/cgBpMCHivh"
+	},
+	{
 		"created_at": "Mon Apr 01 08:38:13 +0000 2019",
 		"id": 1112635265871433700,
 		"id_str": "1112635265871433728",
