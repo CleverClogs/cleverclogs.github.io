eb6899df8e43c952b486a716c8aa7cee8042f9c6
CleverClogs <github@cleverclogs.org>
2019-04-11 01:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30555,
-	"created_at": "Wed Apr 10 14:19:29 +0000 2019",
+	"tweets": 30556,
+	"created_at": "Wed Apr 10 23:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 10 22:19:47 +0000 2019",
+		"id": 1116103509781491700,
+		"id_str": "1116103509781491712",
+		"full_text": "@lo_badiane And you'll get even more followers if you make your account public, Aminata. Looking forward to seeing your tweets ;-)",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			130
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lo_badiane",
+					"name": "Aminata Lo Badiane",
+					"id": 768192302204452900,
+					"id_str": "768192302204452864",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1115749800304160800,
+		"in_reply_to_status_id_str": "1115749800304160769",
+		"in_reply_to_user_id": 768192302204452900,
+		"in_reply_to_user_id_str": "768192302204452864",
+		"in_reply_to_screen_name": "lo_badiane",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20537,
+			"friends_count": 7474,
+			"listed_count": 606,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28598,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25319,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1553353146",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@lo_badiane And you'll get even more followers if you make your account public, Aminata. Looking forward to seeing your tweets ;-)"
+	},
+	{
 		"created_at": "Wed Apr 10 13:28:30 +0000 2019",
 		"id": 1115969808951119900,
 		"id_str": "1115969808951119872",
@@ -36,7 +142,7 @@ Grailbird.data.tweets_2019_04 =
 				},
 				{
 					"screen_name": "AkwyZ",
-					"name": "Antonio Vieira Santos 🙋🏽‍♂️#HM19 #Digitalization",
+					"name": "Antonio Vieira Santos 🙋🏽♂️#HM19 #Digitalization",
 					"id": 19485870,
 					"id_str": "19485870",
 					"indices": [
