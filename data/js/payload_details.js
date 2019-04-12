b37555a612197a482770fa0b21c0e14a45b2458b
CleverClogs <github@cleverclogs.org>
2019-04-12 20:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30561,
-	"created_at": "Fri Apr 12 17:19:29 +0000 2019",
+	"tweets": 30562,
+	"created_at": "Fri Apr 12 18:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,367 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 12 17:40:43 +0000 2019",
+		"id": 1116758055109185500,
+		"id_str": "1116758055109185537",
+		"full_text": "@alberto_hrv @mmarioherraez @eustace_stephen @SalleeClark @jlo731 @Flipgrid @Lanny_Watkins @kerszi @MicrosoftEDU @OneNoteEDU @mtholfsen @SkypeClassroom @Hacking_STEM @MicrosoftTeams @MSMakeCode @PlayCraftLearn @MicrosoftEdge @TweetMeet @fcotexeira @AnicaTrickovic Bonus must-follows:\n\n@MSFTEnable\n@jennylayfluffy \n@LBayne 👗\n\nwant even more? \n\nList with Microsoft Education accounts, incl #MIEFellow\nhttps://t.co/ODPg6KlSNN\n\nWhile you're at it, check the 500+ names on the \n#Microsoft Leaders and Brands Twitter List: \nhttps://t.co/4ftpqE0Mvx",
+		"truncated": false,
+		"display_text_range": [
+			264,
+			541
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MIEFellow",
+					"indices": [
+						388,
+						398
+					]
+				},
+				{
+					"text": "Microsoft",
+					"indices": [
+						473,
+						483
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "alberto_hrv",
+					"name": "Alberto Herraez, M.Ed",
+					"id": 852520911383085000,
+					"id_str": "852520911383085056",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "mmarioherraez",
+					"name": "Mario Herraez M.Ed",
+					"id": 902180372858888200,
+					"id_str": "902180372858888192",
+					"indices": [
+						13,
+						27
+					]
+				},
+				{
+					"screen_name": "eustace_stephen",
+					"name": "Stephen Eustace",
+					"id": 705409101220675600,
+					"id_str": "705409101220675584",
+					"indices": [
+						28,
+						44
+					]
+				},
+				{
+					"screen_name": "SalleeClark",
+					"name": "Sallee Clark",
+					"id": 2673808964,
+					"id_str": "2673808964",
+					"indices": [
+						45,
+						57
+					]
+				},
+				{
+					"screen_name": "jlo731",
+					"name": "Jeni Long",
+					"id": 95272661,
+					"id_str": "95272661",
+					"indices": [
+						58,
+						65
+					]
+				},
+				{
+					"screen_name": "Flipgrid",
+					"name": "Flipgrid",
+					"id": 630608702,
+					"id_str": "630608702",
+					"indices": [
+						66,
+						75
+					]
+				},
+				{
+					"screen_name": "Lanny_Watkins",
+					"name": "Paul 'Lanny' Watkins",
+					"id": 772415035,
+					"id_str": "772415035",
+					"indices": [
+						76,
+						90
+					]
+				},
+				{
+					"screen_name": "kerszi",
+					"name": "Kathi Kersznowski",
+					"id": 595079922,
+					"id_str": "595079922",
+					"indices": [
+						91,
+						98
+					]
+				},
+				{
+					"screen_name": "MicrosoftEDU",
+					"name": "Microsoft Education",
+					"id": 17826187,
+					"id_str": "17826187",
+					"indices": [
+						99,
+						112
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						113,
+						124
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						125,
+						135
+					]
+				},
+				{
+					"screen_name": "SkypeClassroom",
+					"name": "Skype Classroom",
+					"id": 214027732,
+					"id_str": "214027732",
+					"indices": [
+						136,
+						151
+					]
+				},
+				{
+					"screen_name": "Hacking_STEM",
+					"name": "Microsoft Hacking STEM",
+					"id": 806619118539444200,
+					"id_str": "806619118539444224",
+					"indices": [
+						152,
+						165
+					]
+				},
+				{
+					"screen_name": "MicrosoftTeams",
+					"name": "Microsoft Teams",
+					"id": 817461289081409500,
+					"id_str": "817461289081409536",
+					"indices": [
+						166,
+						181
+					]
+				},
+				{
+					"screen_name": "MSMakeCode",
+					"name": "Microsoft MakeCode",
+					"id": 853151395351937000,
+					"id_str": "853151395351937024",
+					"indices": [
+						182,
+						193
+					]
+				},
+				{
+					"screen_name": "PlayCraftLearn",
+					"name": "Minecraft Education",
+					"id": 3253758956,
+					"id_str": "3253758956",
+					"indices": [
+						194,
+						209
+					]
+				},
+				{
+					"screen_name": "MicrosoftEdge",
+					"name": "Microsoft Edge",
+					"id": 19315174,
+					"id_str": "19315174",
+					"indices": [
+						210,
+						224
+					]
+				},
+				{
+					"screen_name": "TweetMeet",
+					"name": "TweetMeet Central #MSFTEduChat",
+					"id": 15665199,
+					"id_str": "15665199",
+					"indices": [
+						225,
+						235
+					]
+				},
+				{
+					"screen_name": "fcotexeira",
+					"name": "Francisco Texeira",
+					"id": 488780635,
+					"id_str": "488780635",
+					"indices": [
+						236,
+						247
+					]
+				},
+				{
+					"screen_name": "AnicaTrickovic",
+					"name": "Anica Tričković #MSFTEduChat",
+					"id": 3062086342,
+					"id_str": "3062086342",
+					"indices": [
+						248,
+						263
+					]
+				},
+				{
+					"screen_name": "MSFTEnable",
+					"name": "MSFT Accessibility",
+					"id": 458619342,
+					"id_str": "458619342",
+					"indices": [
+						285,
+						296
+					]
+				},
+				{
+					"screen_name": "jennylayfluffy",
+					"name": "Jenny Lay-Flurrie",
+					"id": 885414019,
+					"id_str": "885414019",
+					"indices": [
+						297,
+						312
+					]
+				},
+				{
+					"screen_name": "LBayne",
+					"name": "Lindsay Bayne",
+					"id": 18954505,
+					"id_str": "18954505",
+					"indices": [
+						314,
+						321
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ODPg6KlSNN",
+					"expanded_url": "https://twitter.com/onenotec/microsoft-in-education/members",
+					"display_url": "twitter.com/onenotec/micro…",
+					"indices": [
+						399,
+						422
+					]
+				},
+				{
+					"url": "https://t.co/4ftpqE0Mvx",
+					"expanded_url": "https://twitter.com/onenotec/microsoft-leaders-brands/members",
+					"display_url": "twitter.com/onenotec/micro…",
+					"indices": [
+						518,
+						541
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1116744925670932500,
+		"in_reply_to_status_id_str": "1116744925670932481",
+		"in_reply_to_user_id": 2582768738,
+		"in_reply_to_user_id_str": "2582768738",
+		"in_reply_to_screen_name": "OneNoteC",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20565,
+			"friends_count": 7476,
+			"listed_count": 607,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28598,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25326,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1553353146",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@alberto_hrv @mmarioherraez @eustace_stephen @SalleeClark @jlo731 @Flipgrid @Lanny_Watkins @kerszi @MicrosoftEDU @OneNoteEDU @mtholfsen @SkypeClassroom @Hacking_STEM @MicrosoftTeams @MSMakeCode @PlayCraftLearn @MicrosoftEdge @TweetMeet @fcotexeira @AnicaTrickovic Bonus must-follows:\n\n@MSFTEnable\n@jennylayfluffy \n@LBayne 👗\n\nwant even more? \n\nList with Microsoft Education accounts, incl #MIEFellow\nhttps://t.co/ODPg6KlSNN\n\nWhile you're at it, check the 500+ names on the \n#Microsoft Leaders and Brands Twitter List: \nhttps://t.co/4ftpqE0Mvx"
+	},
+	{
 		"created_at": "Fri Apr 12 17:15:49 +0000 2019",
 		"id": 1116751790396121100,
 		"id_str": "1116751790396121088",
