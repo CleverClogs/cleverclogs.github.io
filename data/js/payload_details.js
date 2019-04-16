b970d3a6cc332b1c0b620a106ca541275a062184
CleverClogs <github@cleverclogs.org>
2019-04-16 12:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30569,
-	"created_at": "Tue Apr 16 08:19:28 +0000 2019",
+	"tweets": 30570,
+	"created_at": "Tue Apr 16 10:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 16 10:01:18 +0000 2019",
+		"id": 1118087104511316000,
+		"id_str": "1118091989751410688",
+		"full_text": "💖 We'd just love to know. Please answer our quick Twitter poll:\nAre you joining the #MSFTEduChat TweetMeet today? Our topic is #TeachingHappiness.\n\nTo learn more, go to the blog:\nhttps://t.co/TqtuBqgq54\n\n#MicrosoftEDU #SEL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						84,
+						96
+					]
+				},
+				{
+					"text": "TeachingHappiness",
+					"indices": [
+						127,
+						145
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						204,
+						217
+					]
+				},
+				{
+					"text": "SEL",
+					"indices": [
+						218,
+						222
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TqtuBqgq54",
+					"expanded_url": "https://educationblog.microsoft.com/en-us/2019/04/teaching-happiness-around-the-world-in-the-msfteduchat-tweetmeet-on-april-16/",
+					"display_url": "educationblog.microsoft.com/en-us/2019/04/…",
+					"indices": [
+						179,
+						202
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat",
+			"screen_name": "TweetMeet",
+			"location": "World",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU on topics relevant to educators. Team TweetMeet: @OneNoteC @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/vTmAj2KFoN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vTmAj2KFoN",
+							"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+							"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2226,
+			"friends_count": 1855,
+			"listed_count": 21,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 1359,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 722,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/988509178615549952/hwFi4JQp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1555344980",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "💖 We'd just love to know. Please answer our quick Twitter poll:\nAre you joining the #MSFTEduChat TweetMeet today? Our topic is #TeachingHappiness.\n\nTo learn more, go to the blog:\nhttps://t.co/TqtuBqgq54\n\n#MicrosoftEDU #SEL"
+	},
+	{
 		"created_at": "Tue Apr 16 07:55:13 +0000 2019",
 		"id": 1118060259657048000,
 		"id_str": "1118060259657048065",
