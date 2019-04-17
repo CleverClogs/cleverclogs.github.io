f2929045707d5a9f0d84ecefaeff256e805386e3
CleverClogs <github@cleverclogs.org>
2019-04-17 17:19:32 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30580,
-	"created_at": "Wed Apr 17 11:19:28 +0000 2019",
+	"tweets": 30581,
+	"created_at": "Wed Apr 17 15:19:31 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,161 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 17 15:10:49 +0000 2019",
+		"id": 1118532271970095100,
+		"id_str": "1118532271970095104",
+		"full_text": "Introducing the #Microsoft #Surface Hub 2S.\nMicrosoft has just announced this new, instantaneous #MicrosoftTeams-ready #SurfaceHub model.\n\nStunning promo video:\nhttps://t.co/Z6oXCBlpNY\nMore details here: https://t.co/gmduB39Wnq\nby @Surface",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			239
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Microsoft",
+					"indices": [
+						16,
+						26
+					]
+				},
+				{
+					"text": "Surface",
+					"indices": [
+						27,
+						35
+					]
+				},
+				{
+					"text": "MicrosoftTeams",
+					"indices": [
+						97,
+						112
+					]
+				},
+				{
+					"text": "SurfaceHub",
+					"indices": [
+						119,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "surface",
+					"name": "Microsoft Surface",
+					"id": 612076511,
+					"id_str": "612076511",
+					"indices": [
+						231,
+						239
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Z6oXCBlpNY",
+					"expanded_url": "https://www.youtube.com/watch?v=iWVwhC_8aLY",
+					"display_url": "youtube.com/watch?v=iWVwhC…",
+					"indices": [
+						161,
+						184
+					]
+				},
+				{
+					"url": "https://t.co/gmduB39Wnq",
+					"expanded_url": "https://www.microsoft.com/en-us/surface/business/surface-hub-2",
+					"display_url": "microsoft.com/en-us/surface/…",
+					"indices": [
+						204,
+						227
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20625,
+			"friends_count": 7485,
+			"listed_count": 611,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28598,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25348,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1553353146",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Introducing the #Microsoft #Surface Hub 2S.\nMicrosoft has just announced this new, instantaneous #MicrosoftTeams-ready #SurfaceHub model.\n\nStunning promo video:\nhttps://t.co/Z6oXCBlpNY\nMore details here: https://t.co/gmduB39Wnq\nby @Surface"
+	},
+	{
 		"created_at": "Wed Apr 17 10:50:37 +0000 2019",
 		"id": 1118466791070732300,
 		"id_str": "1118466791070732289",
