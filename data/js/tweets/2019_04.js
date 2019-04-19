35659752cd79c31c6de151e7420bcc44bd83b9ed
CleverClogs <github@cleverclogs.org>
2019-04-19 11:19:29 +0200

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 19 09:09:39 +0000 2019",
+		"id": 1119166156223524900,
+		"id_str": "1119166156223524864",
+		"full_text": "@kyteijas @BeckyKeene @mtholfsen @wakelet @msonenote @NotedByRyan @JBDbiz Can you please keep me in the loop on the development status of an oEmbed endpoint in the #Wakelet API?\nAlways interested in #oEmbed live-embed implementations in #OneNote.\nOh, and don't forget #Sway - would be great to have your iFrame whitelisted there too. \n#WakeletWave",
+		"truncated": false,
+		"display_text_range": [
+			74,
+			347
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Wakelet",
+					"indices": [
+						164,
+						172
+					]
+				},
+				{
+					"text": "oEmbed",
+					"indices": [
+						199,
+						206
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						237,
+						245
+					]
+				},
+				{
+					"text": "Sway",
+					"indices": [
+						268,
+						273
+					]
+				},
+				{
+					"text": "WakeletWave",
+					"indices": [
+						335,
+						347
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kyteijas",
+					"name": "Teijas @ Wakelet",
+					"id": 308328117,
+					"id_str": "308328117",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "BeckyKeene",
+					"name": "Becky Keene",
+					"id": 253925945,
+					"id_str": "253925945",
+					"indices": [
+						10,
+						21
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						22,
+						32
+					]
+				},
+				{
+					"screen_name": "wakelet",
+					"name": "Wakelet",
+					"id": 169922611,
+					"id_str": "169922611",
+					"indices": [
+						33,
+						41
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						42,
+						52
+					]
+				},
+				{
+					"screen_name": "NotedByRyan",
+					"name": "Ryan McMinn",
+					"id": 729049944133226500,
+					"id_str": "729049944133226496",
+					"indices": [
+						53,
+						65
+					]
+				},
+				{
+					"screen_name": "JBDbiz",
+					"name": "James Davis",
+					"id": 3883306341,
+					"id_str": "3883306341",
+					"indices": [
+						66,
+						73
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1118807950565552100,
+		"in_reply_to_status_id_str": "1118807950565552128",
+		"in_reply_to_user_id": 308328117,
+		"in_reply_to_user_id_str": "308328117",
+		"in_reply_to_screen_name": "kyteijas",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20650,
+			"friends_count": 7488,
+			"listed_count": 611,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28606,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25353,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1553353146",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@kyteijas @BeckyKeene @mtholfsen @wakelet @msonenote @NotedByRyan @JBDbiz Can you please keep me in the loop on the development status of an oEmbed endpoint in the #Wakelet API?\nAlways interested in #oEmbed live-embed implementations in #OneNote.\nOh, and don't forget #Sway - would be great to have your iFrame whitelisted there too. \n#WakeletWave"
+	},
+	{
 		"created_at": "Fri Apr 19 08:17:11 +0000 2019",
 		"id": 1117137459287089200,
 		"id_str": "1119152952902438912",
