42d47c23c1909c1b234c21e12817a06d04820470
CleverClogs <github@cleverclogs.org>
2019-04-19 14:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30586,
-	"created_at": "Fri Apr 19 09:19:28 +0000 2019",
+	"tweets": 30587,
+	"created_at": "Fri Apr 19 12:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 19 11:31:44 +0000 2019",
+		"id": 1119201912123826200,
+		"id_str": "1119201912123826176",
+		"full_text": "Why the #OneNote app for iOS is a student's ideal companion.\nA free and flexible digital binder for all your study notes that synchronizes between devices. What's not to like about OneNote on an iPhone or iPad?\n\nhttps://t.co/9bLKOAqxNJ\n[Dutch] by @LarsPaymans for @iPhoned",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						8,
+						16
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LarsPaymans",
+					"name": "Lars Paymans",
+					"id": 22642796,
+					"id_str": "22642796",
+					"indices": [
+						247,
+						259
+					]
+				},
+				{
+					"screen_name": "iphoned",
+					"name": "iPhoned.nl",
+					"id": 14369793,
+					"id_str": "14369793",
+					"indices": [
+						264,
+						272
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9bLKOAqxNJ",
+					"expanded_url": "https://www.iphoned.nl/apps/microsoft-onenote-ios-oogappel/",
+					"display_url": "iphoned.nl/apps/microsoft…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20652,
+			"friends_count": 7489,
+			"listed_count": 611,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28607,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25354,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1553353146",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Why the #OneNote app for iOS is a student's ideal companion.\nA free and flexible digital binder for all your study notes that synchronizes between devices. What's not to like about OneNote on an iPhone or iPad?\n\nhttps://t.co/9bLKOAqxNJ\n[Dutch] by @LarsPaymans for @iPhoned"
+	},
+	{
 		"created_at": "Fri Apr 19 09:09:39 +0000 2019",
 		"id": 1119166156223524900,
 		"id_str": "1119166156223524864",
