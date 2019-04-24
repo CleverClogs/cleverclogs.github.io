92820d6f2cec99f382ee23b9e34009afa2369ad7
CleverClogs <github@cleverclogs.org>
2019-04-24 18:19:30 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30595,
-	"created_at": "Wed Apr 24 12:19:29 +0000 2019",
+	"tweets": 30596,
+	"created_at": "Wed Apr 24 16:19:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,278 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 24 16:08:05 +0000 2019",
+		"id": 1121082083269664800,
+		"id_str": "1121083397122723841",
+		"full_text": "BIG NEWS! Announcing @Wakelet and #OneNote integration 🌊💜 From Teacher tweet to reality in less than a week  😱 Thanks to @BeckyKeene 🙌  #edtech #MIEExpert #WakeletWave #MicrosoftEDU\n\nBackstory &amp; details here: https://t.co/RdmjdbNoJD \n\nSee the integration in video below 👇 https://t.co/frmbTcf0uU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						34,
+						42
+					]
+				},
+				{
+					"text": "edtech",
+					"indices": [
+						136,
+						143
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						144,
+						154
+					]
+				},
+				{
+					"text": "WakeletWave",
+					"indices": [
+						155,
+						167
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						168,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wakelet",
+					"name": "Wakelet",
+					"id": 169922611,
+					"id_str": "169922611",
+					"indices": [
+						21,
+						29
+					]
+				},
+				{
+					"screen_name": "BeckyKeene",
+					"name": "Becky Keene",
+					"id": 253925945,
+					"id_str": "253925945",
+					"indices": [
+						121,
+						132
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RdmjdbNoJD",
+					"expanded_url": "http://aka.ms/WakeletOneNote",
+					"display_url": "aka.ms/WakeletOneNote",
+					"indices": [
+						213,
+						236
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1121081042494984200,
+					"id_str": "1121081042494984193",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1121081042494984193/pu/img/fXGG3DuXssr-6i5B.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1121081042494984193/pu/img/fXGG3DuXssr-6i5B.jpg",
+					"url": "https://t.co/frmbTcf0uU",
+					"display_url": "pic.twitter.com/frmbTcf0uU",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1121082083269664768/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1068,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1068,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 458,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1121081042494984200,
+					"id_str": "1121081042494984193",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1121081042494984193/pu/img/fXGG3DuXssr-6i5B.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1121081042494984193/pu/img/fXGG3DuXssr-6i5B.jpg",
+					"url": "https://t.co/frmbTcf0uU",
+					"display_url": "pic.twitter.com/frmbTcf0uU",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1121082083269664768/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1068,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1068,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 458,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							89,
+							60
+						],
+						"duration_millis": 15767,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121081042494984193/pu/vid/534x360/pq0GKMTpjK7GlHnO.mp4?tag=8"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121081042494984193/pu/vid/1068x720/W4BV58m_LpwlbmZ6.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1121081042494984193/pu/pl/3iJk8HnZoWqDvwqW.m3u8?tag=8"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121081042494984193/pu/vid/266x180/Nta2b4DVI9oP_n5c.mp4?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, Learning Tools & #MicrosoftTeams #edtech #E2 #Road2E2 @OneNoteEDU @MicrosoftEDU",
+			"url": "https://t.co/ANIFpSE2FB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSE2FB",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17684,
+			"friends_count": 3181,
+			"listed_count": 720,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 60076,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28347,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BIG NEWS! Announcing @Wakelet and #OneNote integration 🌊💜 From Teacher tweet to reality in less than a week  😱 Thanks to @BeckyKeene 🙌  #edtech #MIEExpert #WakeletWave #MicrosoftEDU\n\nBackstory &amp; details here: https://t.co/RdmjdbNoJD \n\nSee the integration in video below 👇 https://t.co/frmbTcf0uU"
+	},
+	{
 		"created_at": "Wed Apr 24 12:16:39 +0000 2019",
 		"id": 1121025156405694500,
 		"id_str": "1121025156405694464",
