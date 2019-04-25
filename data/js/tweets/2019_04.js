0e7393143b6f5f3752231b1c381bc0e63be5d688
CleverClogs <github@cleverclogs.org>
2019-04-25 11:19:31 +0200

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 25 08:46:56 +0000 2019",
+		"id": 1121178154280869900,
+		"id_str": "1121334765104390144",
+		"full_text": "#OneNote Class Notebook: a sticker for every idea!  #MIEExpert #FlipgridFever @Taralson @design2research @OneNoteC\n\nLearn more 👉 https://t.co/4QwjsfCvhD  \n\n#StudentVoiceDay 💚💜 https://t.co/Sl9vbyqmsQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						52,
+						62
+					]
+				},
+				{
+					"text": "FlipgridFever",
+					"indices": [
+						63,
+						77
+					]
+				},
+				{
+					"text": "StudentVoiceDay",
+					"indices": [
+						156,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Taralson",
+					"name": "Joey Taralson",
+					"id": 235298575,
+					"id_str": "235298575",
+					"indices": [
+						78,
+						87
+					]
+				},
+				{
+					"screen_name": "design2research",
+					"name": "Dr. Charlie Miller",
+					"id": 66801342,
+					"id_str": "66801342",
+					"indices": [
+						88,
+						104
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						105,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4QwjsfCvhD",
+					"expanded_url": "http://msft.social/5djn8T",
+					"display_url": "msft.social/5djn8T",
+					"indices": [
+						129,
+						152
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1121178003906732000,
+					"id_str": "1121178003906732032",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1121178003906732032/pu/img/rwDd0WoIBYf8M63J.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1121178003906732032/pu/img/rwDd0WoIBYf8M63J.jpg",
+					"url": "https://t.co/Sl9vbyqmsQ",
+					"display_url": "pic.twitter.com/Sl9vbyqmsQ",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1121178154280869890/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1121178003906732000,
+					"id_str": "1121178003906732032",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1121178003906732032/pu/img/rwDd0WoIBYf8M63J.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1121178003906732032/pu/img/rwDd0WoIBYf8M63J.jpg",
+					"url": "https://t.co/Sl9vbyqmsQ",
+					"display_url": "pic.twitter.com/Sl9vbyqmsQ",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1121178154280869890/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 9500,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121178003906732032/pu/vid/640x360/dvKTzMEiFwm9noC1.mp4?tag=8"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121178003906732032/pu/vid/320x180/Y90NJMzm_Oe9a8uv.mp4?tag=8"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121178003906732032/pu/vid/1280x720/y7EVyiJ4XgD5-V_a.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1121178003906732032/pu/pl/2ZMnIMDSA8vqcscs.m3u8?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, Learning Tools & #MicrosoftTeams #edtech #E2 #Road2E2 @OneNoteEDU @MicrosoftEDU",
+			"url": "https://t.co/ANIFpSE2FB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSE2FB",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17707,
+			"friends_count": 3181,
+			"listed_count": 720,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 60319,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28386,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 32,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#OneNote Class Notebook: a sticker for every idea!  #MIEExpert #FlipgridFever @Taralson @design2research @OneNoteC\n\nLearn more 👉 https://t.co/4QwjsfCvhD  \n\n#StudentVoiceDay 💚💜 https://t.co/Sl9vbyqmsQ"
+	},
+	{
 		"created_at": "Wed Apr 24 20:34:06 +0000 2019",
 		"id": 1121150345386893300,
 		"id_str": "1121150345386893312",
