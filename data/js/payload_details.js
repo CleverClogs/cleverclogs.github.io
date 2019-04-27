38822bdb1d5edc0f4c35647556f00e04f9cef1b3
CleverClogs <github@cleverclogs.org>
2019-04-27 21:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30606,
-	"created_at": "Sat Apr 27 18:19:28 +0000 2019",
+	"tweets": 30607,
+	"created_at": "Sat Apr 27 19:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 27 19:12:09 +0000 2019",
+		"id": 1122216882101280800,
+		"id_str": "1122216882101280770",
+		"full_text": "Quick Pad is a simple, 3rd-party rich-text editor for #Windows10 using #FluentDesign.\nYou can get this UWP app for free from the Microsoft Store:\n\nhttps://t.co/6vVOhk1puW\nby Yair Aichenbaum for ATec Solution\n\n💡Tip: for a limited time you can remove the in-app advertisements. https://t.co/mkeLKeHwmg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Windows10",
+					"indices": [
+						54,
+						64
+					]
+				},
+				{
+					"text": "FluentDesign",
+					"indices": [
+						71,
+						84
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6vVOhk1puW",
+					"expanded_url": "https://www.microsoft.com/store/productId/9PDLWQHTLSV3",
+					"display_url": "microsoft.com/store/productI…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1122215746640150500,
+					"id_str": "1122215746640150528",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D5LpRpvWAAAe4JI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5LpRpvWAAAe4JI.jpg",
+					"url": "https://t.co/mkeLKeHwmg",
+					"display_url": "pic.twitter.com/mkeLKeHwmg",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1122216882101280770/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1877,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 623,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1122215746640150500,
+					"id_str": "1122215746640150528",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D5LpRpvWAAAe4JI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5LpRpvWAAAe4JI.jpg",
+					"url": "https://t.co/mkeLKeHwmg",
+					"display_url": "pic.twitter.com/mkeLKeHwmg",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1122216882101280770/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1877,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 623,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20776,
+			"friends_count": 7506,
+			"listed_count": 614,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28670,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25374,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Quick Pad is a simple, 3rd-party rich-text editor for #Windows10 using #FluentDesign.\nYou can get this UWP app for free from the Microsoft Store:\n\nhttps://t.co/6vVOhk1puW\nby Yair Aichenbaum for ATec Solution\n\n💡Tip: for a limited time you can remove the in-app advertisements. https://t.co/mkeLKeHwmg"
+	},
+	{
 		"created_at": "Sat Apr 27 18:14:18 +0000 2019",
 		"id": 1122202324431245300,
 		"id_str": "1122202324431245313",
