3405f6bbd6b75aba36b3c70408252fb3256cc401
CleverClogs <github@cleverclogs.org>
2019-04-29 15:19:30 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30611,
-	"created_at": "Sun Apr 28 18:19:28 +0000 2019",
+	"tweets": 30612,
+	"created_at": "Mon Apr 29 13:19:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,161 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 29 13:01:56 +0000 2019",
+		"id": 1122848492840468500,
+		"id_str": "1122848492840468481",
+		"full_text": "@Opilin @OneNoteDE @OneNoteEDU @kurtsoeser Was this a #OneNote notebook created through Microsoft Teams, Philipp? Was this a Class Notebook or a regular one?\nAssuming the notebook resides on SharePoint, did you check access permissions in SharePoint?\nIf these tips don't help, then file a ticket\nhttps://t.co/ezBkUCozT8",
+		"truncated": false,
+		"display_text_range": [
+			43,
+			319
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						54,
+						62
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Opilin",
+					"name": "Philipp Reitter",
+					"id": 220638896,
+					"id_str": "220638896",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "OneNoteDE",
+					"name": "OneNoteDE",
+					"id": 3883996702,
+					"id_str": "3883996702",
+					"indices": [
+						8,
+						18
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						19,
+						30
+					]
+				},
+				{
+					"screen_name": "kurtsoeser",
+					"name": "Kurt Söser 🤘",
+					"id": 55684861,
+					"id_str": "55684861",
+					"indices": [
+						31,
+						42
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ezBkUCozT8",
+					"expanded_url": "https://aka.ms/EduSupport",
+					"display_url": "aka.ms/EduSupport",
+					"indices": [
+						296,
+						319
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1122804251019894800,
+		"in_reply_to_status_id_str": "1122804251019894784",
+		"in_reply_to_user_id": 220638896,
+		"in_reply_to_user_id_str": "220638896",
+		"in_reply_to_screen_name": "Opilin",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20824,
+			"friends_count": 7518,
+			"listed_count": 614,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28678,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25378,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Opilin @OneNoteDE @OneNoteEDU @kurtsoeser Was this a #OneNote notebook created through Microsoft Teams, Philipp? Was this a Class Notebook or a regular one?\nAssuming the notebook resides on SharePoint, did you check access permissions in SharePoint?\nIf these tips don't help, then file a ticket\nhttps://t.co/ezBkUCozT8"
+	},
+	{
 		"created_at": "Sun Apr 28 18:03:07 +0000 2019",
 		"id": 1120598730816794600,
 		"id_str": "1122561899516899329",
