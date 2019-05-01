282a8b16351db96a4defe4238e7dcfb07b797eeb
CleverClogs <github@cleverclogs.org>
2019-05-01 18:19:30 +0200

    Added 1 tweets

---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,222 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 16:08:32 +0000 2019",
+		"id": 1123620225344319500,
+		"id_str": "1123620225344319489",
+		"full_text": "Just in: the new #OneNote Mini Toolbar on #Windows10 is a quick-access toolbar with frequently used formatting commands.\n\nMore info: \nhttps://t.co/BWACky89wk\nby Jee Soo Han for #Microsoft\n\nAnimated GIF: https://t.co/zU6n6CrAeJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			202
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						17,
+						25
+					]
+				},
+				{
+					"text": "Windows10",
+					"indices": [
+						42,
+						52
+					]
+				},
+				{
+					"text": "Microsoft",
+					"indices": [
+						177,
+						187
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BWACky89wk",
+					"expanded_url": "https://techcommunity.microsoft.com/t5/Office-365-Blog/Edit-your-content-faster-with-OneNote/ba-p/493520",
+					"display_url": "techcommunity.microsoft.com/t5/Office-365-…",
+					"indices": [
+						134,
+						157
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1123620089260126200,
+					"id_str": "1123620089260126209",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5fmhJrXsAEuzGG.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5fmhJrXsAEuzGG.jpg",
+					"url": "https://t.co/zU6n6CrAeJ",
+					"display_url": "pic.twitter.com/zU6n6CrAeJ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1123620225344319489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 998,
+							"h": 612,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 998,
+							"h": 612,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 417,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123620089260126200,
+					"id_str": "1123620089260126209",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5fmhJrXsAEuzGG.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5fmhJrXsAEuzGG.jpg",
+					"url": "https://t.co/zU6n6CrAeJ",
+					"display_url": "pic.twitter.com/zU6n6CrAeJ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1123620225344319489/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 998,
+							"h": 612,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 998,
+							"h": 612,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 417,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							499,
+							306
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D5fmhJrXsAEuzGG.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20856,
+			"friends_count": 7523,
+			"listed_count": 615,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28682,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25384,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just in: the new #OneNote Mini Toolbar on #Windows10 is a quick-access toolbar with frequently used formatting commands.\n\nMore info: \nhttps://t.co/BWACky89wk\nby Jee Soo Han for #Microsoft\n\nAnimated GIF: https://t.co/zU6n6CrAeJ"
+	}
+]
