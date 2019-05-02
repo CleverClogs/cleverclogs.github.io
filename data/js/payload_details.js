04bee6ac59f24e9638fd7b67ec89f5227c550404
CleverClogs <github@cleverclogs.org>
2019-05-02 14:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30619,
-	"created_at": "Thu May 02 11:19:27 +0000 2019",
+	"tweets": 30620,
+	"created_at": "Thu May 02 12:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 02 11:50:45 +0000 2019",
+		"id": 1123917740589428700,
+		"id_str": "1123917740589428738",
+		"full_text": "@ARoseInJax @reillycosgrove Amy, I just realized that Stickers are a premium feature that requires an #Office365 subscription. I'm so used to seeing it that I forgot to mention that.",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			182
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Office365",
+					"indices": [
+						102,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ARoseInJax",
+					"name": "Amy Rose",
+					"id": 88224790,
+					"id_str": "88224790",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "reillycosgrove",
+					"name": "Reilly",
+					"id": 445676837,
+					"id_str": "445676837",
+					"indices": [
+						12,
+						27
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1123912509419872300,
+		"in_reply_to_status_id_str": "1123912509419872262",
+		"in_reply_to_user_id": 88224790,
+		"in_reply_to_user_id_str": "88224790",
+		"in_reply_to_screen_name": "ARoseInJax",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20866,
+			"friends_count": 7523,
+			"listed_count": 615,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28686,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25387,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ARoseInJax @reillycosgrove Amy, I just realized that Stickers are a premium feature that requires an #Office365 subscription. I'm so used to seeing it that I forgot to mention that."
+	},
+	{
 		"created_at": "Thu May 02 11:16:57 +0000 2019",
 		"id": 1123909233395871700,
 		"id_str": "1123909233395871744",
