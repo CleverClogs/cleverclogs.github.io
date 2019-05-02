622e99a8791c45d6ad61133ffa3322e18f2bb7db
CleverClogs <github@cleverclogs.org>
2019-05-02 22:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30620,
-	"created_at": "Thu May 02 12:19:28 +0000 2019",
+	"tweets": 30621,
+	"created_at": "Thu May 02 20:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 02 19:29:39 +0000 2019",
+		"id": 1124033227855933400,
+		"id_str": "1124033227855933441",
+		"full_text": "New features in Onetastic v4.2.0, the add-in for #OneNote 2010/2013/2016 for Windows: \n- Color picker and Color type support in macros\n- Update details can now be seen in the app for installed macros\n\nGet all details on the Onetastic blog:\nhttps://t.co/ADBdmP6T5L\nby Omer Atay https://t.co/1ih2s9whPU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						49,
+						57
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ADBdmP6T5L",
+					"expanded_url": "https://getonetastic.com/blog-color-picker",
+					"display_url": "getonetastic.com/blog-color-pic…",
+					"indices": [
+						240,
+						263
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124033159870525400,
+					"id_str": "1124033159870525440",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/D5leNBnXsAAQzI_.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5leNBnXsAAQzI_.png",
+					"url": "https://t.co/1ih2s9whPU",
+					"display_url": "pic.twitter.com/1ih2s9whPU",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1124033227855933441/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1053,
+							"h": 829,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1053,
+							"h": 829,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 535,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124033159870525400,
+					"id_str": "1124033159870525440",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/D5leNBnXsAAQzI_.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5leNBnXsAAQzI_.png",
+					"url": "https://t.co/1ih2s9whPU",
+					"display_url": "pic.twitter.com/1ih2s9whPU",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1124033227855933441/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1053,
+							"h": 829,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1053,
+							"h": 829,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 535,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20871,
+			"friends_count": 7524,
+			"listed_count": 615,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28686,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25388,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New features in Onetastic v4.2.0, the add-in for #OneNote 2010/2013/2016 for Windows: \n- Color picker and Color type support in macros\n- Update details can now be seen in the app for installed macros\n\nGet all details on the Onetastic blog:\nhttps://t.co/ADBdmP6T5L\nby Omer Atay https://t.co/1ih2s9whPU"
+	},
+	{
 		"created_at": "Thu May 02 11:50:45 +0000 2019",
 		"id": 1123917740589428700,
 		"id_str": "1123917740589428738",
