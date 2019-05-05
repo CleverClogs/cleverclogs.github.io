b2cc7da4e13dc32b8c6a82c9d599232ad1300914
CleverClogs <github@cleverclogs.org>
2019-05-05 23:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30633,
-	"created_at": "Sun May 05 19:19:27 +0000 2019",
+	"tweets": 30634,
+	"created_at": "Sun May 05 21:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 05 20:29:31 +0000 2019",
+		"id": 1125135454695755800,
+		"id_str": "1125135454695755777",
+		"full_text": "Full transcript of this outstanding video is available on Ulrika's blog:\nhttps://t.co/w5ONTeehvI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/w5ONTeehvI",
+					"expanded_url": "https://www.businessproductivity.com/ipad-pro-2018-vs-surface-pro-6-for-note-taking/",
+					"display_url": "businessproductivity.com/ipad-pro-2018-…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1125134939505164300,
+		"in_reply_to_status_id_str": "1125134939505164293",
+		"in_reply_to_user_id": 2582768738,
+		"in_reply_to_user_id_str": "2582768738",
+		"in_reply_to_screen_name": "OneNoteC",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20909,
+			"friends_count": 7527,
+			"listed_count": 616,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28711,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25403,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Full transcript of this outstanding video is available on Ulrika's blog:\nhttps://t.co/w5ONTeehvI"
+	},
+	{
 		"created_at": "Sun May 05 19:17:43 +0000 2019",
 		"id": 1125117386422718500,
 		"id_str": "1125117386422718466",
@@ -45,7 +151,7 @@ Grailbird.data.tweets_2019_05 =
 			"user_mentions": [
 				{
 					"screen_name": "chambernate",
-					"name": "Nate Chamberlain 🏳️‍🌈🌻",
+					"name": "Nate Chamberlain 🏳️🌈🌻",
 					"id": 1463566356,
 					"id_str": "1463566356",
 					"indices": [
@@ -290,7 +396,7 @@ Grailbird.data.tweets_2019_05 =
 			"user": {
 				"id": 1463566356,
 				"id_str": "1463566356",
-				"name": "Nate Chamberlain 🏳️‍🌈🌻",
+				"name": "Nate Chamberlain 🏳️🌈🌻",
 				"screen_name": "chambernate",
 				"location": "Lawrence, KS",
 				"description": "Microsoft MVP & O365 MCSA, Author of The ABCs of SharePoint (https://t.co/lecYgU64Qe) and Rise of the Advocates (https://t.co/jUQd1d8U4Q) and Director of #LSPUG",
