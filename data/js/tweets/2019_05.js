dab0cbb9a57cbfd58139d0d1c3160d47c7b1c869
CleverClogs <github@cleverclogs.org>
2019-05-09 00:19:28 +0200

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 08 21:38:41 +0000 2019",
+		"id": 1126240028307402800,
+		"id_str": "1126240028307402752",
+		"full_text": "@MS_Tech_Tchr @OneNoteEDU @edpuzzle @mtholfsen @justinchando I have no information on that. Maybe the @EdPuzzle team is the best to answer that question. \n#oEmbed",
+		"truncated": false,
+		"display_text_range": [
+			61,
+			162
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "oEmbed",
+					"indices": [
+						155,
+						162
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MS_Tech_Tchr",
+					"name": "Donna M.",
+					"id": 2148024854,
+					"id_str": "2148024854",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						14,
+						25
+					]
+				},
+				{
+					"screen_name": "edpuzzle",
+					"name": "Edpuzzle",
+					"id": 1632870468,
+					"id_str": "1632870468",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						36,
+						46
+					]
+				},
+				{
+					"screen_name": "justinchando",
+					"name": "Justin Chando",
+					"id": 108410718,
+					"id_str": "108410718",
+					"indices": [
+						47,
+						60
+					]
+				},
+				{
+					"screen_name": "edpuzzle",
+					"name": "Edpuzzle",
+					"id": 1632870468,
+					"id_str": "1632870468",
+					"indices": [
+						102,
+						111
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1126227618754453500,
+		"in_reply_to_status_id_str": "1126227618754453504",
+		"in_reply_to_user_id": 2148024854,
+		"in_reply_to_user_id_str": "2148024854",
+		"in_reply_to_screen_name": "MS_Tech_Tchr",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20944,
+			"friends_count": 7536,
+			"listed_count": 622,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28732,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25420,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@MS_Tech_Tchr @OneNoteEDU @edpuzzle @mtholfsen @justinchando I have no information on that. Maybe the @EdPuzzle team is the best to answer that question. \n#oEmbed"
+	},
+	{
 		"created_at": "Wed May 08 19:51:50 +0000 2019",
 		"id": 1126125763873443800,
 		"id_str": "1126213137303207936",
