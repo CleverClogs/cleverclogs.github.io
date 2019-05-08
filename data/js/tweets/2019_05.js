58bb69db5736a00d6d6c78e9ab1a1ae7eade717a
CleverClogs <github@cleverclogs.org>
2019-05-08 22:19:29 +0200

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 08 19:51:50 +0000 2019",
+		"id": 1126125763873443800,
+		"id_str": "1126213137303207936",
+		"full_text": "Meet our wonderful new team.\nProudly announcing the hosts for the global #MSFTEduChat TweetMeet about Inclusive Classrooms and #Accessbility.\n\nSee their tweets and follow the hosts through this Twitter List:\nhttps://t.co/SDE0ZZlUum\nMore updates soon.\n\n#Inclusion #MicrosoftEDU https://t.co/xEf6dOHN9m",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						73,
+						85
+					]
+				},
+				{
+					"text": "Accessbility",
+					"indices": [
+						127,
+						140
+					]
+				},
+				{
+					"text": "Inclusion",
+					"indices": [
+						252,
+						262
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						263,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SDE0ZZlUum",
+					"expanded_url": "https://twitter.com/tweetmeet/msfteduchat-2019-05",
+					"display_url": "twitter.com/tweetmeet/msft…",
+					"indices": [
+						208,
+						231
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1126125761226735600,
+					"id_str": "1126125761226735616",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/D6DNadJWAAA8KFM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6DNadJWAAA8KFM.jpg",
+					"url": "https://t.co/xEf6dOHN9m",
+					"display_url": "pic.twitter.com/xEf6dOHN9m",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1126125763873443841/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1820,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1126125761226735600,
+					"id_str": "1126125761226735616",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/D6DNadJWAAA8KFM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6DNadJWAAA8KFM.jpg",
+					"url": "https://t.co/xEf6dOHN9m",
+					"display_url": "pic.twitter.com/xEf6dOHN9m",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1126125763873443841/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1820,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat",
+			"screen_name": "TweetMeet",
+			"location": "World",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU on topics relevant to educators. Team TweetMeet: @OneNoteC @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/vTmAj2KFoN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vTmAj2KFoN",
+							"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+							"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2394,
+			"friends_count": 2026,
+			"listed_count": 21,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 1550,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 769,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1555675669",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 29,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Meet our wonderful new team.\nProudly announcing the hosts for the global #MSFTEduChat TweetMeet about Inclusive Classrooms and #Accessbility.\n\nSee their tweets and follow the hosts through this Twitter List:\nhttps://t.co/SDE0ZZlUum\nMore updates soon.\n\n#Inclusion #MicrosoftEDU https://t.co/xEf6dOHN9m"
+	},
+	{
 		"created_at": "Wed May 08 18:57:19 +0000 2019",
 		"id": 1126199418057822200,
 		"id_str": "1126199418057822208",
