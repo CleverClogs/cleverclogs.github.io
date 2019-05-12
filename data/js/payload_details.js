40890909598ee7e39cc72a75b7ef409626dcf90f
CleverClogs <github@cleverclogs.org>
2019-05-12 13:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30659,
-	"created_at": "Sun May 12 10:19:28 +0000 2019",
+	"tweets": 30660,
+	"created_at": "Sun May 12 11:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 12 10:59:08 +0000 2019",
+		"id": 1127528631335620600,
+		"id_str": "1127528631335620608",
+		"full_text": "@MichaelGillett @ShilpaRan Your wallpapers are plain stunning, Michael. I like how you designed and structured everything, making the site so easy to navigate.\nSome ideas: \n1. automatically update my wallpaper from your collections\n2. add tiny markers to easily align and tile application windows",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			296
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MichaelGillett",
+					"name": "Michael Gillett - Post #MSBuild Blues",
+					"id": 16966580,
+					"id_str": "16966580",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "ShilpaRan",
+					"name": "Shilpa Ranganathan",
+					"id": 890372445269446700,
+					"id_str": "890372445269446656",
+					"indices": [
+						16,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1127525689022922800,
+		"in_reply_to_status_id_str": "1127525689022922752",
+		"in_reply_to_user_id": 16966580,
+		"in_reply_to_user_id_str": "16966580",
+		"in_reply_to_screen_name": "MichaelGillett",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20996,
+			"friends_count": 7547,
+			"listed_count": 624,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25432,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@MichaelGillett @ShilpaRan Your wallpapers are plain stunning, Michael. I like how you designed and structured everything, making the site so easy to navigate.\nSome ideas: \n1. automatically update my wallpaper from your collections\n2. add tiny markers to easily align and tile application windows"
+	},
+	{
 		"created_at": "Sun May 12 10:15:00 +0000 2019",
 		"id": 1127517523417731100,
 		"id_str": "1127517523417731072",
