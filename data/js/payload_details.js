bb04571737a7a03d45843a86c7058d602dc11957
CleverClogs <github@cleverclogs.org>
2019-05-17 23:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30674,
-	"created_at": "Fri May 17 18:19:28 +0000 2019",
+	"tweets": 30675,
+	"created_at": "Fri May 17 21:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 17 20:40:25 +0000 2019",
+		"id": 1129486855047659500,
+		"id_str": "1129486855047659520",
+		"full_text": "Time to refresh your @TweetDeck. \nThe new version supports adding animated GIFs, threads, polls, emoji and image tagging. \n\nhttps://t.co/DLSn7sZUXS\n#Twitter https://t.co/IgZulcRB1u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Twitter",
+					"indices": [
+						148,
+						156
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TweetDeck",
+					"name": "TweetDeck",
+					"id": 14803701,
+					"id_str": "14803701",
+					"indices": [
+						21,
+						31
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DLSn7sZUXS",
+					"expanded_url": "https://twitter.com/i/tweetdeck_release_notes",
+					"display_url": "twitter.com/i/tweetdeck_re…",
+					"indices": [
+						124,
+						147
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1129486718560866300,
+					"id_str": "1129486718560866312",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6y-L65XkAg80hU.png",
+					"media_url_https": "https://pbs.twimg.com/media/D6y-L65XkAg80hU.png",
+					"url": "https://t.co/IgZulcRB1u",
+					"display_url": "pic.twitter.com/IgZulcRB1u",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1129486855047659520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 895,
+							"h": 707,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 537,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 895,
+							"h": 707,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1129486718560866300,
+					"id_str": "1129486718560866312",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6y-L65XkAg80hU.png",
+					"media_url_https": "https://pbs.twimg.com/media/D6y-L65XkAg80hU.png",
+					"url": "https://t.co/IgZulcRB1u",
+					"display_url": "pic.twitter.com/IgZulcRB1u",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1129486855047659520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 895,
+							"h": 707,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 537,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 895,
+							"h": 707,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21040,
+			"friends_count": 7554,
+			"listed_count": 627,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28776,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25447,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1555676470",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Time to refresh your @TweetDeck. \nThe new version supports adding animated GIFs, threads, polls, emoji and image tagging. \n\nhttps://t.co/DLSn7sZUXS\n#Twitter https://t.co/IgZulcRB1u"
+	},
+	{
 		"created_at": "Fri May 17 18:12:06 +0000 2019",
 		"id": 1129440214890344400,
 		"id_str": "1129449528388669441",
