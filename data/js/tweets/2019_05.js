00a518d25972b5806fd0095e682a57f15e90bc61
CleverClogs <github@cleverclogs.org>
2019-05-30 10:19:30 +0200

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,259 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 30 07:42:42 +0000 2019",
+		"id": 1133924694896259100,
+		"id_str": "1134002177104580608",
+		"full_text": "The lastest @msonenote Android Beta has a litany of exciting updates.\n1. The beautiful new OneNote icon\n2. A revamped &amp; modern notebook experience with ability to reorder pages\n3. Images in Sticky Notes that make those quick notes shine\nPlease update from Store now!\n@OneNoteC https://t.co/4bqjC99M0B",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						12,
+						22
+					]
+				},
+				{
+					"screen_name": "OneNoteC",
+					"name": "OneNote Central",
+					"id": 2582768738,
+					"id_str": "2582768738",
+					"indices": [
+						271,
+						280
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1133924674579050500,
+					"id_str": "1133924674579050496",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D7yCe9ZVsAA2kZh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7yCe9ZVsAA2kZh.jpg",
+					"url": "https://t.co/4bqjC99M0B",
+					"display_url": "pic.twitter.com/4bqjC99M0B",
+					"expanded_url": "https://twitter.com/amrita_rout/status/1133924694896259072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1133924674579050500,
+					"id_str": "1133924674579050496",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D7yCe9ZVsAA2kZh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7yCe9ZVsAA2kZh.jpg",
+					"url": "https://t.co/4bqjC99M0B",
+					"display_url": "pic.twitter.com/4bqjC99M0B",
+					"expanded_url": "https://twitter.com/amrita_rout/status/1133924694896259072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1133924683072413700,
+					"id_str": "1133924683072413696",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D7yCfdCUIAAFE0L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7yCfdCUIAAFE0L.jpg",
+					"url": "https://t.co/4bqjC99M0B",
+					"display_url": "pic.twitter.com/4bqjC99M0B",
+					"expanded_url": "https://twitter.com/amrita_rout/status/1133924694896259072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 340,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1133924685786206200,
+					"id_str": "1133924685786206214",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D7yCfnJVUAYdobr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7yCfnJVUAYdobr.jpg",
+					"url": "https://t.co/4bqjC99M0B",
+					"display_url": "pic.twitter.com/4bqjC99M0B",
+					"expanded_url": "https://twitter.com/amrita_rout/status/1133924694896259072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 340,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2933785406,
+			"id_str": "2933785406",
+			"name": "Amrita Rout",
+			"screen_name": "amrita_rout",
+			"location": "",
+			"description": "Product manager on the awesome NoTa, Notes and Tasks team at Microsoft. My views are my own....",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 166,
+			"friends_count": 79,
+			"listed_count": 5,
+			"created_at": "Wed Dec 17 17:18:16 +0000 2014",
+			"favourites_count": 304,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 226,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/545267895358324736/dFb9aw_C_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/545267895358324736/dFb9aw_C_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The lastest @msonenote Android Beta has a litany of exciting updates.\n1. The beautiful new OneNote icon\n2. A revamped &amp; modern notebook experience with ability to reorder pages\n3. Images in Sticky Notes that make those quick notes shine\nPlease update from Store now!\n@OneNoteC https://t.co/4bqjC99M0B"
+	},
+	{
 		"created_at": "Wed May 29 21:29:13 +0000 2019",
 		"id": 1133847789673287700,
 		"id_str": "1133847789673287680",
