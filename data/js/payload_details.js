0848f1f06760edff5f84929ca2b1d0392b87f4ae
CleverClogs <github@cleverclogs.org>
2019-05-30 23:19:30 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30694,
-	"created_at": "Thu May 30 15:19:31 +0000 2019",
+	"tweets": 30695,
+	"created_at": "Thu May 30 21:19:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,155 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 30 20:46:14 +0000 2019",
+		"id": 1134199360386940900,
+		"id_str": "1134199360386940929",
+		"full_text": "Engage your students from any corner in your classroom.\n\nWireless-projection device @ScreenBeam and #OneNote enable business-English teacher Farhana Khan to better prepare students for their future. \n\nhttps://t.co/lODLM7WSiS\nby #MIEExpert @Farhana1979 \n#MicrosoftEDU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						100,
+						108
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						228,
+						238
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						253,
+						266
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ScreenBeam",
+					"name": "ScreenBeam",
+					"id": 2600318149,
+					"id_str": "2600318149",
+					"indices": [
+						84,
+						95
+					]
+				},
+				{
+					"screen_name": "Farhana1979",
+					"name": "Farhana Khan",
+					"id": 479895972,
+					"id_str": "479895972",
+					"indices": [
+						239,
+						251
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lODLM7WSiS",
+					"expanded_url": "https://edu-blog.screenbeam.com/2019/05/30/office365-screenbeam/",
+					"display_url": "edu-blog.screenbeam.com/2019/05/30/off…",
+					"indices": [
+						201,
+						224
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21179,
+			"friends_count": 7566,
+			"listed_count": 632,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28807,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25472,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1558794013",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Engage your students from any corner in your classroom.\n\nWireless-projection device @ScreenBeam and #OneNote enable business-English teacher Farhana Khan to better prepare students for their future. \n\nhttps://t.co/lODLM7WSiS\nby #MIEExpert @Farhana1979 \n#MicrosoftEDU"
+	},
+	{
 		"created_at": "Thu May 30 15:06:16 +0000 2019",
 		"id": 1134111611680174100,
 		"id_str": "1134113805519282179",
