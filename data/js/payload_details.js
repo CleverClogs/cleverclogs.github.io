858f40b4fd55d8d932c772d7e521398dd505d765
CleverClogs <github@cleverclogs.org>
2019-06-03 00:39:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30698,
-	"created_at": "Sun Jun 02 19:20:58 +0000 2019",
+	"tweets": 30699,
+	"created_at": "Sun Jun 02 22:39:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sun Jun 02 21:27:49 +0000 2019",
+		"id": 1135296986838683600,
+		"id_str": "1135296986838683648",
+		"full_text": "Designing a horizontal #OneNote section navigation system. \nThe buttons are in fact active on control-click.\n\n❓Can you figure out how I made this? Useful? Thoughts?\n\nScreenshot below is from a proof-of-concept page from the TweetMeets notebook: https://t.co/BQwsf1DRGJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						23,
+						31
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1135295724298678300,
+					"id_str": "1135295724298678274",
+					"indices": [
+						245,
+						268
+					],
+					"media_url": "http://pbs.twimg.com/media/D8FhcjnXsAIiZdo.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8FhcjnXsAIiZdo.png",
+					"url": "https://t.co/BQwsf1DRGJ",
+					"display_url": "pic.twitter.com/BQwsf1DRGJ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1135296986838683648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 338,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1017,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 596,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135295724298678300,
+					"id_str": "1135295724298678274",
+					"indices": [
+						245,
+						268
+					],
+					"media_url": "http://pbs.twimg.com/media/D8FhcjnXsAIiZdo.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8FhcjnXsAIiZdo.png",
+					"url": "https://t.co/BQwsf1DRGJ",
+					"display_url": "pic.twitter.com/BQwsf1DRGJ",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1135296986838683648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 338,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1017,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 596,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21218,
+			"friends_count": 7568,
+			"listed_count": 633,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28803,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25476,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1559399571",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Designing a horizontal #OneNote section navigation system. \nThe buttons are in fact active on control-click.\n\n❓Can you figure out how I made this? Useful? Thoughts?\n\nScreenshot below is from a proof-of-concept page from the TweetMeets notebook: https://t.co/BQwsf1DRGJ"
+	},
+	{
 		"created_at": "Sun Jun 02 19:11:30 +0000 2019",
 		"id": 1135262681273897000,
 		"id_str": "1135262681273896960",
