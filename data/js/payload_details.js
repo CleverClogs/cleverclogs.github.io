4ccfe9d70b70b7be7998e42e02cb32d6af1ee033
CleverClogs <github@cleverclogs.org>
2019-06-03 16:19:32 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30705,
-	"created_at": "Mon Jun 03 12:19:29 +0000 2019",
+	"tweets": 30706,
+	"created_at": "Mon Jun 03 14:19:30 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 03 13:51:28 +0000 2019",
+		"id": 1135544533037584400,
+		"id_str": "1135544533037584386",
+		"full_text": "The Forte Labs #OneNote Resource Guide is a public, shared notebook with videos, tutorials, information and resources.\n\nTiago Forte created this notebook as part of his Building a Second Brain (BASB) methodology. \nhttps://t.co/VoojzYORbD\nby @ForteLabs\n#notetaking #productivity https://t.co/a2FlpHysHB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						15,
+						23
+					]
+				},
+				{
+					"text": "notetaking",
+					"indices": [
+						252,
+						263
+					]
+				},
+				{
+					"text": "productivity",
+					"indices": [
+						264,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fortelabs",
+					"name": "Tiago Forte",
+					"id": 1909232666,
+					"id_str": "1909232666",
+					"indices": [
+						241,
+						251
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VoojzYORbD",
+					"expanded_url": "https://praxis.fortelabs.co/onenote-resource-guide/",
+					"display_url": "praxis.fortelabs.co/onenote-resour…",
+					"indices": [
+						214,
+						237
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1135544126433312800,
+					"id_str": "1135544126433312770",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D8JDXdrWwAILfWD.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8JDXdrWwAILfWD.png",
+					"url": "https://t.co/a2FlpHysHB",
+					"display_url": "pic.twitter.com/a2FlpHysHB",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1135544533037584386/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 459,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 989,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 989,
+							"h": 667,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135544126433312800,
+					"id_str": "1135544126433312770",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D8JDXdrWwAILfWD.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8JDXdrWwAILfWD.png",
+					"url": "https://t.co/a2FlpHysHB",
+					"display_url": "pic.twitter.com/a2FlpHysHB",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1135544533037584386/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 459,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 989,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 989,
+							"h": 667,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21220,
+			"friends_count": 7566,
+			"listed_count": 632,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28809,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25486,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1559399571",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Forte Labs #OneNote Resource Guide is a public, shared notebook with videos, tutorials, information and resources.\n\nTiago Forte created this notebook as part of his Building a Second Brain (BASB) methodology. \nhttps://t.co/VoojzYORbD\nby @ForteLabs\n#notetaking #productivity https://t.co/a2FlpHysHB"
+	},
+	{
 		"created_at": "Mon Jun 03 11:58:06 +0000 2019",
 		"id": 1135516004199620600,
 		"id_str": "1135516004199620608",
