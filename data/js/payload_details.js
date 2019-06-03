706ede0cb401af961088e6112098fb3986f4ba88
CleverClogs <github@cleverclogs.org>
2019-06-03 23:19:31 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30708,
-	"created_at": "Mon Jun 03 17:19:28 +0000 2019",
+	"tweets": 30709,
+	"created_at": "Mon Jun 03 21:19:29 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 03 20:26:14 +0000 2019",
+		"id": 1135277571892006900,
+		"id_str": "1135643880081317890",
+		"full_text": "Know someone who's new to Twitter and who could use a how-to guide? \n\n#TwitterEDU, an outstanding and free ePub guide by David Truss, helps newcomers make the most out of it – just in time for the #MSFTEduChat TweetMeet events!\nhttps://t.co/QSqyTsyjjX \n\nby @datruss \n#MicrosoftEDU https://t.co/V7jqEOOxdV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TwitterEDU",
+					"indices": [
+						70,
+						81
+					]
+				},
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						197,
+						209
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						267,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "datruss",
+					"name": "David Truss",
+					"id": 10298552,
+					"id_str": "10298552",
+					"indices": [
+						257,
+						265
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QSqyTsyjjX",
+					"expanded_url": "http://www.davidtruss.com/twitteredu/",
+					"display_url": "davidtruss.com/twitteredu/",
+					"indices": [
+						228,
+						251
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1135277569765494800,
+					"id_str": "1135277569765494784",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D8FQ70tXUAAIzSY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8FQ70tXUAAIzSY.jpg",
+					"url": "https://t.co/V7jqEOOxdV",
+					"display_url": "pic.twitter.com/V7jqEOOxdV",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1135277571892006912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135277569765494800,
+					"id_str": "1135277569765494784",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D8FQ70tXUAAIzSY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8FQ70tXUAAIzSY.jpg",
+					"url": "https://t.co/V7jqEOOxdV",
+					"display_url": "pic.twitter.com/V7jqEOOxdV",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1135277571892006912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat",
+			"screen_name": "TweetMeet",
+			"location": "World",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU on topics relevant to educators. Team TweetMeet: @OneNoteC @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/vTmAj2KFoN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vTmAj2KFoN",
+							"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+							"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2647,
+			"friends_count": 2239,
+			"listed_count": 24,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 1915,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 849,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1558793639",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Know someone who's new to Twitter and who could use a how-to guide? \n\n#TwitterEDU, an outstanding and free ePub guide by David Truss, helps newcomers make the most out of it – just in time for the #MSFTEduChat TweetMeet events!\nhttps://t.co/QSqyTsyjjX \n\nby @datruss \n#MicrosoftEDU https://t.co/V7jqEOOxdV"
+	},
+	{
 		"created_at": "Mon Jun 03 17:10:46 +0000 2019",
 		"id": 1135594687987945500,
 		"id_str": "1135594687987945473",
