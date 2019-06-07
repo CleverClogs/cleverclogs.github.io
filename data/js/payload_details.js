d6ef7c6bac2fe99289c1f73b3f54686b31417294
CleverClogs <github@cleverclogs.org>
2019-06-08 01:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30716,
-	"created_at": "Thu Jun 06 19:19:32 +0000 2019",
+	"tweets": 30717,
+	"created_at": "Fri Jun 07 23:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 07 22:22:16 +0000 2019",
+		"id": 1137122112487845900,
+		"id_str": "1137122632124567561",
+		"full_text": "The #OneNote EDU engineering team is hard at work getting some exciting stuff ready for #ISTE19 🎉💜🎉 \n\nStay tuned... 🔮 \n#MIEExpert #MicrosoftEDU https://t.co/XKIFmIp9PI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						4,
+						12
+					]
+				},
+				{
+					"text": "ISTE19",
+					"indices": [
+						88,
+						95
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						119,
+						129
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						130,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1137122105705738200,
+					"id_str": "1137122105705738240",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/D8feh8jVsAAXJik.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8feh8jVsAAXJik.jpg",
+					"url": "https://t.co/XKIFmIp9PI",
+					"display_url": "pic.twitter.com/XKIFmIp9PI",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1137122112487845888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1294,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 758,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1137122105705738200,
+					"id_str": "1137122105705738240",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/D8feh8jVsAAXJik.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8feh8jVsAAXJik.jpg",
+					"url": "https://t.co/XKIFmIp9PI",
+					"display_url": "pic.twitter.com/XKIFmIp9PI",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1137122112487845888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1294,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 758,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, Learning Tools & #MicrosoftTeams #edtech #Accessibility @OneNoteEDU @MicrosoftEDU",
+			"url": "https://t.co/ANIFpSE2FB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSE2FB",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18636,
+			"friends_count": 3230,
+			"listed_count": 733,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 64590,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29261,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The #OneNote EDU engineering team is hard at work getting some exciting stuff ready for #ISTE19 🎉💜🎉 \n\nStay tuned... 🔮 \n#MIEExpert #MicrosoftEDU https://t.co/XKIFmIp9PI"
+	},
+	{
 		"created_at": "Thu Jun 06 18:27:20 +0000 2019",
 		"id": 1136701120536092700,
 		"id_str": "1136701120536092672",
