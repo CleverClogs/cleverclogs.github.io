456628b170fc37266b6ebf6410deaec5fb980d1a
CleverClogs <github@cleverclogs.org>
2019-06-09 12:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30717,
-	"created_at": "Fri Jun 07 23:19:27 +0000 2019",
+	"tweets": 30718,
+	"created_at": "Sun Jun 09 10:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sun Jun 09 09:46:35 +0000 2019",
+		"id": 1137657231737069600,
+		"id_str": "1137657231737069569",
+		"full_text": "@soslearning @mtholfsen @OneNoteEDU Now, I should add that I only tested this in the Windows client. If your students use notebooks in OneNote Online, then this trick will not work, because these expect HTTP hyperlinks, not OneNote: links. \nSo the 'solution' may not be as ideal as it seems.",
+		"truncated": false,
+		"display_text_range": [
+			36,
+			291
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "soslearning",
+					"name": "Esam Baboukhan",
+					"id": 498850763,
+					"id_str": "498850763",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						24,
+						35
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1137654556060528600,
+		"in_reply_to_status_id_str": "1137654556060528640",
+		"in_reply_to_user_id": 498850763,
+		"in_reply_to_user_id_str": "498850763",
+		"in_reply_to_screen_name": "soslearning",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21263,
+			"friends_count": 7564,
+			"listed_count": 633,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28817,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25499,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1559399571",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@soslearning @mtholfsen @OneNoteEDU Now, I should add that I only tested this in the Windows client. If your students use notebooks in OneNote Online, then this trick will not work, because these expect HTTP hyperlinks, not OneNote: links. \nSo the 'solution' may not be as ideal as it seems."
+	},
+	{
 		"created_at": "Fri Jun 07 22:22:16 +0000 2019",
 		"id": 1137122112487845900,
 		"id_str": "1137122632124567561",
