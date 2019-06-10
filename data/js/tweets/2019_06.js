f0dec8497a1a3e5385a335175f7c402010ff2445
CleverClogs <github@cleverclogs.org>
2019-06-10 22:19:30 +0200

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 10 19:42:18 +0000 2019",
+		"id": 1138169537784877000,
+		"id_str": "1138169537784877056",
+		"full_text": "@mathpaul @annetsmith Ik bedoelde andersom. Dus DE-activeren. Vergeef mij.",
+		"truncated": false,
+		"display_text_range": [
+			22,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mathpaul",
+					"name": "ℙαυ⌊  📐📖📝⌨️",
+					"id": 45334593,
+					"id_str": "45334593",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "annetsmith",
+					"name": "Annet Smith🐰🌍",
+					"id": 14809933,
+					"id_str": "14809933",
+					"indices": [
+						10,
+						21
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1138168564597366800,
+		"in_reply_to_status_id_str": "1138168564597366787",
+		"in_reply_to_user_id": 45334593,
+		"in_reply_to_user_id_str": "45334593",
+		"in_reply_to_screen_name": "mathpaul",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21276,
+			"friends_count": 7566,
+			"listed_count": 633,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28817,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25501,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1559399571",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@mathpaul @annetsmith Ik bedoelde andersom. Dus DE-activeren. Vergeef mij."
+	},
+	{
 		"created_at": "Mon Jun 10 17:31:21 +0000 2019",
 		"id": 1138124200928055300,
 		"id_str": "1138136584488984577",
