b9de6579ee4ea2e5f669e9606902a361134b09fe
CleverClogs <github@cleverclogs.org>
2019-06-18 10:19:30 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30732,
-	"created_at": "Mon Jun 17 21:19:28 +0000 2019",
+	"tweets": 30733,
+	"created_at": "Tue Jun 18 08:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,187 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 18 07:24:15 +0000 2019",
+		"id": 1140882901711634400,
+		"id_str": "1140882901711634432",
+		"full_text": "@genially_en @mtholfsen @kerszi @teacherkimj @BeckyKeene @Conchircabello Kindly keep me posted on #oEmbed integration between @Genially_en and #OneNote.",
+		"truncated": false,
+		"display_text_range": [
+			73,
+			152
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "oEmbed",
+					"indices": [
+						98,
+						105
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						143,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "genially_en",
+					"name": "Genially",
+					"id": 4149933699,
+					"id_str": "4149933699",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"screen_name": "kerszi",
+					"name": "Kathi Kersznowski",
+					"id": 595079922,
+					"id_str": "595079922",
+					"indices": [
+						24,
+						31
+					]
+				},
+				{
+					"screen_name": "teacherkimj",
+					"name": "Kim James",
+					"id": 3098515639,
+					"id_str": "3098515639",
+					"indices": [
+						32,
+						44
+					]
+				},
+				{
+					"screen_name": "BeckyKeene",
+					"name": "Becky Keene",
+					"id": 253925945,
+					"id_str": "253925945",
+					"indices": [
+						45,
+						56
+					]
+				},
+				{
+					"screen_name": "Conchircabello",
+					"name": "Conchi",
+					"id": 219170897,
+					"id_str": "219170897",
+					"indices": [
+						57,
+						72
+					]
+				},
+				{
+					"screen_name": "genially_en",
+					"name": "Genially",
+					"id": 4149933699,
+					"id_str": "4149933699",
+					"indices": [
+						126,
+						138
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1140664931831996400,
+		"in_reply_to_status_id_str": "1140664931831996421",
+		"in_reply_to_user_id": 4149933699,
+		"in_reply_to_user_id_str": "4149933699",
+		"in_reply_to_screen_name": "genially_en",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra–OneNote MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21433,
+			"friends_count": 7571,
+			"listed_count": 635,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28816,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25510,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1559399571",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@genially_en @mtholfsen @kerszi @teacherkimj @BeckyKeene @Conchircabello Kindly keep me posted on #oEmbed integration between @Genially_en and #OneNote."
+	},
+	{
 		"created_at": "Mon Jun 17 20:46:05 +0000 2019",
 		"id": 1140664420198170600,
 		"id_str": "1140722305603125249",
