4aaf4a8bd6f138fa70695441be817d8afb824fe8
CleverClogs <github@cleverclogs.org>
2019-07-25 15:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30780,
-	"created_at": "Wed Jul 24 10:19:27 +0000 2019",
+	"tweets": 30781,
+	"created_at": "Thu Jul 25 13:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 25 12:14:55 +0000 2019",
+		"id": 1154364404227616800,
+		"id_str": "1154364404227616768",
+		"full_text": "@genially_en @mtholfsen @kerszi @teacherkimj @BeckyKeene @Conchircabello Hi everyone. Any progress on the Genially #oEmbed implementation? Seems you have the @embedly integration in place, right?\n\nImagine creations such as these to be live-embedded in #OneNote:\nhttps://t.co/CLGIKy5GHY",
+		"truncated": false,
+		"display_text_range": [
+			73,
+			285
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "oEmbed",
+					"indices": [
+						115,
+						122
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						252,
+						260
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "genially_en",
+					"name": "Genially",
+					"id": 4149933699,
+					"id_str": "4149933699",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"screen_name": "kerszi",
+					"name": "Kathi Kersznowski",
+					"id": 595079922,
+					"id_str": "595079922",
+					"indices": [
+						24,
+						31
+					]
+				},
+				{
+					"screen_name": "teacherkimj",
+					"name": "Kim James",
+					"id": 3098515639,
+					"id_str": "3098515639",
+					"indices": [
+						32,
+						44
+					]
+				},
+				{
+					"screen_name": "BeckyKeene",
+					"name": "Becky Keene",
+					"id": 253925945,
+					"id_str": "253925945",
+					"indices": [
+						45,
+						56
+					]
+				},
+				{
+					"screen_name": "Conchircabello",
+					"name": "Conchi",
+					"id": 219170897,
+					"id_str": "219170897",
+					"indices": [
+						57,
+						72
+					]
+				},
+				{
+					"screen_name": "embedly",
+					"name": "Embedly",
+					"id": 82485597,
+					"id_str": "82485597",
+					"indices": [
+						158,
+						166
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/CLGIKy5GHY",
+					"expanded_url": "https://view.genial.ly/5ac895170305a00da1fe15f8/interactive-content-harry-potter-6e5e",
+					"display_url": "view.genial.ly/5ac895170305a0…",
+					"indices": [
+						262,
+						285
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1140664931831996400,
+		"in_reply_to_status_id_str": "1140664931831996421",
+		"in_reply_to_user_id": 4149933699,
+		"in_reply_to_user_id_str": "4149933699",
+		"in_reply_to_screen_name": "genially_en",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21975,
+			"friends_count": 7589,
+			"listed_count": 637,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28774,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25560,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1563651168",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@genially_en @mtholfsen @kerszi @teacherkimj @BeckyKeene @Conchircabello Hi everyone. Any progress on the Genially #oEmbed implementation? Seems you have the @embedly integration in place, right?\n\nImagine creations such as these to be live-embedded in #OneNote:\nhttps://t.co/CLGIKy5GHY"
+	},
+	{
 		"created_at": "Wed Jul 24 09:50:56 +0000 2019",
 		"id": 1153965782956728300,
 		"id_str": "1153965782956728325",
