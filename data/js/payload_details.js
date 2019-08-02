c0821cdc5b8dbfb3d5fc2342809b30944728d5f4
CleverClogs <github@cleverclogs.org>
2019-08-02 05:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30787,
-	"created_at": "Thu Aug 01 17:19:27 +0000 2019",
+	"tweets": 30788,
+	"created_at": "Fri Aug 02 03:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 02 02:48:18 +0000 2019",
+		"id": 1157090662443098000,
+		"id_str": "1157120913135063042",
+		"full_text": "Ever noticed there aren't any #MicrosoftEDU books 📚 out there for educators? We're going to change that! Beyond excited to sign a contract with @dbc_inc and my co-author @beckykeene. Come \"Sail the 7Cs of @MicrosoftEDU\" ⛵ Dropping anchor by June 2020! https://t.co/iT3QioZiQx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			251
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						30,
+						43
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dbc_inc",
+					"name": "Dave Burgess Consulting, Inc.",
+					"id": 973623764809261000,
+					"id_str": "973623764809261056",
+					"indices": [
+						144,
+						152
+					]
+				},
+				{
+					"screen_name": "BeckyKeene",
+					"name": "Becky Keene",
+					"id": 253925945,
+					"id_str": "253925945",
+					"indices": [
+						170,
+						181
+					]
+				},
+				{
+					"screen_name": "MicrosoftEDU",
+					"name": "Microsoft Education",
+					"id": 17826187,
+					"id_str": "17826187",
+					"indices": [
+						205,
+						218
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1157090656809939000,
+					"id_str": "1157090656809938944",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EA7P0mQUcAARgKk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA7P0mQUcAARgKk.jpg",
+					"url": "https://t.co/iT3QioZiQx",
+					"display_url": "pic.twitter.com/iT3QioZiQx",
+					"expanded_url": "https://twitter.com/kerszi/status/1157090662443098112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 481,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 481,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157090656809939000,
+					"id_str": "1157090656809938944",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EA7P0mQUcAARgKk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA7P0mQUcAARgKk.jpg",
+					"url": "https://t.co/iT3QioZiQx",
+					"display_url": "pic.twitter.com/iT3QioZiQx",
+					"expanded_url": "https://twitter.com/kerszi/status/1157090662443098112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 481,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 481,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1157090656889651200,
+					"id_str": "1157090656889651205",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EA7P0mjUwAUVFLv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA7P0mjUwAUVFLv.jpg",
+					"url": "https://t.co/iT3QioZiQx",
+					"display_url": "pic.twitter.com/iT3QioZiQx",
+					"expanded_url": "https://twitter.com/kerszi/status/1157090662443098112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 428,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 428,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 428,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 595079922,
+			"id_str": "595079922",
+			"name": "Kathi Kersznowski",
+			"screen_name": "kerszi",
+			"location": "New Jersey, USA",
+			"description": "TechIntegration Expert, Global Presenter, Keynote Speaker, MIEExpert, Empatico, Flipgrid, #LearningInTheLoo & #Fliphunt creator, #GlobalGoalsSchools, #TeachSDGs",
+			"url": "https://t.co/F3EubyVGqy",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/F3EubyVGqy",
+							"expanded_url": "http://kerszi.wordpress.com/",
+							"display_url": "kerszi.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14037,
+			"friends_count": 9198,
+			"listed_count": 635,
+			"created_at": "Wed May 30 23:31:13 +0000 2012",
+			"favourites_count": 41824,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29892,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3B94D9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1109051636209471493/5tkEX1Wq_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1109051636209471493/5tkEX1Wq_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/595079922/1560972741",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 42,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ever noticed there aren't any #MicrosoftEDU books 📚 out there for educators? We're going to change that! Beyond excited to sign a contract with @dbc_inc and my co-author @beckykeene. Come \"Sail the 7Cs of @MicrosoftEDU\" ⛵ Dropping anchor by June 2020! https://t.co/iT3QioZiQx"
+	},
+	{
 		"created_at": "Thu Aug 01 17:06:09 +0000 2019",
 		"id": 1156974410189987800,
 		"id_str": "1156974410189987840",
