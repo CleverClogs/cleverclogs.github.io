5665e1618f1498c4ce93715afdd1bef79ddb082e
CleverClogs <github@cleverclogs.org>
2019-08-12 23:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30794,
-	"created_at": "Sun Aug 11 16:19:27 +0000 2019",
+	"tweets": 30795,
+	"created_at": "Mon Aug 12 21:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,350 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Mon Aug 12 20:58:26 +0000 2019",
+		"id": 1161019131401031700,
+		"id_str": "1161019131401031680",
+		"full_text": "❓ Any #OneNote #breakout specialists here who want to work with Jessica? \nShe has interesting questions about setting up digital #escaperooms in OneNote Class Notebooks inside #MicrosoftTeams.\n\ncc @sig225 @maclejeune @jlo731 @salleeclark\n@Sfm36 @daperfecttablet @eustace_stephen https://t.co/mmsRPal1cB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						6,
+						14
+					]
+				},
+				{
+					"text": "breakout",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"text": "escaperooms",
+					"indices": [
+						129,
+						141
+					]
+				},
+				{
+					"text": "MicrosoftTeams",
+					"indices": [
+						176,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sig225",
+					"name": "Cal Armstrong",
+					"id": 12895782,
+					"id_str": "12895782",
+					"indices": [
+						197,
+						204
+					]
+				},
+				{
+					"screen_name": "maclejeune",
+					"name": "Melanie LeJeune",
+					"id": 82766519,
+					"id_str": "82766519",
+					"indices": [
+						205,
+						216
+					]
+				},
+				{
+					"screen_name": "jlo731",
+					"name": "Jeni Long",
+					"id": 95272661,
+					"id_str": "95272661",
+					"indices": [
+						217,
+						224
+					]
+				},
+				{
+					"screen_name": "SalleeClark",
+					"name": "Sallee Clark",
+					"id": 2673808964,
+					"id_str": "2673808964",
+					"indices": [
+						225,
+						237
+					]
+				},
+				{
+					"screen_name": "Sfm36",
+					"name": "Sarah Clark",
+					"id": 384551986,
+					"id_str": "384551986",
+					"indices": [
+						238,
+						244
+					]
+				},
+				{
+					"screen_name": "daperfecttablet",
+					"name": "Robert Baker",
+					"id": 70403313,
+					"id_str": "70403313",
+					"indices": [
+						245,
+						261
+					]
+				},
+				{
+					"screen_name": "eustace_stephen",
+					"name": "Stephen Eustace",
+					"id": 705409101220675600,
+					"id_str": "705409101220675584",
+					"indices": [
+						262,
+						278
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mmsRPal1cB",
+					"expanded_url": "https://twitter.com/liivlaid/status/1160997704044097542",
+					"display_url": "twitter.com/liivlaid/statu…",
+					"indices": [
+						279,
+						302
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news and resources by Marjolein Hoekstra, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22105,
+			"friends_count": 7588,
+			"listed_count": 634,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28757,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25573,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1563651168",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1160997704044097500,
+		"quoted_status_id_str": "1160997704044097542",
+		"quoted_status_permalink": {
+			"url": "https://t.co/mmsRPal1cB",
+			"expanded": "https://twitter.com/liivlaid/status/1160997704044097542",
+			"display": "twitter.com/liivlaid/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Aug 12 19:33:17 +0000 2019",
+			"id": 1160997704044097500,
+			"id_str": "1160997704044097542",
+			"full_text": "I want to make a digital breakout room in \n@msonenote and I need to know if I can create it in an already existing #ClassNotebook in a ClassTeam? Preferably I'd like to distribute a section goup with the breakout. Possible? \n\n@OneNoteC #breakout, #breakoutedu #escaperooms",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "ClassNotebook",
+						"indices": [
+							115,
+							129
+						]
+					},
+					{
+						"text": "breakout",
+						"indices": [
+							236,
+							245
+						]
+					},
+					{
+						"text": "breakoutedu",
+						"indices": [
+							247,
+							259
+						]
+					},
+					{
+						"text": "escaperooms",
+						"indices": [
+							260,
+							272
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "msonenote",
+						"name": "Microsoft OneNote",
+						"id": 23735316,
+						"id_str": "23735316",
+						"indices": [
+							43,
+							53
+						]
+					},
+					{
+						"screen_name": "OneNoteC",
+						"name": "OneNote Central",
+						"id": 2582768738,
+						"id_str": "2582768738",
+						"indices": [
+							226,
+							235
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2363991571,
+				"id_str": "2363991571",
+				"name": "Jessica Liivlaid",
+				"screen_name": "liivlaid",
+				"location": "Ånge, Sverige",
+				"description": "English teacher @ Bygg- och anläggningsprogrammet. #OneNote and #MicrosoftTeams lover. #MIE Tweets in 🇬🇧 & 🇸🇪 may occur. 🏳️‍🌈 ♀️",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 164,
+				"friends_count": 575,
+				"listed_count": 2,
+				"created_at": "Thu Feb 27 10:40:42 +0000 2014",
+				"favourites_count": 3410,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 483,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1156499810394017793/NtB2rylT_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1156499810394017793/NtB2rylT_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2363991571/1564566017",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "❓ Any #OneNote #breakout specialists here who want to work with Jessica? \nShe has interesting questions about setting up digital #escaperooms in OneNote Class Notebooks inside #MicrosoftTeams.\n\ncc @sig225 @maclejeune @jlo731 @salleeclark\n@Sfm36 @daperfecttablet @eustace_stephen https://t.co/mmsRPal1cB"
+	},
+	{
 		"created_at": "Sun Aug 11 15:51:16 +0000 2019",
 		"id": 1160579209321832400,
 		"id_str": "1160579442143563776",
