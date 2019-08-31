b2ac63dcd08cc1e678cf96aaa7864b2f26c4c58e
CleverClogs <github@cleverclogs.org>
2019-09-01 00:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30803,
-	"created_at": "Wed Aug 28 07:19:28 +0000 2019",
+	"tweets": 30804,
+	"created_at": "Sat Aug 31 22:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 31 21:20:24 +0000 2019",
+		"id": 1167870538569568300,
+		"id_str": "1167910031544569856",
+		"full_text": "Sort by Last Name (or First) is back and improved in Review Student Work for #OneNote Class Notebooks.  Rolled out  for the OneNote Windows 10 app, Online, iPad and Mac 💜 \n\n#edtech #MIEExpert https://t.co/xR0G4jOIco",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						77,
+						85
+					]
+				},
+				{
+					"text": "edtech",
+					"indices": [
+						173,
+						180
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						181,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1167870532580069400,
+					"id_str": "1167870532580069384",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/media/EDUcD84U0AgGFXZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDUcD84U0AgGFXZ.jpg",
+					"url": "https://t.co/xR0G4jOIco",
+					"display_url": "pic.twitter.com/xR0G4jOIco",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1167870538569568257/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 389,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 687,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1518,
+							"h": 869,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1167870532580069400,
+					"id_str": "1167870532580069384",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/media/EDUcD84U0AgGFXZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDUcD84U0AgGFXZ.jpg",
+					"url": "https://t.co/xR0G4jOIco",
+					"display_url": "pic.twitter.com/xR0G4jOIco",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1167870538569568257/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 389,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 687,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1518,
+							"h": 869,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, #ImmersiveReader & #MicrosoftTeams #edtech #accessibility @OneNoteEDU",
+			"url": "https://t.co/ANIFpSE2FB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSE2FB",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20966,
+			"friends_count": 3410,
+			"listed_count": 762,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 76617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32128,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sort by Last Name (or First) is back and improved in Review Student Work for #OneNote Class Notebooks.  Rolled out  for the OneNote Windows 10 app, Online, iPad and Mac 💜 \n\n#edtech #MIEExpert https://t.co/xR0G4jOIco"
+	},
+	{
 		"created_at": "Wed Aug 28 07:13:29 +0000 2019",
 		"id": 1166422325601951700,
 		"id_str": "1166609731265470465",
