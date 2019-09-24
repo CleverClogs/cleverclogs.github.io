da21a085a753b15b7c6f2cc269b74f5750bea0b4
CleverClogs <github@cleverclogs.org>
2019-09-24 16:19:29 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30825,
-	"created_at": "Sun Sep 22 22:19:27 +0000 2019",
+	"tweets": 30826,
+	"created_at": "Tue Sep 24 14:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 24 14:11:50 +0000 2019",
+		"id": 1176499486824239000,
+		"id_str": "1176499486824239104",
+		"full_text": "Make your #OneNote notebooks interactive with live-embeds.\n\nIn the video below, OneNote in Education expert Marija Petreska demonstrates how teachers can benefit from all the integrations that OneNote supports – and more are coming!\n\nJust wonderful, @teachermarija!\n#oEmbed https://t.co/2JACcO8NJ9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						10,
+						18
+					]
+				},
+				{
+					"text": "oEmbed",
+					"indices": [
+						266,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "teachermarija",
+					"name": "Marija Petreska",
+					"id": 521427482,
+					"id_str": "521427482",
+					"indices": [
+						250,
+						264
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2JACcO8NJ9",
+					"expanded_url": "https://twitter.com/teachermarija/status/1176485215193194497",
+					"display_url": "twitter.com/teachermarija/…",
+					"indices": [
+						274,
+						297
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22490,
+			"friends_count": 7590,
+			"listed_count": 639,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25599,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1567765456",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1176485215193194500,
+		"quoted_status_id_str": "1176485215193194497",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2JACcO8NJ9",
+			"expanded": "https://twitter.com/teachermarija/status/1176485215193194497",
+			"display": "twitter.com/teachermarija/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 24 13:15:07 +0000 2019",
+			"id": 1176485215193194500,
+			"id_str": "1176485215193194497",
+			"full_text": "Well done #OneNote team \nTeachers seeing #OneNoteEdu for the first time were mind blown by this\nBut you, having proved to be the best #edtech tool, need to keep on adding things I can embed \n@msonenote @OneNoteEDU @mtholfsen \n#mieexpert #msftedu \nhttps://t.co/IreJYgRJkv",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				270
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OneNote",
+						"indices": [
+							10,
+							18
+						]
+					},
+					{
+						"text": "OneNoteEdu",
+						"indices": [
+							41,
+							52
+						]
+					},
+					{
+						"text": "edtech",
+						"indices": [
+							134,
+							141
+						]
+					},
+					{
+						"text": "mieexpert",
+						"indices": [
+							226,
+							236
+						]
+					},
+					{
+						"text": "msftedu",
+						"indices": [
+							237,
+							245
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "msonenote",
+						"name": "Microsoft OneNote",
+						"id": 23735316,
+						"id_str": "23735316",
+						"indices": [
+							191,
+							201
+						]
+					},
+					{
+						"screen_name": "OneNoteEDU",
+						"name": "OneNote Education",
+						"id": 3096566760,
+						"id_str": "3096566760",
+						"indices": [
+							202,
+							213
+						]
+					},
+					{
+						"screen_name": "mtholfsen",
+						"name": "Mike Tholfsen",
+						"id": 27673327,
+						"id_str": "27673327",
+						"indices": [
+							214,
+							224
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/IreJYgRJkv",
+						"expanded_url": "https://youtu.be/7QZKmYW6jgo",
+						"display_url": "youtu.be/7QZKmYW6jgo",
+						"indices": [
+							247,
+							270
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 521427482,
+				"id_str": "521427482",
+				"name": "Marija Petreska",
+				"screen_name": "teachermarija",
+				"location": "Macedonia",
+				"description": "English Language Teacher, Microsoft Certified Educator, MIE Fellow, Apple Teacher, Associate @ImmersiveMinds",
+				"url": "https://t.co/546CoXQHMo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/546CoXQHMo",
+								"expanded_url": "https://about.me/petreskamarija",
+								"display_url": "about.me/petreskamarija",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3608,
+				"friends_count": 1028,
+				"listed_count": 553,
+				"created_at": "Sun Mar 11 15:48:42 +0000 2012",
+				"favourites_count": 10572,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 74879,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1090888767081205760/Kuzs6mSi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1090888767081205760/Kuzs6mSi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/521427482/1549969195",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Make your #OneNote notebooks interactive with live-embeds.\n\nIn the video below, OneNote in Education expert Marija Petreska demonstrates how teachers can benefit from all the integrations that OneNote supports – and more are coming!\n\nJust wonderful, @teachermarija!\n#oEmbed https://t.co/2JACcO8NJ9"
+	},
+	{
 		"created_at": "Sun Sep 22 21:55:48 +0000 2019",
 		"id": 1175846603481145300,
 		"id_str": "1175891470244634631",
