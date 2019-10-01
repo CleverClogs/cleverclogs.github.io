f85bfe0db5f96688dd6aa31b119c502c21524ec7
CleverClogs <github@cleverclogs.org>
2019-10-01 18:19:28 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30832,
-	"created_at": "Tue Oct 01 14:19:30 +0000 2019",
+	"tweets": 30833,
+	"created_at": "Tue Oct 01 16:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 01 16:18:16 +0000 2019",
+		"id": 1179066118041813000,
+		"id_str": "1179068017289957376",
+		"full_text": "NEW! Lots of new Math articles are published under our Learning Tools help section 🚀 Check them out, share widely!  #edtech #mathchat #MIEExpert \n\nhttps://t.co/yOJd36Q8ZP https://t.co/vWnyTxOj5E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edtech",
+					"indices": [
+						116,
+						123
+					]
+				},
+				{
+					"text": "mathchat",
+					"indices": [
+						124,
+						133
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						134,
+						144
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yOJd36Q8ZP",
+					"expanded_url": "https://support.office.com/en-us/article/learning-tools-eff7f7e3-7e21-42f0-a6f1-da7027f98261?ui=en-US&rs=en-US&ad=US#ID0EABAAA=Math",
+					"display_url": "support.office.com/en-us/article/…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1179066109552541700,
+					"id_str": "1179066109552541696",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/EFziYTKUwAAuJqO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFziYTKUwAAuJqO.jpg",
+					"url": "https://t.co/vWnyTxOj5E",
+					"display_url": "pic.twitter.com/vWnyTxOj5E",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1179066118041812996/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1495,
+							"h": 869,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 395,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 698,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1179066109552541700,
+					"id_str": "1179066109552541696",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/EFziYTKUwAAuJqO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFziYTKUwAAuJqO.jpg",
+					"url": "https://t.co/vWnyTxOj5E",
+					"display_url": "pic.twitter.com/vWnyTxOj5E",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1179066118041812996/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1495,
+							"h": 869,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 395,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 698,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, #ImmersiveReader & #MicrosoftTeams #edtech #accessibility @OneNoteEDU",
+			"url": "https://t.co/ANIFpSE2FB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSE2FB",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21349,
+			"friends_count": 3425,
+			"listed_count": 762,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 79284,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32786,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "NEW! Lots of new Math articles are published under our Learning Tools help section 🚀 Check them out, share widely!  #edtech #mathchat #MIEExpert \n\nhttps://t.co/yOJd36Q8ZP https://t.co/vWnyTxOj5E"
+	},
+	{
 		"created_at": "Tue Oct 01 13:28:02 +0000 2019",
 		"id": 1179024671636770800,
 		"id_str": "1179025177906155520",
