b4a5ae5613990dc9daeb8e1c2cca87f178b8b8a7
CleverClogs <github@cleverclogs.org>
2019-10-08 23:19:52 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30844,
-	"created_at": "Tue Oct 08 16:19:30 +0000 2019",
+	"tweets": 30845,
+	"created_at": "Tue Oct 08 21:19:52 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 08 20:54:31 +0000 2019",
+		"id": 1181674253835493400,
+		"id_str": "1181674253835493376",
+		"full_text": "@Daniel_Rubino @casaout This #SurfaceHub + #OneNote thread seems to cover the issue:\n\nhttps://t.co/8a7jVFD5dS",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SurfaceHub",
+					"indices": [
+						29,
+						40
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						43,
+						51
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Daniel_Rubino",
+					"name": "Daniel Rubino",
+					"id": 37025150,
+					"id_str": "37025150",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "casaout",
+					"name": "André Meyer",
+					"id": 148433515,
+					"id_str": "148433515",
+					"indices": [
+						15,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8a7jVFD5dS",
+					"expanded_url": "https://techcommunity.microsoft.com/t5/Surface-Hub/OneNote-Online-not-working-in-Edge-on-Surface-Hub/m-p/790518",
+					"display_url": "techcommunity.microsoft.com/t5/Surface-Hub…",
+					"indices": [
+						86,
+						109
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1181672409163849700,
+		"in_reply_to_status_id_str": "1181672409163849731",
+		"in_reply_to_user_id": 37025150,
+		"in_reply_to_user_id_str": "37025150",
+		"in_reply_to_screen_name": "Daniel_Rubino",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22593,
+			"friends_count": 7597,
+			"listed_count": 640,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25618,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1569512582",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Daniel_Rubino @casaout This #SurfaceHub + #OneNote thread seems to cover the issue:\n\nhttps://t.co/8a7jVFD5dS"
+	},
+	{
 		"created_at": "Tue Oct 08 15:39:46 +0000 2019",
 		"id": 1181595045633372200,
 		"id_str": "1181595045633372161",
