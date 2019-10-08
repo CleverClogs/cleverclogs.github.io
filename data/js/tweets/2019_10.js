c7bff2e2ef635102273be5be91dcb75b27531442
CleverClogs <github@cleverclogs.org>
2019-10-08 16:19:28 +0200

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,339 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 08 14:14:15 +0000 2019",
+		"id": 1181573524005236700,
+		"id_str": "1181573524005236739",
+		"full_text": "This #OneNote tweet by @thesqlgrrrl made my day.\n\nOneNote is that type of tool that allows you to write notes about anything, anytime, anywhere. \nOrganize notes hierarchically, alphabetically or simply search for them when you need them. https://t.co/ODGe4qoWlS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						5,
+						13
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thesqlgrrrl",
+					"name": "Isabelle Van Campenhoudt #SQL #PowerBI #DataLove",
+					"id": 24179025,
+					"id_str": "24179025",
+					"indices": [
+						23,
+						35
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ODGe4qoWlS",
+					"expanded_url": "https://twitter.com/thesqlgrrrl/status/1181569571419037696",
+					"display_url": "twitter.com/thesqlgrrrl/st…",
+					"indices": [
+						238,
+						261
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22591,
+			"friends_count": 7595,
+			"listed_count": 640,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28727,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25616,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1569512582",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1181569571419037700,
+		"quoted_status_id_str": "1181569571419037696",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ODGe4qoWlS",
+			"expanded": "https://twitter.com/thesqlgrrrl/status/1181569571419037696",
+			"display": "twitter.com/thesqlgrrrl/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 08 13:58:32 +0000 2019",
+			"id": 1181569571419037700,
+			"id_str": "1181569571419037696",
+			"full_text": "When you open #OneNote, create a new notebook about something but an old one already exists and the content is the perfect fit for your day. This is like googling a problem and find the solution on your own blog. My life is an endless  #LostAndFound https://t.co/ZNAg1U2QFD",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				249
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OneNote",
+						"indices": [
+							14,
+							22
+						]
+					},
+					{
+						"text": "LostAndFound",
+						"indices": [
+							236,
+							249
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1181569561298161700,
+						"id_str": "1181569561298161669",
+						"indices": [
+							250,
+							273
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/EGXHQXqXUAUtEkc.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EGXHQXqXUAUtEkc.jpg",
+						"url": "https://t.co/ZNAg1U2QFD",
+						"display_url": "pic.twitter.com/ZNAg1U2QFD",
+						"expanded_url": "https://twitter.com/thesqlgrrrl/status/1181569571419037696/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1181569561298161700,
+						"id_str": "1181569561298161669",
+						"indices": [
+							250,
+							273
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/EGXHQXqXUAUtEkc.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EGXHQXqXUAUtEkc.jpg",
+						"url": "https://t.co/ZNAg1U2QFD",
+						"display_url": "pic.twitter.com/ZNAg1U2QFD",
+						"expanded_url": "https://twitter.com/thesqlgrrrl/status/1181569571419037696/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/EGXHQXqXUAUtEkc.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 24179025,
+				"id_str": "24179025",
+				"name": "Isabelle Van Campenhoudt #SQL #PowerBI #DataLove",
+				"screen_name": "thesqlgrrrl",
+				"location": "Brussels",
+				"description": "Performance Matters Too - Data Platform MVP",
+				"url": "https://t.co/EqMkHQGtqo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/EqMkHQGtqo",
+								"expanded_url": "http://thesqlgrrrl.wordpress.com",
+								"display_url": "thesqlgrrrl.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1410,
+				"friends_count": 557,
+				"listed_count": 102,
+				"created_at": "Fri Mar 13 14:28:33 +0000 2009",
+				"favourites_count": 2581,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3403,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B2DFDA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/967728176972656640/L7vgQ2Hk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/967728176972656640/L7vgQ2Hk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/24179025/1519558193",
+				"profile_link_color": "93A644",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This #OneNote tweet by @thesqlgrrrl made my day.\n\nOneNote is that type of tool that allows you to write notes about anything, anytime, anywhere. \nOrganize notes hierarchically, alphabetically or simply search for them when you need them. https://t.co/ODGe4qoWlS"
+	},
+	{
 		"created_at": "Mon Oct 07 16:34:54 +0000 2019",
 		"id": 1181246534375149600,
 		"id_str": "1181246534375149571",
