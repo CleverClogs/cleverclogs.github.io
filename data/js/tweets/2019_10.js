5bd30b13b81101fc0fe76e1d637b32ae619655c8
CleverClogs <github@cleverclogs.org>
2019-10-26 11:19:29 +0200

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,399 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sat Oct 26 08:48:34 +0000 2019",
+		"id": 1188014545777971200,
+		"id_str": "1188014545777971201",
+		"full_text": "Eye opener.\n\nMatt Miller, of @#DitchThatTxtbk fame, explains educators can now use their Google account to access all of the free #MicrosoftEDU services, including Office Online. \n\nNice comments about #OneNote too!\nThanks for sharing this, @jmattmiller.\n\n#DitchBook #MIEExpert https://t.co/aYUxZ9aV0v",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DitchThatTxtbk",
+					"indices": [
+						30,
+						45
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						130,
+						143
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						201,
+						209
+					]
+				},
+				{
+					"text": "DitchBook",
+					"indices": [
+						255,
+						265
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						266,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jmattmiller",
+					"name": "Matt Miller",
+					"id": 307899854,
+					"id_str": "307899854",
+					"indices": [
+						240,
+						252
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/aYUxZ9aV0v",
+					"expanded_url": "https://twitter.com/jmattmiller/status/1187836764586569729",
+					"display_url": "twitter.com/jmattmiller/st…",
+					"indices": [
+						277,
+						300
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22768,
+			"friends_count": 7680,
+			"listed_count": 640,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28741,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1571558832",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1187836764586569700,
+		"quoted_status_id_str": "1187836764586569729",
+		"quoted_status_permalink": {
+			"url": "https://t.co/aYUxZ9aV0v",
+			"expanded": "https://twitter.com/jmattmiller/status/1187836764586569729",
+			"display": "twitter.com/jmattmiller/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Oct 25 21:02:08 +0000 2019",
+			"id": 1187836764586569700,
+			"id_str": "1187836764586569729",
+			"full_text": "You can log into Microsoft products using a Google account?!?!\n\nThis was a mind-blowing moment for me. No longer do we need to stick to just MS or G. With lots of MS tools being free, we can pick and choose. \n\nDid you know about this? #DitchBook #edtech #microsoftedu #googleedu https://t.co/YJrlAlJjr7",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				278
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "DitchBook",
+						"indices": [
+							235,
+							245
+						]
+					},
+					{
+						"text": "edtech",
+						"indices": [
+							246,
+							253
+						]
+					},
+					{
+						"text": "microsoftedu",
+						"indices": [
+							254,
+							267
+						]
+					},
+					{
+						"text": "googleedu",
+						"indices": [
+							268,
+							278
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1187836582004322300,
+						"id_str": "1187836582004322304",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1187836582004322304/pu/img/-Oz5ckDUwk0fAiwO.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1187836582004322304/pu/img/-Oz5ckDUwk0fAiwO.jpg",
+						"url": "https://t.co/YJrlAlJjr7",
+						"display_url": "pic.twitter.com/YJrlAlJjr7",
+						"expanded_url": "https://twitter.com/jmattmiller/status/1187836764586569729/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1187836582004322300,
+						"id_str": "1187836582004322304",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1187836582004322304/pu/img/-Oz5ckDUwk0fAiwO.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1187836582004322304/pu/img/-Oz5ckDUwk0fAiwO.jpg",
+						"url": "https://t.co/YJrlAlJjr7",
+						"display_url": "pic.twitter.com/YJrlAlJjr7",
+						"expanded_url": "https://twitter.com/jmattmiller/status/1187836764586569729/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 131033,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1187836582004322304/pu/pl/fx51YqAttn1Ad3tm.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1187836582004322304/pu/vid/640x360/9CcNkqdUAg_0RpRs.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1187836582004322304/pu/vid/480x270/oYC_odpE9z1k6h1P.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1187836582004322304/pu/vid/1280x720/yS3aN7ZlemzP84Ux.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 307899854,
+				"id_str": "307899854",
+				"name": "Matt Miller",
+				"screen_name": "jmattmiller",
+				"location": "Indiana #DitchBook #GTTribe",
+				"description": "Educator. #DitchBook Author. Textbook ditcher. GCI #GTAATX. Blogger. Speaker. Podcaster. Christ follower. Happy husband/dad of 3. Livin' the dream.",
+				"url": "http://t.co/DJCmkE0SYz",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/DJCmkE0SYz",
+								"expanded_url": "http://www.DitchThatTextbook.com",
+								"display_url": "DitchThatTextbook.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 77285,
+				"friends_count": 7926,
+				"listed_count": 1555,
+				"created_at": "Mon May 30 13:54:53 +0000 2011",
+				"favourites_count": 92489,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 60204,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/943647258653970432/LYbyJHCs_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/943647258653970432/LYbyJHCs_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/307899854/1427727669",
+				"profile_link_color": "E4BC3F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 117,
+			"favorite_count": 364,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Eye opener.\n\nMatt Miller, of @#DitchThatTxtbk fame, explains educators can now use their Google account to access all of the free #MicrosoftEDU services, including Office Online. \n\nNice comments about #OneNote too!\nThanks for sharing this, @jmattmiller.\n\n#DitchBook #MIEExpert https://t.co/aYUxZ9aV0v"
+	},
+	{
 		"created_at": "Fri Oct 25 15:50:10 +0000 2019",
 		"id": 1187758254769750000,
 		"id_str": "1187758254769750017",
