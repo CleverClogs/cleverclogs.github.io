a0de21f8d14bac39f1d44f4ab66658693225c1ef
CleverClogs <github@cleverclogs.org>
2019-11-04 22:19:28 +0100

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Mon Nov 04 20:58:48 +0000 2019",
+		"id": 1191370297062019000,
+		"id_str": "1191459805388513286",
+		"full_text": "We hear you loud and clear — We are bringing OneNote Desktop (Win32) back into active development with Dark Mode being our first new feature. Our Win10 app remains in active development as well. https://t.co/C3ApyOVF1u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/C3ApyOVF1u",
+					"expanded_url": "https://aka.ms/YourOneNote",
+					"display_url": "aka.ms/YourOneNote",
+					"indices": [
+						195,
+						218
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 729049944133226500,
+			"id_str": "729049944133226496",
+			"name": "Ryan McMinn",
+			"screen_name": "NotedByRyan",
+			"location": "Redmond, WA",
+			"description": "Head of product for Microsoft OneNote & Sticky Notes",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1159,
+			"friends_count": 159,
+			"listed_count": 37,
+			"created_at": "Sat May 07 20:47:06 +0000 2016",
+			"favourites_count": 1490,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 867,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1072157033355329536/ZU_bti5l_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1072157033355329536/ZU_bti5l_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/729049944133226496/1520128968",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 65,
+		"favorite_count": 188,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We hear you loud and clear — We are bringing OneNote Desktop (Win32) back into active development with Dark Mode being our first new feature. Our Win10 app remains in active development as well. https://t.co/C3ApyOVF1u"
+	},
+	{
 		"created_at": "Mon Nov 04 17:23:30 +0000 2019",
 		"id": 1191371383000879000,
 		"id_str": "1191405621482012673",
