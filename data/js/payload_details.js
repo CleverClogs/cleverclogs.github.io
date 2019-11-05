36fa423ad3099042be0a3ebed84167631abbba29
CleverClogs <github@cleverclogs.org>
2019-11-05 22:19:29 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30892,
-	"created_at": "Tue Nov 05 20:19:28 +0000 2019",
+	"tweets": 30893,
+	"created_at": "Tue Nov 05 21:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,291 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Tue Nov 05 20:36:06 +0000 2019",
+		"id": 1191815836253732900,
+		"id_str": "1191816479731277824",
+		"full_text": "Wow! You’ve already traveled 12,183,058 virtual miles and the number keeps climbing. Check back here for live mileage updates! https://t.co/gGBhe4QAhB\n\n#MSFTGlobalConnect #MicrosoftEDU https://t.co/P2O1BKzPNN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			184
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTGlobalConnect",
+					"indices": [
+						152,
+						170
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						171,
+						184
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gGBhe4QAhB",
+					"expanded_url": "http://msft.social/zIgUoV",
+					"display_url": "msft.social/zIgUoV",
+					"indices": [
+						127,
+						150
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1191815833556832300,
+					"id_str": "1191815833556832256",
+					"indices": [
+						185,
+						208
+					],
+					"media_url": "http://pbs.twimg.com/media/EIouL6TXUAAubr3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIouL6TXUAAubr3.jpg",
+					"url": "https://t.co/P2O1BKzPNN",
+					"display_url": "pic.twitter.com/P2O1BKzPNN",
+					"expanded_url": "https://twitter.com/SkypeClassroom/status/1191815836253732871/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1214,
+							"h": 683,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 467,
+									"y": 88,
+									"h": 252,
+									"w": 252
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 467,
+									"y": 88,
+									"h": 252,
+									"w": 252
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 261,
+									"y": 49,
+									"h": 141,
+									"w": 141
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 461,
+									"y": 86,
+									"h": 249,
+									"w": 249
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1191815833556832300,
+					"id_str": "1191815833556832256",
+					"indices": [
+						185,
+						208
+					],
+					"media_url": "http://pbs.twimg.com/media/EIouL6TXUAAubr3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIouL6TXUAAubr3.jpg",
+					"url": "https://t.co/P2O1BKzPNN",
+					"display_url": "pic.twitter.com/P2O1BKzPNN",
+					"expanded_url": "https://twitter.com/SkypeClassroom/status/1191815836253732871/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1214,
+							"h": 683,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 467,
+									"y": 88,
+									"h": 252,
+									"w": 252
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 467,
+									"y": 88,
+									"h": 252,
+									"w": 252
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 261,
+									"y": 49,
+									"h": 141,
+									"w": 141
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 461,
+									"y": 86,
+									"h": 249,
+									"w": 249
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://prod2.sprinklr.com\" rel=\"nofollow\">Sprinklr Publisher</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 214027732,
+			"id_str": "214027732",
+			"name": "Skype in the Classroom",
+			"screen_name": "SkypeClassroom",
+			"location": "Everywhere",
+			"description": "Bringing the 🌎 to every classroom and empowering global voice through free live educational experiences! #MicrosoftEDU #SkypeClassroom",
+			"url": "https://t.co/4j25UxjnNz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4j25UxjnNz",
+							"expanded_url": "http://www.skypeintheclassroom.com/",
+							"display_url": "skypeintheclassroom.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 108529,
+			"friends_count": 8637,
+			"listed_count": 1562,
+			"created_at": "Wed Nov 10 11:32:30 +0000 2010",
+			"favourites_count": 43561,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 32740,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "25AFF0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1177666887179804674/EoCKOUST_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1177666887179804674/EoCKOUST_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/214027732/1569571632",
+			"profile_link_color": "00B0F0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DAF1FA",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wow! You’ve already traveled 12,183,058 virtual miles and the number keeps climbing. Check back here for live mileage updates! https://t.co/gGBhe4QAhB\n\n#MSFTGlobalConnect #MicrosoftEDU https://t.co/P2O1BKzPNN"
+	},
+	{
 		"created_at": "Tue Nov 05 19:48:03 +0000 2019",
 		"id": 1191370712793133000,
 		"id_str": "1191804387519336448",
