4f66f948937cb38d9329f05ef61fe6a575342e2d
CleverClogs <github@cleverclogs.org>
2019-11-18 16:19:29 +0100

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Mon Nov 18 14:40:24 +0000 2019",
+		"id": 1195433836148142000,
+		"id_str": "1196438007638827008",
+		"full_text": "We're rolling out a new feature for #OneNote 2016: Dark Mode! Change your apps interface from light to dark to provide better contrast and improve readability. Give it a try! https://t.co/he1lDRmaE1 #MicrosoftEDU https://t.co/2ZEFYtAvEK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			212
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						36,
+						44
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						199,
+						212
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/he1lDRmaE1",
+					"expanded_url": "http://msft.social/fYUnSK",
+					"display_url": "msft.social/fYUnSK",
+					"indices": [
+						175,
+						198
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1195433793219416000,
+					"id_str": "1195433793219416065",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EJcIs4VXUAEejJw.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EJcIs4VXUAEejJw.jpg",
+					"url": "https://t.co/2ZEFYtAvEK",
+					"display_url": "pic.twitter.com/2ZEFYtAvEK",
+					"expanded_url": "https://twitter.com/MicrosoftEDU/status/1195433836148142081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1195433793219416000,
+					"id_str": "1195433793219416065",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EJcIs4VXUAEejJw.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EJcIs4VXUAEejJw.jpg",
+					"url": "https://t.co/2ZEFYtAvEK",
+					"display_url": "pic.twitter.com/2ZEFYtAvEK",
+					"expanded_url": "https://twitter.com/MicrosoftEDU/status/1195433836148142081/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EJcIs4VXUAEejJw.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"https://prod2.sprinklr.com\" rel=\"nofollow\">Sprinklr Publisher</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17826187,
+			"id_str": "17826187",
+			"name": "Microsoft Education",
+			"screen_name": "MicrosoftEDU",
+			"location": "Redmond, WA",
+			"description": "Empowering every student and educator to achieve more with the best of Microsoft tips and resources for education. #MicrosoftEDU",
+			"url": "https://t.co/PstVZHAjVU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PstVZHAjVU",
+							"expanded_url": "https://educationblog.microsoft.com",
+							"display_url": "educationblog.microsoft.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 369537,
+			"friends_count": 4379,
+			"listed_count": 3394,
+			"created_at": "Wed Dec 03 02:29:33 +0000 2008",
+			"favourites_count": 26694,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 38222,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "00BCF2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1062798346409074688/6pQHQSeL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1062798346409074688/6pQHQSeL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17826187/1508952382",
+			"profile_link_color": "0078D7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 78,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We're rolling out a new feature for #OneNote 2016: Dark Mode! Change your apps interface from light to dark to provide better contrast and improve readability. Give it a try! https://t.co/he1lDRmaE1 #MicrosoftEDU https://t.co/2ZEFYtAvEK"
+	},
+	{
 		"created_at": "Sun Nov 17 23:12:53 +0000 2019",
 		"id": 1195713316645032000,
 		"id_str": "1196204589680345089",
