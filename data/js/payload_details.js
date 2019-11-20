a71fcc8a26a5ffb93e84a3c2c5b07c6050010dfc
CleverClogs <github@cleverclogs.org>
2019-11-20 19:19:29 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30911,
-	"created_at": "Wed Nov 20 17:19:28 +0000 2019",
+	"tweets": 30912,
+	"created_at": "Wed Nov 20 18:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,10 +1,146 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Wed Nov 20 18:00:55 +0000 2019",
+		"id": 1197213247138619400,
+		"id_str": "1197213247138619393",
+		"full_text": "@will_k_hoven @DarrellaaS @msonenote As a handy pair of Format Painter keyboard shortcuts, you may want to memorize these two:\n\nCtrl+Shift+C = copy format\nCtrl+Shift+V = paste format \n\nThese keystrokes work in #OneNote, Word, PowerPoint and Outlook.",
+		"truncated": false,
+		"display_text_range": [
+			37,
+			249
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						210,
+						218
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "will_k_hoven",
+					"name": "Will K. Hoven",
+					"id": 1179472299478659000,
+					"id_str": "1179472299478659074",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "DarrellaaS",
+					"name": "Darrell as a Service 🛎",
+					"id": 401809242,
+					"id_str": "401809242",
+					"indices": [
+						14,
+						25
+					]
+				},
+				{
+					"screen_name": "msonenote",
+					"name": "Microsoft OneNote",
+					"id": 23735316,
+					"id_str": "23735316",
+					"indices": [
+						26,
+						36
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1197191159409496000,
+		"in_reply_to_status_id_str": "1197191159409496065",
+		"in_reply_to_user_id": 1179472299478659000,
+		"in_reply_to_user_id_str": "1179472299478659074",
+		"in_reply_to_screen_name": "will_k_hoven",
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "World",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/Udn06tcMta",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Udn06tcMta",
+							"expanded_url": "http://onen.link/onenote-community-notebook-read-only",
+							"display_url": "onen.link/onenote-commun…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23051,
+			"friends_count": 7720,
+			"listed_count": 645,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28740,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25676,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1571558832",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@will_k_hoven @DarrellaaS @msonenote As a handy pair of Format Painter keyboard shortcuts, you may want to memorize these two:\n\nCtrl+Shift+C = copy format\nCtrl+Shift+V = paste format \n\nThese keystrokes work in #OneNote, Word, PowerPoint and Outlook."
+	},
+	{
 		"created_at": "Wed Nov 20 16:28:54 +0000 2019",
 		"id": 1197185408288903200,
 		"id_str": "1197190087492165632",
-		"full_text": "I'm so proud of my great friend and 🎈Team @TweetMeet member @fcotexeira 🧙‍♂️\n\nI am learning so much from you and our amazing @OneNoteC! \n\nGrateful forever 💖 https://t.co/DSXknn8giu",
+		"full_text": "I'm so proud of my great friend and 🎈Team @TweetMeet member @fcotexeira 🧙♂️\n\nI am learning so much from you and our amazing @OneNoteC! \n\nGrateful forever 💖 https://t.co/DSXknn8giu",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -505,7 +641,7 @@ Grailbird.data.tweets_2019_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "I'm so proud of my great friend and 🎈Team @TweetMeet member @fcotexeira 🧙‍♂️\n\nI am learning so much from you and our amazing @OneNoteC! \n\nGrateful forever 💖 https://t.co/DSXknn8giu"
+		"text": "I'm so proud of my great friend and 🎈Team @TweetMeet member @fcotexeira 🧙♂️\n\nI am learning so much from you and our amazing @OneNoteC! \n\nGrateful forever 💖 https://t.co/DSXknn8giu"
 	},
 	{
 		"created_at": "Tue Nov 19 21:06:42 +0000 2019",
