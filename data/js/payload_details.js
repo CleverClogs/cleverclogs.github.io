d6c3d3e7b014e4781ebeccc06d4d9ea6d3b6f5d5
CleverClogs <github@cleverclogs.org>
2019-12-02 21:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30924,
-	"created_at": "Wed Nov 27 20:19:28 +0000 2019",
+	"tweets": 30925,
+	"created_at": "Mon Dec 02 20:19:28 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,456 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Mon Dec 02 19:36:42 +0000 2019",
+		"id": 1201585497094664200,
+		"id_str": "1201586004542529543",
+		"full_text": "🎈 Take your Wakelet Collections to the next level.\n\nUse Wakelet's Text cards to add annotations. Explain the context of your resources and why your followers should check them out.\n\nMore details in the blog post below:\nThank you, @kyteijas! \n#MicrosoftEDU #MIEExpert #MSFTEduChat https://t.co/ExQ0Qxx3a9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						242,
+						255
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						256,
+						266
+					]
+				},
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						267,
+						279
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kyteijas",
+					"name": "Teijas",
+					"id": 308328117,
+					"id_str": "308328117",
+					"indices": [
+						230,
+						239
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ExQ0Qxx3a9",
+					"expanded_url": "https://twitter.com/kyteijas/status/1201584317861728256",
+					"display_url": "twitter.com/kyteijas/statu…",
+					"indices": [
+						280,
+						303
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat",
+			"screen_name": "TweetMeet",
+			"location": "All TweetMeet announcements on the Microsoft Education blog:",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU connecting educators. \nTeam TweetMeet: Marjolein Hoekstra, @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/vTmAj2KFoN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vTmAj2KFoN",
+							"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+							"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4019,
+			"friends_count": 3545,
+			"listed_count": 33,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 4893,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1466,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1574727601",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1201584317861728300,
+		"quoted_status_id_str": "1201584317861728256",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ExQ0Qxx3a9",
+			"expanded": "https://twitter.com/kyteijas/status/1201584317861728256",
+			"display": "twitter.com/kyteijas/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Dec 02 19:30:00 +0000 2019",
+			"id": 1201584317861728300,
+			"id_str": "1201584317861728256",
+			"full_text": "Check out this amazing @wakelet blog by @CleverClogs on the art of content curation! 🖼️👩‍🎨\n\nRead on 👇\n\nhttps://t.co/kN08aia82y\n\n#WakeletWave #MIEExpert https://t.co/R063kCv3LZ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				151
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "WakeletWave",
+						"indices": [
+							128,
+							140
+						]
+					},
+					{
+						"text": "MIEExpert",
+						"indices": [
+							141,
+							151
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "wakelet",
+						"name": "Wakelet",
+						"id": 169922611,
+						"id_str": "169922611",
+						"indices": [
+							23,
+							31
+						]
+					},
+					{
+						"screen_name": "CleverClogs",
+						"name": "Marjolein Hoekstra",
+						"id": 1424471,
+						"id_str": "1424471",
+						"indices": [
+							40,
+							52
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/kN08aia82y",
+						"expanded_url": "https://blog.wakelet.com/2019/10/11/content-curation-with-wakelet/",
+						"display_url": "blog.wakelet.com/2019/10/11/con…",
+						"indices": [
+							103,
+							126
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1201466496276410400,
+						"id_str": "1201466496276410369",
+						"indices": [
+							152,
+							175
+						],
+						"media_url": "http://pbs.twimg.com/media/EKx3aagWsAEUNFO.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EKx3aagWsAEUNFO.jpg",
+						"url": "https://t.co/R063kCv3LZ",
+						"display_url": "pic.twitter.com/R063kCv3LZ",
+						"expanded_url": "https://twitter.com/kyteijas/status/1201584317861728256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1159,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 1024,
+										"y": 377,
+										"h": 447,
+										"w": 447
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 290,
+										"y": 106,
+										"h": 126,
+										"w": 126
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 873,
+										"y": 321,
+										"h": 381,
+										"w": 381
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 512,
+										"y": 188,
+										"h": 223,
+										"w": 223
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1201466496276410400,
+						"id_str": "1201466496276410369",
+						"indices": [
+							152,
+							175
+						],
+						"media_url": "http://pbs.twimg.com/media/EKx3aagWsAEUNFO.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EKx3aagWsAEUNFO.jpg",
+						"url": "https://t.co/R063kCv3LZ",
+						"display_url": "pic.twitter.com/R063kCv3LZ",
+						"expanded_url": "https://twitter.com/kyteijas/status/1201584317861728256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1159,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 1024,
+										"y": 377,
+										"h": 447,
+										"w": 447
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 290,
+										"y": 106,
+										"h": 126,
+										"w": 126
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 873,
+										"y": 321,
+										"h": 381,
+										"w": 381
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 512,
+										"y": 188,
+										"h": 223,
+										"w": 223
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 308328117,
+				"id_str": "308328117",
+				"name": "Teijas",
+				"screen_name": "kyteijas",
+				"location": "Manchester, England",
+				"description": "Business Development lead/Engagement @Wakelet. Ride the #WakeletWave 🌊 - teijas@wakelet.com",
+				"url": "https://t.co/LjJpN8kET5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/LjJpN8kET5",
+								"expanded_url": "http://www.wakelet.com/@teijasky",
+								"display_url": "wakelet.com/@teijasky",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2013,
+				"friends_count": 2340,
+				"listed_count": 7,
+				"created_at": "Tue May 31 06:43:41 +0000 2011",
+				"favourites_count": 2889,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1520,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1019982926828711939/9uCZxRM3_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1019982926828711939/9uCZxRM3_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/308328117/1565257860",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎈 Take your Wakelet Collections to the next level.\n\nUse Wakelet's Text cards to add annotations. Explain the context of your resources and why your followers should check them out.\n\nMore details in the blog post below:\nThank you, @kyteijas! \n#MicrosoftEDU #MIEExpert #MSFTEduChat https://t.co/ExQ0Qxx3a9"
+	}
+]
