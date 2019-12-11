048be175c40b91d701d7f41dcf5717a441296b0f
CleverClogs <github@cleverclogs.org>
2019-12-11 23:19:31 +0100

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,351 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Wed Dec 11 21:22:58 +0000 2019",
+		"id": 1204835742041723000,
+		"id_str": "1204874239351885825",
+		"full_text": "Going paperless with #Teams and #OneNote in Anatomy as Ss draw microscopic bone structures @MonRegHS https://t.co/WbSKcv1yPf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Teams",
+					"indices": [
+						21,
+						27
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						32,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MonRegHS",
+					"name": "Monmouth Regional HS",
+					"id": 1668235670,
+					"id_str": "1668235670",
+					"indices": [
+						91,
+						100
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1204835638857666600,
+					"id_str": "1204835638857666561",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/ELhvoUrXkAET62x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELhvoUrXkAET62x.jpg",
+					"url": "https://t.co/WbSKcv1yPf",
+					"display_url": "pic.twitter.com/WbSKcv1yPf",
+					"expanded_url": "https://twitter.com/SprSciTeachMRHS/status/1204835742041722881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1204835638857666600,
+					"id_str": "1204835638857666561",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/ELhvoUrXkAET62x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELhvoUrXkAET62x.jpg",
+					"url": "https://t.co/WbSKcv1yPf",
+					"display_url": "pic.twitter.com/WbSKcv1yPf",
+					"expanded_url": "https://twitter.com/SprSciTeachMRHS/status/1204835742041722881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1204835668159058000,
+					"id_str": "1204835668159057922",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/ELhvqB1XUAI91Tk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELhvqB1XUAI91Tk.jpg",
+					"url": "https://t.co/WbSKcv1yPf",
+					"display_url": "pic.twitter.com/WbSKcv1yPf",
+					"expanded_url": "https://twitter.com/SprSciTeachMRHS/status/1204835742041722881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 974,
+									"y": 728,
+									"h": 76,
+									"w": 76
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 323,
+									"y": 241,
+									"h": 25,
+									"w": 25
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 570,
+									"y": 426,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 974,
+									"y": 728,
+									"h": 76,
+									"w": 76
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1204835692699955200,
+					"id_str": "1204835692699955200",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/ELhvrdQXsAAw8UQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELhvrdQXsAAw8UQ.jpg",
+					"url": "https://t.co/WbSKcv1yPf",
+					"display_url": "pic.twitter.com/WbSKcv1yPf",
+					"expanded_url": "https://twitter.com/SprSciTeachMRHS/status/1204835742041722881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2567912267,
+			"id_str": "2567912267",
+			"name": "C_Famelio",
+			"screen_name": "SprSciTeachMRHS",
+			"location": "",
+			"description": "Science Teacher at MRHS; Microsoft Certified Educator; MIE Expert 2018-19, 2019-20",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 192,
+			"friends_count": 218,
+			"listed_count": 13,
+			"created_at": "Tue May 27 21:29:06 +0000 2014",
+			"favourites_count": 2031,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 930,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1037512320619040769/pEOa08oF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1037512320619040769/pEOa08oF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2567912267/1472153416",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Going paperless with #Teams and #OneNote in Anatomy as Ss draw microscopic bone structures @MonRegHS https://t.co/WbSKcv1yPf"
+	},
+	{
 		"created_at": "Mon Dec 09 22:07:37 +0000 2019",
 		"id": 1204160697921425400,
 		"id_str": "1204160697921425408",
