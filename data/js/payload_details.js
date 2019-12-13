e72c601954119b9d359f5f735cc76e4a2c0b7b39
CleverClogs <github@cleverclogs.org>
2019-12-13 19:19:29 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30937,
-	"created_at": "Wed Dec 11 23:19:28 +0000 2019",
+	"tweets": 30938,
+	"created_at": "Fri Dec 13 18:19:28 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 13 18:10:11 +0000 2019",
+		"id": 1205541929443905500,
+		"id_str": "1205550498713100289",
+		"full_text": "🎈 Wondering what it's like to be a TweetMeet host?\n\nIn this ⚡ brand-new ⚡ Wakelet, many #MSFTEduChat hosts reflect on how our #Road2TweetMeet program boosts their global connections and their digital-skills levels:\nhttps://t.co/fytp0Yy4sL\n\n#BestOf2019 #MicrosoftEdu #MIEExpert https://t.co/dBZhYcudVr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						88,
+						100
+					]
+				},
+				{
+					"text": "Road2TweetMeet",
+					"indices": [
+						126,
+						141
+					]
+				},
+				{
+					"text": "BestOf2019",
+					"indices": [
+						240,
+						251
+					]
+				},
+				{
+					"text": "MicrosoftEdu",
+					"indices": [
+						252,
+						265
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						266,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fytp0Yy4sL",
+					"expanded_url": "https://wakelet.com/wake/73a00196-bebc-49d8-8d00-8d0da7415cdc",
+					"display_url": "wakelet.com/wake/73a00196-…",
+					"indices": [
+						215,
+						238
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1205540081186029600,
+					"id_str": "1205540081186029568",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/ELrwUQ2WwAAXJ_q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELrwUQ2WwAAXJ_q.jpg",
+					"url": "https://t.co/dBZhYcudVr",
+					"display_url": "pic.twitter.com/dBZhYcudVr",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1205541929443905536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1205540081186029600,
+					"id_str": "1205540081186029568",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/ELrwUQ2WwAAXJ_q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELrwUQ2WwAAXJ_q.jpg",
+					"url": "https://t.co/dBZhYcudVr",
+					"display_url": "pic.twitter.com/dBZhYcudVr",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1205541929443905536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat",
+			"screen_name": "TweetMeet",
+			"location": "All TweetMeet announcements on the Microsoft Education blog:",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU connecting educators. \nTeam TweetMeet: Marjolein Hoekstra, @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/vTmAj2KFoN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vTmAj2KFoN",
+							"expanded_url": "https://educationblog.microsoft.com/tag/tweetmeet",
+							"display_url": "educationblog.microsoft.com/tag/tweetmeet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4076,
+			"friends_count": 3559,
+			"listed_count": 34,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 5010,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1508,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1574727601",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎈 Wondering what it's like to be a TweetMeet host?\n\nIn this ⚡ brand-new ⚡ Wakelet, many #MSFTEduChat hosts reflect on how our #Road2TweetMeet program boosts their global connections and their digital-skills levels:\nhttps://t.co/fytp0Yy4sL\n\n#BestOf2019 #MicrosoftEdu #MIEExpert https://t.co/dBZhYcudVr"
+	},
+	{
 		"created_at": "Wed Dec 11 22:40:15 +0000 2019",
 		"id": 1204893685005066200,
 		"id_str": "1204893685005066240",
