cbc5966e8d321781075d23bbb8eb455c3f7c8f12
CleverClogs <github@cleverclogs.org>
2019-12-27 23:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30941,
-	"created_at": "Fri Dec 20 18:19:27 +0000 2019",
+	"tweets": 30942,
+	"created_at": "Fri Dec 27 22:19:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,283 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 27 21:20:53 +0000 2019",
+		"id": 1210671921412681700,
+		"id_str": "1210671921412681728",
+		"full_text": "Let's take the #SurfaceGo for a thorough spin. \n\nIn-depth review of this 'pretty baby', by UX Design student Jason Brown. How will it match up with a Surface Pro?\n\nhttps://t.co/N2j3Kqh6mz\nby @JasonBrown2K13 on @SurfaceEDUGuy's blog\n#OneNote #Microsoft\n\ncc @panos_panay @ralfgroene https://t.co/bXL2bZFzgC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SurfaceGo",
+					"indices": [
+						15,
+						25
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						232,
+						240
+					]
+				},
+				{
+					"text": "Microsoft",
+					"indices": [
+						241,
+						251
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JasonBrown2K13",
+					"name": "Jason Brown",
+					"id": 1726525092,
+					"id_str": "1726525092",
+					"indices": [
+						191,
+						206
+					]
+				},
+				{
+					"screen_name": "SurfaceEDUGuy",
+					"name": "SurfaceEDUGuy",
+					"id": 1092825050640724000,
+					"id_str": "1092825050640723969",
+					"indices": [
+						210,
+						224
+					]
+				},
+				{
+					"screen_name": "panos_panay",
+					"name": "Panos Panay",
+					"id": 20416077,
+					"id_str": "20416077",
+					"indices": [
+						256,
+						268
+					]
+				},
+				{
+					"screen_name": "ralfgroene",
+					"name": "Ralf Groene",
+					"id": 883666477,
+					"id_str": "883666477",
+					"indices": [
+						269,
+						280
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/N2j3Kqh6mz",
+					"expanded_url": "https://surfaceeduguy.home.blog/2019/12/13/surface-go-a-pretty-baby-but-does-it-go/",
+					"display_url": "surfaceeduguy.home.blog/2019/12/13/sur…",
+					"indices": [
+						164,
+						187
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1210671906862633000,
+					"id_str": "1210671906862632960",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/EM0rr1QXkAAhlKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EM0rr1QXkAAhlKN.jpg",
+					"url": "https://t.co/bXL2bZFzgC",
+					"display_url": "pic.twitter.com/bXL2bZFzgC",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1210671921412681728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 267,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 804,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 471,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1210671906862633000,
+					"id_str": "1210671906862632960",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/EM0rr1QXkAAhlKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EM0rr1QXkAAhlKN.jpg",
+					"url": "https://t.co/bXL2bZFzgC",
+					"display_url": "pic.twitter.com/bXL2bZFzgC",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1210671921412681728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 267,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 804,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 471,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "OneNote 2016 Secrets book:",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/jNdXyq42Kc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jNdXyq42Kc",
+							"expanded_url": "https://www.amazon.com/OneNote-Secrets-Tips-2013-2016-ebook/dp/B01NAR5HNJ/",
+							"display_url": "amazon.com/OneNote-Secret…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23296,
+			"friends_count": 7724,
+			"listed_count": 642,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28759,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25698,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1574945310",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Let's take the #SurfaceGo for a thorough spin. \n\nIn-depth review of this 'pretty baby', by UX Design student Jason Brown. How will it match up with a Surface Pro?\n\nhttps://t.co/N2j3Kqh6mz\nby @JasonBrown2K13 on @SurfaceEDUGuy's blog\n#OneNote #Microsoft\n\ncc @panos_panay @ralfgroene https://t.co/bXL2bZFzgC"
+	},
+	{
 		"created_at": "Fri Dec 20 16:52:00 +0000 2019",
 		"id": 1207772097642193000,
 		"id_str": "1208067536186687490",
