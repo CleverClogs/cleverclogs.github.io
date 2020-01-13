765bf6555bf4225b389235f80bc0e5918e8ac1fa
CleverClogs <github@cleverclogs.org>
2020-01-13 19:19:35 +0100

    Added 1 tweets

--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,690 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 13 18:19:13 +0000 2020",
+		"id": 1216786796228567000,
+		"id_str": "1216786796228567046",
+		"full_text": "Immersive Reader keeps spreading across edtech tools and services. \n\nToday, #MicrosoftEDU's @MTholfsen announces the launch of the beta testing phase of #ImmersiveReader integration with @PearDeck.\n\nIn addition to Mike's post below, do read this post too: \nhttps://t.co/JDpnePKSdC https://t.co/uVWG8iNoxx https://t.co/eIL3uMW3SU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			304
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						76,
+						89
+					]
+				},
+				{
+					"text": "ImmersiveReader",
+					"indices": [
+						153,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mtholfsen",
+					"name": "Mike Tholfsen",
+					"id": 27673327,
+					"id_str": "27673327",
+					"indices": [
+						92,
+						102
+					]
+				},
+				{
+					"screen_name": "PearDeck",
+					"name": "Pear Deck",
+					"id": 1849321280,
+					"id_str": "1849321280",
+					"indices": [
+						187,
+						196
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JDpnePKSdC",
+					"expanded_url": "https://www.peardeck.com/pear-deck-blog/2020/1/9/building-inclusive-classrooms-with-pear-deck-immersive-reader",
+					"display_url": "peardeck.com/pear-deck-blog…",
+					"indices": [
+						257,
+						280
+					]
+				},
+				{
+					"url": "https://t.co/uVWG8iNoxx",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1216768773614526464",
+					"display_url": "twitter.com/mtholfsen/stat…",
+					"indices": [
+						281,
+						304
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1216786787366064000,
+					"id_str": "1216786787366064128",
+					"indices": [
+						305,
+						328
+					],
+					"media_url": "http://pbs.twimg.com/media/EOLlIuRX0AAskHD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOLlIuRX0AAskHD.jpg",
+					"url": "https://t.co/eIL3uMW3SU",
+					"display_url": "pic.twitter.com/eIL3uMW3SU",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1216786796228567046/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1603,
+							"h": 836,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 626,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1216786787366064000,
+					"id_str": "1216786787366064128",
+					"indices": [
+						305,
+						328
+					],
+					"media_url": "http://pbs.twimg.com/media/EOLlIuRX0AAskHD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOLlIuRX0AAskHD.jpg",
+					"url": "https://t.co/eIL3uMW3SU",
+					"display_url": "pic.twitter.com/eIL3uMW3SU",
+					"expanded_url": "https://twitter.com/OneNoteC/status/1216786796228567046/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1603,
+							"h": 836,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 626,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "OneNote 2016 Secrets book:",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/jNdXyq42Kc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jNdXyq42Kc",
+							"expanded_url": "https://www.amazon.com/OneNote-Secrets-Tips-2013-2016-ebook/dp/B01NAR5HNJ/",
+							"display_url": "amazon.com/OneNote-Secret…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23352,
+			"friends_count": 7735,
+			"listed_count": 643,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25700,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1577664478",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1216768773614526500,
+		"quoted_status_id_str": "1216768773614526464",
+		"quoted_status_permalink": {
+			"url": "https://t.co/uVWG8iNoxx",
+			"expanded": "https://twitter.com/mtholfsen/status/1216768773614526464",
+			"display": "twitter.com/mtholfsen/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jan 13 17:07:36 +0000 2020",
+			"id": 1216768773614526500,
+			"id_str": "1216768773614526464",
+			"full_text": "NEW! @PearDeck 🍐 integrates with the Microsoft Immersive Reader to make learning more accessible for all 💯 #edtech #accessibility #MIEExpert\n\nDetails at the blog link below\n\n👉 https://t.co/0F2xckSh2v https://t.co/dAVa5wIyhT",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				199
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "edtech",
+						"indices": [
+							107,
+							114
+						]
+					},
+					{
+						"text": "accessibility",
+						"indices": [
+							115,
+							129
+						]
+					},
+					{
+						"text": "MIEExpert",
+						"indices": [
+							130,
+							140
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "PearDeck",
+						"name": "Pear Deck",
+						"id": 1849321280,
+						"id_str": "1849321280",
+						"indices": [
+							5,
+							14
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/0F2xckSh2v",
+						"expanded_url": "https://techcommunity.microsoft.com/t5/education-blog/pear-deck-integrates-with-microsoft-immersive-reader-to-make/ba-p/1101740",
+						"display_url": "techcommunity.microsoft.com/t5/education-b…",
+						"indices": [
+							176,
+							199
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1216768766719086600,
+						"id_str": "1216768766719086593",
+						"indices": [
+							200,
+							223
+						],
+						"media_url": "http://pbs.twimg.com/media/EOLUvyIU0AEnw0n.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EOLUvyIU0AEnw0n.jpg",
+						"url": "https://t.co/dAVa5wIyhT",
+						"display_url": "pic.twitter.com/dAVa5wIyhT",
+						"expanded_url": "https://twitter.com/mtholfsen/status/1216768773614526464/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1513,
+								"h": 856,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"all": {
+								"tags": [
+									{
+										"type": "user",
+										"user_id": "1849321280",
+										"screen_name": "PearDeck",
+										"name": "Pear Deck"
+									},
+									{
+										"type": "user",
+										"user_id": "17826187",
+										"screen_name": "MicrosoftEDU",
+										"name": "Microsoft Education"
+									},
+									{
+										"type": "user",
+										"user_id": "2853888990",
+										"screen_name": "Bohnzy",
+										"name": "Ben Bohnson"
+									},
+									{
+										"type": "user",
+										"user_id": "6339822",
+										"screen_name": "Steven_Abrahams",
+										"name": "Steven Abrahams"
+									},
+									{
+										"type": "user",
+										"user_id": "458619342",
+										"screen_name": "MSFTEnable",
+										"name": "MSFT Accessibility"
+									},
+									{
+										"type": "user",
+										"user_id": "2582768738",
+										"screen_name": "OneNoteC",
+										"name": "OneNote Central"
+									},
+									{
+										"type": "user",
+										"user_id": "15491451",
+										"screen_name": "hojusaram",
+										"name": "Troy Waller"
+									},
+									{
+										"type": "user",
+										"user_id": "35310749",
+										"screen_name": "ArranDyslexia",
+										"name": "Arran Smith"
+									},
+									{
+										"type": "user",
+										"user_id": "3075876573",
+										"screen_name": "rachelmberger",
+										"name": "Rachel Berger"
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 406,
+										"y": 188,
+										"h": 104,
+										"w": 104
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 717,
+										"y": 332,
+										"h": 184,
+										"w": 184
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 905,
+										"y": 419,
+										"h": 233,
+										"w": 233
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 905,
+										"y": 419,
+										"h": 233,
+										"w": 233
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1216768766719086600,
+						"id_str": "1216768766719086593",
+						"indices": [
+							200,
+							223
+						],
+						"media_url": "http://pbs.twimg.com/media/EOLUvyIU0AEnw0n.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EOLUvyIU0AEnw0n.jpg",
+						"url": "https://t.co/dAVa5wIyhT",
+						"display_url": "pic.twitter.com/dAVa5wIyhT",
+						"expanded_url": "https://twitter.com/mtholfsen/status/1216768773614526464/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1513,
+								"h": 856,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"all": {
+								"tags": [
+									{
+										"type": "user",
+										"user_id": "1849321280",
+										"screen_name": "PearDeck",
+										"name": "Pear Deck"
+									},
+									{
+										"type": "user",
+										"user_id": "17826187",
+										"screen_name": "MicrosoftEDU",
+										"name": "Microsoft Education"
+									},
+									{
+										"type": "user",
+										"user_id": "2853888990",
+										"screen_name": "Bohnzy",
+										"name": "Ben Bohnson"
+									},
+									{
+										"type": "user",
+										"user_id": "6339822",
+										"screen_name": "Steven_Abrahams",
+										"name": "Steven Abrahams"
+									},
+									{
+										"type": "user",
+										"user_id": "458619342",
+										"screen_name": "MSFTEnable",
+										"name": "MSFT Accessibility"
+									},
+									{
+										"type": "user",
+										"user_id": "2582768738",
+										"screen_name": "OneNoteC",
+										"name": "OneNote Central"
+									},
+									{
+										"type": "user",
+										"user_id": "15491451",
+										"screen_name": "hojusaram",
+										"name": "Troy Waller"
+									},
+									{
+										"type": "user",
+										"user_id": "35310749",
+										"screen_name": "ArranDyslexia",
+										"name": "Arran Smith"
+									},
+									{
+										"type": "user",
+										"user_id": "3075876573",
+										"screen_name": "rachelmberger",
+										"name": "Rachel Berger"
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 406,
+										"y": 188,
+										"h": 104,
+										"w": 104
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 717,
+										"y": 332,
+										"h": 184,
+										"w": 184
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 905,
+										"y": 419,
+										"h": 233,
+										"w": 233
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 905,
+										"y": 419,
+										"h": 233,
+										"w": 233
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 27673327,
+				"id_str": "27673327",
+				"name": "Mike Tholfsen",
+				"screen_name": "mtholfsen",
+				"location": "Redmond, WA",
+				"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, #ImmersiveReader & #MicrosoftTeams @OneNoteEDU #edtech #accessibility",
+				"url": "https://t.co/ANIFpSE2FB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ANIFpSE2FB",
+								"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+								"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 22824,
+				"friends_count": 3513,
+				"listed_count": 771,
+				"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+				"favourites_count": 87334,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 34728,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+				"profile_link_color": "9266CC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Immersive Reader keeps spreading across edtech tools and services. \n\nToday, #MicrosoftEDU's @MTholfsen announces the launch of the beta testing phase of #ImmersiveReader integration with @PearDeck.\n\nIn addition to Mike's post below, do read this post too: \nhttps://t.co/JDpnePKSdC https://t.co/uVWG8iNoxx https://t.co/eIL3uMW3SU"
+	},
+	{
 		"created_at": "Mon Jan 13 12:26:05 +0000 2020",
 		"id": 1216010961439613000,
 		"id_str": "1216697925172109314",
