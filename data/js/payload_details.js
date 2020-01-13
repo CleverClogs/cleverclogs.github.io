9db12c8dc99575e2e7d4193188b6ffbb4f21c07d
CleverClogs <github@cleverclogs.org>
2020-01-13 14:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30945,
-	"created_at": "Mon Jan 06 08:19:27 +0000 2020",
+	"tweets": 30946,
+	"created_at": "Mon Jan 13 13:19:27 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,826 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 13 12:26:05 +0000 2020",
+		"id": 1216010961439613000,
+		"id_str": "1216697925172109314",
+		"full_text": "🥁 🎈  Announcing our hosts! \nWe're super-proud to announce the ⚡brand-new ⚡ crew for the #MSFTEduChat TweetMeet about Devices for #StudentSuccess.\n\nFollow them all through this Twitter List:\nhttps://t.co/m3SBMDwmMT\n\nWill join us January 21? We love 💖 your RT 🔁\n#MicrosoftEDU https://t.co/lqvY7wq2XX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSFTEduChat",
+					"indices": [
+						88,
+						100
+					]
+				},
+				{
+					"text": "StudentSuccess",
+					"indices": [
+						129,
+						144
+					]
+				},
+				{
+					"text": "MicrosoftEDU",
+					"indices": [
+						260,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/m3SBMDwmMT",
+					"expanded_url": "https://twitter.com/tweetmeet/lists/msfteduchat-2020-01",
+					"display_url": "twitter.com/tweetmeet/list…",
+					"indices": [
+						190,
+						213
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1216010958226755600,
+					"id_str": "1216010958226755584",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/EOAjhhgWkAA00w3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOAjhhgWkAA00w3.jpg",
+					"url": "https://t.co/lqvY7wq2XX",
+					"display_url": "pic.twitter.com/lqvY7wq2XX",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1216010961439612928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1820,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 1254,
+									"y": 678,
+									"h": 117,
+									"w": 117
+								},
+								{
+									"x": 702,
+									"y": 382,
+									"h": 143,
+									"w": 143
+								},
+								{
+									"x": 716,
+									"y": 671,
+									"h": 126,
+									"w": 126
+								},
+								{
+									"x": 1524,
+									"y": 408,
+									"h": 127,
+									"w": 127
+								},
+								{
+									"x": 159,
+									"y": 392,
+									"h": 136,
+									"w": 136
+								},
+								{
+									"x": 975,
+									"y": 399,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 424,
+									"y": 652,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 973,
+									"y": 661,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 1229,
+									"y": 389,
+									"h": 152,
+									"w": 152
+								},
+								{
+									"x": 168,
+									"y": 664,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 444,
+									"y": 408,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 53,
+									"y": 421,
+									"h": 366,
+									"w": 366
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1254,
+									"y": 678,
+									"h": 117,
+									"w": 117
+								},
+								{
+									"x": 702,
+									"y": 382,
+									"h": 143,
+									"w": 143
+								},
+								{
+									"x": 716,
+									"y": 671,
+									"h": 126,
+									"w": 126
+								},
+								{
+									"x": 1524,
+									"y": 408,
+									"h": 127,
+									"w": 127
+								},
+								{
+									"x": 159,
+									"y": 392,
+									"h": 136,
+									"w": 136
+								},
+								{
+									"x": 975,
+									"y": 399,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 424,
+									"y": 652,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 973,
+									"y": 661,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 1229,
+									"y": 389,
+									"h": 152,
+									"w": 152
+								},
+								{
+									"x": 168,
+									"y": 664,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 444,
+									"y": 408,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 53,
+									"y": 421,
+									"h": 366,
+									"w": 366
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 826,
+									"y": 447,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 462,
+									"y": 251,
+									"h": 94,
+									"w": 94
+								},
+								{
+									"x": 472,
+									"y": 442,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 1004,
+									"y": 269,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 104,
+									"y": 258,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 642,
+									"y": 263,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 279,
+									"y": 429,
+									"h": 92,
+									"w": 92
+								},
+								{
+									"x": 641,
+									"y": 435,
+									"h": 92,
+									"w": 92
+								},
+								{
+									"x": 810,
+									"y": 256,
+									"h": 100,
+									"w": 100
+								},
+								{
+									"x": 110,
+									"y": 437,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 292,
+									"y": 269,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 34,
+									"y": 277,
+									"h": 241,
+									"w": 241
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 468,
+									"y": 253,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 262,
+									"y": 142,
+									"h": 53,
+									"w": 53
+								},
+								{
+									"x": 267,
+									"y": 250,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 569,
+									"y": 152,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 59,
+									"y": 146,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 364,
+									"y": 149,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 158,
+									"y": 243,
+									"h": 52,
+									"w": 52
+								},
+								{
+									"x": 363,
+									"y": 246,
+									"h": 52,
+									"w": 52
+								},
+								{
+									"x": 459,
+									"y": 145,
+									"h": 56,
+									"w": 56
+								},
+								{
+									"x": 62,
+									"y": 248,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 165,
+									"y": 152,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 19,
+									"y": 157,
+									"h": 136,
+									"w": 136
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1216010958226755600,
+					"id_str": "1216010958226755584",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/EOAjhhgWkAA00w3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOAjhhgWkAA00w3.jpg",
+					"url": "https://t.co/lqvY7wq2XX",
+					"display_url": "pic.twitter.com/lqvY7wq2XX",
+					"expanded_url": "https://twitter.com/TweetMeet/status/1216010961439612928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1820,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 1254,
+									"y": 678,
+									"h": 117,
+									"w": 117
+								},
+								{
+									"x": 702,
+									"y": 382,
+									"h": 143,
+									"w": 143
+								},
+								{
+									"x": 716,
+									"y": 671,
+									"h": 126,
+									"w": 126
+								},
+								{
+									"x": 1524,
+									"y": 408,
+									"h": 127,
+									"w": 127
+								},
+								{
+									"x": 159,
+									"y": 392,
+									"h": 136,
+									"w": 136
+								},
+								{
+									"x": 975,
+									"y": 399,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 424,
+									"y": 652,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 973,
+									"y": 661,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 1229,
+									"y": 389,
+									"h": 152,
+									"w": 152
+								},
+								{
+									"x": 168,
+									"y": 664,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 444,
+									"y": 408,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 53,
+									"y": 421,
+									"h": 366,
+									"w": 366
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1254,
+									"y": 678,
+									"h": 117,
+									"w": 117
+								},
+								{
+									"x": 702,
+									"y": 382,
+									"h": 143,
+									"w": 143
+								},
+								{
+									"x": 716,
+									"y": 671,
+									"h": 126,
+									"w": 126
+								},
+								{
+									"x": 1524,
+									"y": 408,
+									"h": 127,
+									"w": 127
+								},
+								{
+									"x": 159,
+									"y": 392,
+									"h": 136,
+									"w": 136
+								},
+								{
+									"x": 975,
+									"y": 399,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 424,
+									"y": 652,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 973,
+									"y": 661,
+									"h": 140,
+									"w": 140
+								},
+								{
+									"x": 1229,
+									"y": 389,
+									"h": 152,
+									"w": 152
+								},
+								{
+									"x": 168,
+									"y": 664,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 444,
+									"y": 408,
+									"h": 135,
+									"w": 135
+								},
+								{
+									"x": 53,
+									"y": 421,
+									"h": 366,
+									"w": 366
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 826,
+									"y": 447,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 462,
+									"y": 251,
+									"h": 94,
+									"w": 94
+								},
+								{
+									"x": 472,
+									"y": 442,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 1004,
+									"y": 269,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 104,
+									"y": 258,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 642,
+									"y": 263,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 279,
+									"y": 429,
+									"h": 92,
+									"w": 92
+								},
+								{
+									"x": 641,
+									"y": 435,
+									"h": 92,
+									"w": 92
+								},
+								{
+									"x": 810,
+									"y": 256,
+									"h": 100,
+									"w": 100
+								},
+								{
+									"x": 110,
+									"y": 437,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 292,
+									"y": 269,
+									"h": 89,
+									"w": 89
+								},
+								{
+									"x": 34,
+									"y": 277,
+									"h": 241,
+									"w": 241
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 468,
+									"y": 253,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 262,
+									"y": 142,
+									"h": 53,
+									"w": 53
+								},
+								{
+									"x": 267,
+									"y": 250,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 569,
+									"y": 152,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 59,
+									"y": 146,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 364,
+									"y": 149,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 158,
+									"y": 243,
+									"h": 52,
+									"w": 52
+								},
+								{
+									"x": 363,
+									"y": 246,
+									"h": 52,
+									"w": 52
+								},
+								{
+									"x": 459,
+									"y": 145,
+									"h": 56,
+									"w": 56
+								},
+								{
+									"x": 62,
+									"y": 248,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 165,
+									"y": 152,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 19,
+									"y": 157,
+									"h": 136,
+									"w": 136
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15665199,
+			"id_str": "15665199",
+			"name": "TweetMeet Central #MSFTEduChat",
+			"screen_name": "TweetMeet",
+			"location": "Create your own TweetMeet Friend Card:",
+			"description": "#MSFTEduChat TweetMeets are global, multilingual chats by @MicrosoftEDU to connect educators. \nTeam: Marjolein Hoekstra (#MVP), @fcotexeira & @AnicaTrickovic",
+			"url": "https://t.co/zl3b8neRWm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zl3b8neRWm",
+							"expanded_url": "https://twt.mt/tweetmeet-friend-card",
+							"display_url": "twt.mt/tweetmeet-frie…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4394,
+			"friends_count": 3943,
+			"listed_count": 35,
+			"created_at": "Wed Jul 30 19:57:53 +0000 2008",
+			"favourites_count": 5862,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1576,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125073469702647808/77LVVB2i_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15665199/1577552829",
+			"profile_link_color": "E3008C",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 80,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🥁 🎈  Announcing our hosts! \nWe're super-proud to announce the ⚡brand-new ⚡ crew for the #MSFTEduChat TweetMeet about Devices for #StudentSuccess.\n\nFollow them all through this Twitter List:\nhttps://t.co/m3SBMDwmMT\n\nWill join us January 21? We love 💖 your RT 🔁\n#MicrosoftEDU https://t.co/lqvY7wq2XX"
+	},
+	{
 		"created_at": "Mon Jan 06 07:53:05 +0000 2020",
 		"id": 1022337444262211600,
 		"id_str": "1214092510127153152",
