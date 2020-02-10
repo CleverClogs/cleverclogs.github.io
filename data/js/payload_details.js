9007e3a2850cd6f4d28a96c97e9d780a6d2af619
CleverClogs <github@cleverclogs.org>
2020-02-10 22:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30973,
-	"created_at": "Sun Feb 09 01:19:27 +0000 2020",
+	"tweets": 30974,
+	"created_at": "Mon Feb 10 21:19:27 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,159 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 10 20:21:16 +0000 2020",
+		"id": 1226964370258829300,
+		"id_str": "1226964370258829318",
+		"full_text": "Creative and tech YouTuber Sara Dietschy is a fan of #Microsoft #OneNote in combination with digital inking on pen-enabled two-in-one Windows devices and iPad. \n\nCheck out her brand-new video:\nhttps://t.co/fbTZL2sInj \nby @saradietschy \n\n#digitalink #notetaking #studentlife",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Microsoft",
+					"indices": [
+						53,
+						63
+					]
+				},
+				{
+					"text": "OneNote",
+					"indices": [
+						64,
+						72
+					]
+				},
+				{
+					"text": "digitalink",
+					"indices": [
+						237,
+						248
+					]
+				},
+				{
+					"text": "notetaking",
+					"indices": [
+						249,
+						260
+					]
+				},
+				{
+					"text": "studentlife",
+					"indices": [
+						261,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "saradietschy",
+					"name": "Sara 🍑y",
+					"id": 258475649,
+					"id_str": "258475649",
+					"indices": [
+						221,
+						234
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/fbTZL2sInj",
+					"expanded_url": "https://www.youtube.com/watch?v=DrB7B2dxCiA",
+					"display_url": "youtube.com/watch?v=DrB7B2…",
+					"indices": [
+						193,
+						216
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2582768738,
+			"id_str": "2582768738",
+			"name": "OneNote Central",
+			"screen_name": "OneNoteC",
+			"location": "OneNote 2016 Secrets book:",
+			"description": "Independent curation of Microsoft #OneNote tips, news & resources by Marjolein Hoekstra, Microsoft MVP, trainer, consultant, #MSFTEduChat @TweetMeet strategist.",
+			"url": "https://t.co/jNdXyq42Kc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jNdXyq42Kc",
+							"expanded_url": "https://www.amazon.com/OneNote-Secrets-Tips-2013-2016-ebook/dp/B01NAR5HNJ/",
+							"display_url": "amazon.com/OneNote-Secret…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23635,
+			"friends_count": 7757,
+			"listed_count": 649,
+			"created_at": "Sun Jun 22 20:11:47 +0000 2014",
+			"favourites_count": 28755,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25722,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/905876685945536514/byKN0QAg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2582768738/1580135141",
+			"profile_link_color": "7719AA",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Creative and tech YouTuber Sara Dietschy is a fan of #Microsoft #OneNote in combination with digital inking on pen-enabled two-in-one Windows devices and iPad. \n\nCheck out her brand-new video:\nhttps://t.co/fbTZL2sInj \nby @saradietschy \n\n#digitalink #notetaking #studentlife"
+	},
+	{
 		"created_at": "Sun Feb 09 00:32:33 +0000 2020",
 		"id": 1226184342147199000,
 		"id_str": "1226302832502091781",
