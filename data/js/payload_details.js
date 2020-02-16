a574b554692fc953e83a753df640fab39d07b4ba
CleverClogs <github@cleverclogs.org>
2020-02-16 21:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30977,
-	"created_at": "Thu Feb 13 17:19:28 +0000 2020",
+	"tweets": 30978,
+	"created_at": "Sun Feb 16 20:19:27 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 16 19:24:26 +0000 2020",
+		"id": 1229121395369472000,
+		"id_str": "1229124393672040448",
+		"full_text": "Did you know there is a special page to fix up permissions issues with your #OneNote Class or Staff Notebook? Self-serve permissions fixing, ready today! #edtech #MIEExpert \n\n✅ Link here: https://t.co/5vOmHcmyCZ\n\n✅ Support article here: https://t.co/IyyqqnieQE https://t.co/5dQhQhujL9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OneNote",
+					"indices": [
+						76,
+						84
+					]
+				},
+				{
+					"text": "edtech",
+					"indices": [
+						154,
+						161
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						162,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5vOmHcmyCZ",
+					"expanded_url": "https://www.onenote.com/classnotebook/notebook-fix",
+					"display_url": "onenote.com/classnotebook/…",
+					"indices": [
+						188,
+						211
+					]
+				},
+				{
+					"url": "https://t.co/IyyqqnieQE",
+					"expanded_url": "https://support.office.com/en-us/article/Fix-Class-Notebook-when-Welcome-Collaboration-Space-or-Content-Library-are-missing-dae0729e-a7c5-46ed-809b-ec06900f0d21",
+					"display_url": "support.office.com/en-us/article/…",
+					"indices": [
+						237,
+						260
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1229121389090594800,
+					"id_str": "1229121389090594816",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/EQ63ZCaUYAAfCHS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQ63ZCaUYAAfCHS.jpg",
+					"url": "https://t.co/5dQhQhujL9",
+					"display_url": "pic.twitter.com/5dQhQhujL9",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1229121395369472000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1222,
+							"h": 682,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 670,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1229121389090594800,
+					"id_str": "1229121389090594816",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/EQ63ZCaUYAAfCHS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQ63ZCaUYAAfCHS.jpg",
+					"url": "https://t.co/5dQhQhujL9",
+					"display_url": "pic.twitter.com/5dQhQhujL9",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1229121395369472000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1222,
+							"h": 682,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 670,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, #ImmersiveReader & #MicrosoftTeams @OneNoteEDU #edtech #accessibility",
+			"url": "https://t.co/ANIFpSVE4b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSVE4b",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23625,
+			"friends_count": 3560,
+			"listed_count": 777,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 90880,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35588,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Did you know there is a special page to fix up permissions issues with your #OneNote Class or Staff Notebook? Self-serve permissions fixing, ready today! #edtech #MIEExpert \n\n✅ Link here: https://t.co/5vOmHcmyCZ\n\n✅ Support article here: https://t.co/IyyqqnieQE https://t.co/5dQhQhujL9"
+	},
+	{
 		"created_at": "Thu Feb 13 16:25:34 +0000 2020",
 		"id": 1227992219145179100,
 		"id_str": "1227992219145179136",
