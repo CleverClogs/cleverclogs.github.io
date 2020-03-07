3ed2d12055935312e85923163039116974950115
CleverClogs <github@cleverclogs.org>
2020-03-07 19:19:28 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30987,
-	"created_at": "Fri Mar 06 22:19:27 +0000 2020",
+	"tweets": 30988,
+	"created_at": "Sat Mar 07 18:19:27 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sat Mar 07 17:23:27 +0000 2020",
+		"id": 1236338050285764600,
+		"id_str": "1236341708125868034",
+		"full_text": "NEW! Remote teaching and learning in Office 365.\n\nWe just published a new support guide to help educators, administrators and students learning remotely with @MicrosoftTeams, @OneNoteEDU, and @Office365 (free tools for EDU!) #edchat #cpchat #suptchat\n\n🔗 https://t.co/Ih6KP8VHfp https://t.co/EtXpqRjGUK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edchat",
+					"indices": [
+						225,
+						232
+					]
+				},
+				{
+					"text": "cpchat",
+					"indices": [
+						233,
+						240
+					]
+				},
+				{
+					"text": "suptchat",
+					"indices": [
+						241,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MicrosoftTeams",
+					"name": "Microsoft Teams",
+					"id": 817461289081409500,
+					"id_str": "817461289081409536",
+					"indices": [
+						158,
+						173
+					]
+				},
+				{
+					"screen_name": "OneNoteEDU",
+					"name": "OneNote Education",
+					"id": 3096566760,
+					"id_str": "3096566760",
+					"indices": [
+						175,
+						186
+					]
+				},
+				{
+					"screen_name": "Office365",
+					"name": "Office 365",
+					"id": 137365854,
+					"id_str": "137365854",
+					"indices": [
+						192,
+						202
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ih6KP8VHfp",
+					"expanded_url": "https://support.office.com/en-us/article/remote-teaching-and-learning-in-office-365-f651ccae-7b65-478b-8366-51bb884025c4?ui=en-US&rs=en-US&ad=US",
+					"display_url": "support.office.com/en-us/article/…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1236337561011810300,
+					"id_str": "1236337561011810305",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/EShadhqU8AEkPwg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EShadhqU8AEkPwg.jpg",
+					"url": "https://t.co/EtXpqRjGUK",
+					"display_url": "pic.twitter.com/EtXpqRjGUK",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1236338050285764609/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1236337561011810300,
+					"id_str": "1236337561011810305",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/EShadhqU8AEkPwg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EShadhqU8AEkPwg.jpg",
+					"url": "https://t.co/EtXpqRjGUK",
+					"display_url": "pic.twitter.com/EtXpqRjGUK",
+					"expanded_url": "https://twitter.com/mtholfsen/status/1236338050285764609/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27673327,
+			"id_str": "27673327",
+			"name": "Mike Tholfsen",
+			"screen_name": "mtholfsen",
+			"location": "Redmond, WA",
+			"description": "Product Manager on the #MicrosoftEDU team, building #OneNote, Class Notebooks, #ImmersiveReader & #MicrosoftTeams @OneNoteEDU #edtech #NCCE2020",
+			"url": "https://t.co/ANIFpSVE4b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANIFpSVE4b",
+							"expanded_url": "https://techcommunity.microsoft.com/t5/Education-Blog/bg-p/EducationBlog",
+							"display_url": "techcommunity.microsoft.com/t5/Education-B…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23911,
+			"friends_count": 3582,
+			"listed_count": 780,
+			"created_at": "Mon Mar 30 16:27:51 +0000 2009",
+			"favourites_count": 91972,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35904,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715882163283988481/4vljH58f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27673327/1422222208",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 27,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "NEW! Remote teaching and learning in Office 365.\n\nWe just published a new support guide to help educators, administrators and students learning remotely with @MicrosoftTeams, @OneNoteEDU, and @Office365 (free tools for EDU!) #edchat #cpchat #suptchat\n\n🔗 https://t.co/Ih6KP8VHfp https://t.co/EtXpqRjGUK"
+	},
+	{
 		"created_at": "Fri Mar 06 21:36:50 +0000 2020",
 		"id": 1236042912217120800,
 		"id_str": "1236043083676094465",
